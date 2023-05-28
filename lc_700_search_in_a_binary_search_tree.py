ccd980786dcdab67ebee2a41e186e7d947c02e6c
Anh Lai <laingocanh1990@gmail.com>
2023-05-28 15:00:41 +0700

    700. Search in a Binary Search Tree

---
+++ lc_700_search_in_a_binary_search_tree.py
@@ -0,0 +1,23 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def searchBST(root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+    if not root:
+        return None
+    if root.val == val:
+        return root
+    if val < root.val:
+        return searchBST(root.left, val)
+    return searchBST(root.right, val)
+
+
+class Solution:
+    pass
+
