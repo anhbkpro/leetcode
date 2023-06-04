49a371074b79559f86fbff66cda68f03c70ad85e
Anh Lai <laingocanh1990@gmail.com>
2023-06-04 08:00:56 +0700

    739. Daily Temperatures - Approach 1: Monotonic Stack

---
+++ lc_739_daily_temperatures.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+def dailyTemperatures(temperatures: List[int]) -> List[int]:
+    stack = []
+    result = [0] * len(temperatures)
+    for i, t in enumerate(temperatures):
+        while stack and temperatures[stack[-1]] < t:
+            result[stack[-1]] = i - stack[-1]
+            stack.pop()
+        stack.append(i)
+    return result
+
+
+class Solution:
+    pass
