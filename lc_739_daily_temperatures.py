4cc4878d29150567482a097973bf682e2d0b40f0
Anh Lai <laingocanh1990@gmail.com>
2023-06-04 08:04:46 +0700

    739. Daily Temperatures - Approach 1: Monotonic Stack

--- lc_739_daily_temperatures.py
+++ lc_739_daily_temperatures.py
@@ -2,13 +2,18 @@ from typing import List
 
 
 def dailyTemperatures(temperatures: List[int]) -> List[int]:
+    n = len(temperatures)
     stack = []
-    result = [0] * len(temperatures)
-    for i, t in enumerate(temperatures):
-        while stack and temperatures[stack[-1]] < t:
-            result[stack[-1]] = i - stack[-1]
-            stack.pop()
-        stack.append(i)
+    result = [0] * n
+
+    for curr_day, curr_temp in enumerate(temperatures):
+        # Pop until the current day's temperature is not
+        # warmer than the temperature at the top of the stack
+        while stack and temperatures[stack[-1]] < curr_temp:
+            prev_day = stack.pop()
+            result[prev_day] = curr_day - prev_day
+        stack.append(curr_day)
+
     return result
 
 
