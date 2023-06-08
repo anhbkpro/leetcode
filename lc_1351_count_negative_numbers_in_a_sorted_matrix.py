a81fb0ac72feb5286ce7bc630619065c3b959daa
Anh Lai <laingocanh1990@gmail.com>
2023-06-08 13:45:29 +0700

    simplify code

--- lc_1351_count_negative_numbers_in_a_sorted_matrix.py
+++ lc_1351_count_negative_numbers_in_a_sorted_matrix.py
@@ -2,22 +2,22 @@ from typing import List
 
 
 def countNegatives(grid: List[List[int]]) -> int:
-    rows = len(grid)
     cols = len(grid[0])
     last_negative = cols - 1
     answer = 0
-    for row in range(rows):
-        if grid[row][0] < 0:
+    for row in grid:
+        if row[0] < 0:
             answer += cols
             continue
 
-        if grid[row][cols - 1] >= 0:
+        if row[cols - 1] >= 0:
             continue
-
+        # Using binary search find the index
+        # which has the first negative element.
         lo, hi = 0, last_negative
         while lo <= hi:
             mid = lo + (hi - lo) // 2
-            if grid[row][mid] < 0:
+            if row[mid] < 0:
                 hi = mid - 1
             else:
                 lo = mid + 1
