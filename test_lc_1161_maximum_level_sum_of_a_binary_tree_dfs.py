74a31b94fda1867d6d6164674588a4dd4c9161b0
Anh Lai <laingocanh1990@gmail.com>
2023-06-09 09:19:17 +0700

    test 1161. Maximum Level Sum of a Binary Tree - Approach 2: Depth First Search

---
+++ test_lc_1161_maximum_level_sum_of_a_binary_tree_dfs.py
@@ -0,0 +1,6 @@
+from lc_1161_maximum_level_sum_of_a_binary_tree_dfs import maxLevelSum, TreeNode
+
+
+def test_max_level_sum():
+    node = TreeNode(val=1, left=TreeNode(val=7, left=TreeNode(val=7), right=TreeNode(val=-8)), right=TreeNode(val=0))
+    assert maxLevelSum(node) == 2
