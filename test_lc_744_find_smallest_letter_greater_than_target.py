7772c338b7e3fd34441315e6e7e289ebe30cdf48
Anh Lai <laingocanh1990@gmail.com>
2023-06-09 08:31:36 +0700

    test 744. Find Smallest Letter Greater Than Target - Approach 1: Brute Force

---
+++ test_lc_744_find_smallest_letter_greater_than_target.py
@@ -0,0 +1,5 @@
+from lc_744_find_smallest_letter_greater_than_target import nextGreatestLetter
+
+
+def test_next_greatest_letter():
+    assert nextGreatestLetter(letters=["c", "f", "j"], target="a") == "c"
