334b6cc5660d155982816214afc35f32cfabfd22
Anh Lai <laingocanh1990@gmail.com>
2023-06-16 13:22:10 +0700

    test 17. Letter Combinations of a Phone Number - Approach: Backtracking

---
+++ test_lc_17_letter_combinations_of_a_phone_number.py
@@ -0,0 +1,5 @@
+from lc_17_letter_combinations_of_a_phone_number import letter_combinations
+
+
+def test_letter_combinations():
+    assert letter_combinations("23") == ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
