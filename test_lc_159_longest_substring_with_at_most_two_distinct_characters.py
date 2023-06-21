18b17bd0a072242a308c6eaa9459b5e50691b493
Anh Lai <laingocanh1990@gmail.com>
2023-06-21 07:06:11 +0700

    test 159. Longest Substring with At Most Two Distinct Characters - Approach 1: Sliding Window

---
+++ test_lc_159_longest_substring_with_at_most_two_distinct_characters.py
@@ -0,0 +1,6 @@
+from lc_159_longest_substring_with_at_most_two_distinct_characters import lengthOfLongestSubstringTwoDistinct
+
+
+def test_length_of_longest_substring_two_distinct():
+    assert lengthOfLongestSubstringTwoDistinct("eceba") == 3
+    assert lengthOfLongestSubstringTwoDistinct("ccaabbb") == 5
