3634706b88ab3f261bbb856c5dbe7ff9c0546d23
Anh Lai <laingocanh1990@gmail.com>
2023-06-22 20:00:51 +0700

    test 340. Longest Substring with At Most K Distinct Characters - Approach 1: Binary Search + Fixed Size Sliding Window

---
+++ test_lc_340_longest_substring_with_at_most_k_distinct_characters_bs.py
@@ -0,0 +1,5 @@
+from lc_340_longest_substring_with_at_most_k_distinct_characters_bs import lengthOfLongestSubstringKDistinct
+
+
+def test_length_of_longest_substring_kdistinct():
+    assert lengthOfLongestSubstringKDistinct(s="eceba", k=2) == 3
