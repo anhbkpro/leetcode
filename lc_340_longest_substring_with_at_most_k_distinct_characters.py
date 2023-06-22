b3be2f99528150aafae4d5bab49818f395739660
Anh Lai <laingocanh1990@gmail.com>
2023-06-22 19:50:32 +0700

    340. Longest Substring with At Most K Distinct Characters - Approach 2: Sliding Window

---
+++ lc_340_longest_substring_with_at_most_k_distinct_characters.py
@@ -0,0 +1,25 @@
+import collections
+
+
+def lengthOfLongestSubstringKDistinct(s: str, k: int) -> int:
+    n = len(s)
+    max_size = 0
+    counter = collections.Counter()
+
+    left = 0
+    for right in range(n):
+        counter[s[right]] += 1
+
+        while len(counter) > k:
+            counter[s[left]] -= 1
+            if counter[s[left]] == 0:
+                del counter[s[left]]
+            left += 1
+
+        max_size = max(max_size, right - left + 1)
+
+    return max_size
+
+
+class Solution:
+    pass
