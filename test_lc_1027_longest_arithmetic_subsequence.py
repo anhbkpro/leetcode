6a02ff21283ffb2b7ae035a64ef3d3244e1ea6f9
Anh Lai <laingocanh1990@gmail.com>
2023-06-23 22:01:51 +0700

    test 1027. Longest Arithmetic Subsequence - DP

---
+++ test_lc_1027_longest_arithmetic_subsequence.py
@@ -0,0 +1,6 @@
+from lc_1027_longest_arithmetic_subsequence import longestArithSeqLength
+
+
+def test_longest_arith_seq_length():
+    assert longestArithSeqLength([3, 6, 9, 12]) == 4
+    assert longestArithSeqLength([9, 4, 7, 2, 10]) == 3
