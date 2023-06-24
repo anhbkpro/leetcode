d61d67abf88f85aa13e7a8eb8fb64556741276f5
Anh Lai <laingocanh1990@gmail.com>
2023-06-24 14:32:56 +0700

    test 956. Tallest Billboard - Approach 2: Dynamic Programming

---
+++ test_lc_956_tallest_billboard.py
@@ -0,0 +1,6 @@
+from lc_956_tallest_billboard import tallestBillboard
+
+
+def test_tallest_billboard():
+    assert tallestBillboard([1, 2, 3, 6]) == 6
+    assert tallestBillboard([1, 2, 3, 4, 5, 6]) == 10
