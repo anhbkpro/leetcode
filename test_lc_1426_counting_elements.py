67871d11069cf24be4c46588557f1263b871e3ad
Anh Lai <laingocanh1990@gmail.com>
2023-06-25 08:19:07 +0700

    test 1426. Counting Elements - Hashing

---
+++ test_lc_1426_counting_elements.py
@@ -0,0 +1,6 @@
+from lc_1426_counting_elements import countElements
+
+
+def test_count_elements():
+    assert countElements([1, 2, 3]) == 2
+    assert countElements([1, 1, 3, 3, 5, 5, 7, 7]) == 0
