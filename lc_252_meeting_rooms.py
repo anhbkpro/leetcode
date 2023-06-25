513f1c600a91719a7846c945345549bc7c65f5c8
Anh Lai <laingocanh1990@gmail.com>
2023-06-25 15:25:28 +0700

    252. Meeting Rooms

--- lc_252_meeting_rooms.py
+++ lc_252_meeting_rooms.py
@@ -2,15 +2,10 @@ from typing import List
 
 
 def canAttendMeetings(intervals: List[List[int]]) -> bool:
-    if len(intervals) <= 1:
-        return True
-
     intervals.sort()
-
     for i in range(len(intervals) - 1):
         if intervals[i][1] > intervals[i + 1][0]:
             return False
-
     return True
 
 
