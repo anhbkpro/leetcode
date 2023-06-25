8d085ec76916d913e18df993926336734bbb8577
Anh Lai <laingocanh1990@gmail.com>
2023-06-25 11:43:59 +0700

    using short syntax of intersection: &

--- lc_1198_find_smallest_common_element_in_all_rows.py
+++ lc_1198_find_smallest_common_element_in_all_rows.py
@@ -5,9 +5,7 @@ from typing import List
 def smallestCommonElement(mat: List[List[int]]) -> int:
     common_hash = set(mat[0])
     for row in mat[1:]:
-        current_hash = set(row)
-        common_hash = common_hash.intersection(current_hash)
-        print(common_hash)
+        common_hash &= set(row)
 
     ans = sys.maxsize
     for item in common_hash:
