181909770d7852a39b104e283d0139a3a858686a
Anh Lai <laingocanh1990@gmail.com>
2023-06-25 11:12:20 +0700

    test 1575. Count All Possible Routes - Approach 1: Recursive Dynamic Programming

---
+++ test_lc_1575_count_all_possible_routes.py
@@ -0,0 +1,6 @@
+from lc_1575_count_all_possible_routes import countRoutes
+
+
+def test_count_routes():
+    assert countRoutes([2, 3, 6, 8, 4], 1, 3, 5) == 4
+    assert countRoutes([4, 3, 1], 1, 0, 6) == 5
