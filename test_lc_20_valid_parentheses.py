175f3b1d3899f85765cadb385c81c6bb3cf7c9b4
Anh Lai <laingocanh1990@gmail.com>
2023-06-28 06:38:14 +0700

    test 20. Valid Parentheses - Stack

--- test_lc_20_valid_parentheses.py
+++ test_lc_20_valid_parentheses.py
@@ -1,7 +1,10 @@
-from lc_20_valid_parentheses import isValid
+from lc_20_valid_parentheses import isValid, isValid_LC_solution
 
 
 def test_is_valid():
     assert isValid("()") is True
     assert isValid("()[]{}") is True
     assert isValid("(]") is False
+    assert isValid_LC_solution("()") is True
+    assert isValid_LC_solution("()[]{}") is True
+    assert isValid_LC_solution("(]") is False
