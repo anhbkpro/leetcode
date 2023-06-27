b3c7bcbf65b0ccb222e25bf9ca898f6b2f28f957
Anh Lai <laingocanh1990@gmail.com>
2023-06-27 21:37:01 +0700

    test 64. Minimum Path Sum - DP

---
+++ test_lc_64_minimum_path_sum.py
@@ -0,0 +1,6 @@
+from lc_64_minimum_path_sum import minPathSum
+
+
+def test_min_path_sum():
+    assert minPathSum([[1, 3, 1], [1, 5, 1], [4, 2, 1]]) == 7
+    assert minPathSum([[1, 2, 3], [4, 5, 6]]) == 12
