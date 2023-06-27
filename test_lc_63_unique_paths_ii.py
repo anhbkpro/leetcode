8d505fc53fb7dd4cfd6ea8638d50fe8c57f154e2
Anh Lai <laingocanh1990@gmail.com>
2023-06-28 06:36:20 +0700

    test 63. Unique Paths II - DP using 1 dimension array

--- test_lc_63_unique_paths_ii.py
+++ test_lc_63_unique_paths_ii.py
@@ -1,6 +1,8 @@
-from lc_63_unique_paths_ii import uniquePathsWithObstacles
+from lc_63_unique_paths_ii import uniquePathsWithObstacles, uniquePathsWithObstacles1DArray
 
 
 def test_unique_paths_with_obstacles():
     assert uniquePathsWithObstacles([[0, 0, 0], [0, 1, 0], [0, 0, 0]]) == 2
     assert uniquePathsWithObstacles([[0, 1], [0, 0]]) == 1
+    assert uniquePathsWithObstacles1DArray([[0, 0, 0], [0, 1, 0], [0, 0, 0]]) == 2
+    assert uniquePathsWithObstacles1DArray([[0, 1], [0, 0]]) == 1
