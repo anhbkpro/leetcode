4ee0d086bf5f7911b1d176f3c1429178cadaa7b3
Anh Lai <laingocanh1990@gmail.com>
2023-06-30 06:32:02 +0700

    add comments

--- lc_1474_delete_n_nodes_after_m_nodes_of_a_linked_list.py
+++ lc_1474_delete_n_nodes_after_m_nodes_of_a_linked_list.py
@@ -10,17 +10,19 @@ def deleteNodes(head: ListNode, m: int, n: int) -> ListNode:
     curr = head
     last = head
     while curr:
-        m_count = m
-        n_count = n
+        m_count, n_count = m, n
+        # traverse m nodes
         while curr and m_count:
             last = curr
             curr = curr.next
             m_count -= 1
 
+        # traverse n nodes
         while curr and n_count:
             curr = curr.next
             n_count -= 1
 
+        # delete n nodes
         last.next = curr
 
     return head
