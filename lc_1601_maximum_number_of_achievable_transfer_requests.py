27d779f84c9f4764bfb4fe9b757fa5e82c1f67f2
Anh Lai <laingocanh1990@gmail.com>
2023-07-02 07:56:50 +0700

    what is itertools.combinations?

--- lc_1601_maximum_number_of_achievable_transfer_requests.py
+++ lc_1601_maximum_number_of_achievable_transfer_requests.py
@@ -3,12 +3,13 @@ import itertools
 
 def maximumRequests(n, req):
     for k in range(len(req), 0, -1):
+        # https://docs.python.org/3/library/itertools.html#itertools.combinations
         for c in itertools.combinations(range(len(req)), k):
             degree = [0] * n
             for i in c:
                 degree[req[i][0]] -= 1
                 degree[req[i][1]] += 1
-            if not any(degree):
+            if not any(degree):  # all(degree[i] == 0 for i in range(n)): For example: degree: [0, 0, 0, 0, 0]
                 return k
     return 0
 
