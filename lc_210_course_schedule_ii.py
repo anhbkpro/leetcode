9074e31c89d88d65a3f70bd05ef528345bacfa0b
Anh Lai <laingocanh1990@gmail.com>
2023-07-12 22:08:48 +0700

    210. Course Schedule II - Approach 2: Using Node Indegree (Kahn's Algorithm)

---
+++ lc_210_course_schedule_ii.py
@@ -0,0 +1,36 @@
+from collections import deque
+from typing import List
+
+
+def find_order(numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+    indegree = [0] * numCourses
+    adj = [[] for _ in range(numCourses)]
+
+    for pair in prerequisites:
+        first, second = pair
+        indegree[first] += 1
+        adj[second].append(first)
+
+    q = deque()
+    for i in range(numCourses):
+        if indegree[i] == 0:
+            q.append(i)
+
+    res = []
+    while q:
+        node = q.popleft()
+        res.append(node)
+        for neighbor in adj[node]:
+            # Delete the edge "node -> neighbor".
+            indegree[neighbor] -= 1
+            if indegree[neighbor] == 0:
+                q.append(neighbor)
+
+    if any(indegree):
+        return []
+
+    return res
+
+
+class Solution:
+    pass
