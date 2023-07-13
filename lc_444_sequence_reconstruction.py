63c99b87cda113cdf1c02bd2c84222371a82a8b5
Anh Lai <laingocanh1990@gmail.com>
2023-07-13 11:15:45 +0700

    444. Sequence Reconstruction - Topological Sort Using Kahn's Algorithm

---
+++ lc_444_sequence_reconstruction.py
@@ -0,0 +1,37 @@
+from collections import deque
+from typing import List
+
+
+def sequenceReconstruction(nums: List[int], sequences: List[List[int]]) -> bool:
+    n = len(nums)
+    indegree = [0] * (n + 1)
+    adj = [[] for _ in range(n + 1)]
+
+    for sequence in sequences:
+        for idx in range(len(sequence) - 1):
+            second, first = sequence[idx], sequence[idx + 1]
+            indegree[second] += 1
+            adj[first].append(second)
+
+    q = deque()
+    for i in range(1, n + 1):
+        if indegree[i] == 0:
+            q.append(i)
+
+    if len(q) > 1:
+        return False
+
+    while q:
+        node = q.popleft()
+        for neighbor in adj[node]:
+            indegree[neighbor] -= 1
+            if indegree[neighbor] == 0:
+                q.append(neighbor)
+                if len(q) > 1:  # so this means there are more than one supersequence
+                    return False
+
+    return True
+
+
+class Solution:
+    pass
