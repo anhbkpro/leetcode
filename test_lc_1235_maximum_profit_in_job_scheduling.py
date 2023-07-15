d0cd6f8638b0889cf591a5c105772dc6f798b8a7
Anh Lai <laingocanh1990@gmail.com>
2023-07-15 11:44:56 +0700

    test 1235. Maximum Profit in Job Scheduling - Approach: Top-Down Dynamic Programming + Binary Search

---
+++ test_lc_1235_maximum_profit_in_job_scheduling.py
@@ -0,0 +1,7 @@
+from lc_1235_maximum_profit_in_job_scheduling import job_scheduling
+
+
+def test_job_scheduling():
+    assert job_scheduling(startTime=[1, 2, 3, 3], endTime=[3, 4, 5, 6], profit=[50, 10, 40, 70]) == 120
+    assert job_scheduling(startTime=[1, 2, 3, 4, 6], endTime=[3, 5, 10, 6, 9], profit=[20, 20, 100, 70, 60]) == 150
+    assert job_scheduling(startTime=[1, 1, 1], endTime=[2, 3, 4], profit=[5, 6, 4]) == 6
