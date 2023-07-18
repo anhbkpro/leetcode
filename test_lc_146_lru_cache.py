cbcbc69e72c1752cf3e9cefb078d366e5ac4209e
Anh Lai <laingocanh1990@gmail.com>
2023-07-18 22:11:11 +0700

    test 146. LRU Cache - Approach: Doubly Linked List

---
+++ test_lc_146_lru_cache.py
@@ -0,0 +1,13 @@
+from lc_146_lru_cache import LRUCache
+
+def test_lru_cache():
+    cache = LRUCache(2)
+    cache.put(1, 1)
+    cache.put(2, 2)
+    assert cache.get(1) == 1
+    cache.put(3, 3)
+    assert cache.get(2) == -1
+    cache.put(4, 4)
+    assert cache.get(1) == -1
+    assert cache.get(3) == 3
+    assert cache.get(4) == 4
