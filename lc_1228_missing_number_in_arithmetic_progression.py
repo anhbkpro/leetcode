a1673219976dbed950190c2369e8909173bbb3b3
Anh Lai <laingocanh1990@gmail.com>
2023-07-20 21:03:32 +0700

    Linear search

--- lc_1228_missing_number_in_arithmetic_progression.py
+++ lc_1228_missing_number_in_arithmetic_progression.py
@@ -4,12 +4,14 @@ from typing import List
 def missing_number(arr: List[int]) -> int:
     n = len(arr)
     step = (arr[-1] - arr[0]) // n
+    expected = arr[0]
 
-    for i in range(n):
-        if arr[i] != arr[0] + i * step:
-            return arr[0] + i * step
+    for val in arr:
+        if val != expected:
+            return expected
+        expected += step
 
-    return arr[0]
+    return expected
 
 
 class Solution:
