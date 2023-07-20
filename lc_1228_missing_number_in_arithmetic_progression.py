60a5983175a789313935a0aec94c2bfcb3ab0130
Anh Lai <laingocanh1990@gmail.com>
2023-07-20 21:07:21 +0700

    1228. Missing Number In Arithmetic Progression - Binary Search

--- lc_1228_missing_number_in_arithmetic_progression.py
+++ lc_1228_missing_number_in_arithmetic_progression.py
@@ -14,5 +14,21 @@ def missing_number(arr: List[int]) -> int:
     return expected
 
 
+def missing_number_binary_search(arr: List[int]) -> int:
+    n = len(arr)
+    step = (arr[-1] - arr[0]) // n
+    left = 0
+    right = n - 1
+
+    while left < right:
+        mid = (left + right) // 2
+        if arr[mid] == arr[0] + mid * step:
+            left = mid + 1
+        else:
+            right = mid
+
+    return arr[0] + left * step
+
+
 class Solution:
     pass
