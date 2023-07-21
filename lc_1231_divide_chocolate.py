17204111d48519828a7c3de7fd6f80fbafe1af23
Anh Lai <laingocanh1990@gmail.com>
2023-07-21 21:39:28 +0700

    1231. Divide Chocolate - Binary Search + Greedy

---
+++ lc_1231_divide_chocolate.py
@@ -0,0 +1,30 @@
+from typing import List
+
+
+def maximizeSweetness(sweetness: List[int], k: int) -> int:
+    number_of_people = k + 1
+    lo, hi = min(sweetness), sum(sweetness) // number_of_people
+    while lo < hi:
+        mid = (lo + hi + 1) // 2
+        if canCutSweetness(mid, sweetness, k):
+            lo = mid
+        else:
+            hi = mid - 1
+
+    return hi
+
+
+class Solution:
+    pass
+
+
+def canCutSweetness(mid: int, sweetness: List[int], k: int):
+    cur_sweetness = 0
+    people_with_chocolate = 0
+    for s in sweetness:
+        cur_sweetness += s
+        if cur_sweetness >= mid:
+            cur_sweetness = 0
+            people_with_chocolate += 1
+
+    return people_with_chocolate >= k + 1
