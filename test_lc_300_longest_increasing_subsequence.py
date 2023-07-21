91bf59eacc032c94b905e5bd2612de71f78c956c
Anh Lai <laingocanh1990@gmail.com>
2023-07-21 21:49:09 +0700

    test 300. Longest Increasing Subsequence - Dynamic Programming

---
+++ test_lc_300_longest_increasing_subsequence.py
@@ -0,0 +1,6 @@
+from lc_300_longest_increasing_subsequence import lengthOfLIS
+
+
+def test_length_of_lis():
+    assert lengthOfLIS([1, 3, 5, 4, 7]) == 4
+    assert lengthOfLIS([2, 2, 2, 2, 2]) == 1
