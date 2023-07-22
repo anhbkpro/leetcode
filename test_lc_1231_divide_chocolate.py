ed3afc5e56e7373d64bcaa410c1ac4adb3d965ab
Anh Lai <laingocanh1990@gmail.com>
2023-07-22 14:30:09 +0700

    test 1231. Divide Chocolate

--- test_lc_1231_divide_chocolate.py
+++ test_lc_1231_divide_chocolate.py
@@ -1,7 +1,7 @@
-from lc_1231_divide_chocolate import maximizeSweetness
+from lc_1231_divide_chocolate import maximize_sweetness
 
 
 def test_maximize_sweetness():
-    assert maximizeSweetness(sweetness=[1, 2, 3, 4, 5, 6, 7, 8, 9], k=5) == 6
-    assert maximizeSweetness(sweetness=[5, 6, 7, 8, 9, 1, 2, 3, 4], k=8) == 1
-    assert maximizeSweetness(sweetness=[1, 2, 2, 1, 2, 2, 1, 2, 2], k=2) == 5
+    assert maximize_sweetness(sweetness=[1, 2, 3, 4, 5, 6, 7, 8, 9], k=5) == 6
+    assert maximize_sweetness(sweetness=[5, 6, 7, 8, 9, 1, 2, 3, 4], k=8) == 1
+    assert maximize_sweetness(sweetness=[1, 2, 2, 1, 2, 2, 1, 2, 2], k=2) == 5
