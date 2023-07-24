ced10ba08a6f14351764cc3f8484cc44c1547af5
Anh Lai <laingocanh1990@gmail.com>
2023-07-24 10:24:34 +0700

    57. Insert Interval - Binary Search

---
+++ lc_57_insert_interval.py
@@ -0,0 +1,20 @@
+from bisect import bisect
+
+
+class Solution:
+    @staticmethod
+    def insert(intervals, newInterval):
+        # O(logN)
+        position = bisect(intervals, newInterval)
+        # O(N)
+        intervals.insert(position, newInterval)
+
+        answer = []
+        # O(N)
+        for i in range(len(intervals)):
+            if not answer or intervals[i][0] > answer[-1][1]:
+                answer.append(intervals[i])
+            else:
+                answer[-1][1] = max(answer[-1][1], intervals[i][1])
+
+        return answer
