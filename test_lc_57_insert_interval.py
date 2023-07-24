849f3ce1a88fbd3078482621fbaabee58c41fc9c
Anh Lai <laingocanh1990@gmail.com>
2023-07-24 10:24:45 +0700

    test 57. Insert Interval - Binary Search

---
+++ test_lc_57_insert_interval.py
@@ -0,0 +1,8 @@
+from lc_57_insert_interval import Solution
+
+
+def test_insert():
+    assert Solution.insert(intervals=[[1, 3], [6, 9]], newInterval=[2, 5]) == [[1, 5], [6, 9]]
+    assert Solution.insert(intervals=[[1, 2], [3, 5], [6, 7], [8, 10], [12, 16]], newInterval=[4, 8]) == [[1, 2],
+                                                                                                          [3, 10],
+                                                                                                          [12, 16]]
