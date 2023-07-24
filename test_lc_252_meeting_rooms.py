05147ed8bfef22ac6001fe7eb10d5ed45af84530
Anh Lai <laingocanh1990@gmail.com>
2023-07-24 10:07:03 +0700

    252. Meeting Rooms

--- test_lc_252_meeting_rooms.py
+++ test_lc_252_meeting_rooms.py
@@ -1,6 +1,6 @@
-from lc_252_meeting_rooms import canAttendMeetings
+from lc_252_meeting_rooms import can_attend_meetings
 
 
 def test_can_attend_meetings():
-    assert canAttendMeetings([[0, 30], [5, 10], [15, 20]]) is False
-    assert canAttendMeetings([[7, 10], [2, 4]]) is True
+    assert can_attend_meetings([[0, 30], [5, 10], [15, 20]]) is False
+    assert can_attend_meetings([[7, 10], [2, 4]]) is True
