b560ecb750f3cf5eb2d2559a5c335c5d499165f8
Anh Lai <laingocanh1990@gmail.com>
2023-07-24 21:45:38 +0700

    test 253. Meeting Rooms II - Approach 1: Priority Queues

--- test_lc_253_meeting_rooms_ii.py
+++ test_lc_253_meeting_rooms_ii.py
@@ -1,6 +1,6 @@
-from lc_253_meeting_rooms_ii import minMeetingRooms
+from lc_253_meeting_rooms_ii import min_meeting_rooms
 
 
 def test_min_meeting_rooms():
-    assert minMeetingRooms([[0, 30], [5, 10], [15, 20]]) == 2
-    assert minMeetingRooms([[7, 10], [2, 4]]) == 1
+    assert min_meeting_rooms([[0, 30], [5, 10], [15, 20]]) == 2
+    assert min_meeting_rooms([[7, 10], [2, 4]]) == 1
