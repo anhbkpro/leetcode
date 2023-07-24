b028faa65885eecdf5e99151c2c390c0b5b5cb3b
Anh Lai <laingocanh1990@gmail.com>
2023-07-24 21:35:57 +0700

    236. Lowest Common Ancestor of a Binary Tree

---
+++ test_lc_236_lowest_common_ancestor_of_a_binary_tree.py
@@ -0,0 +1,19 @@
+from lc_236_lowest_common_ancestor_of_a_binary_tree import Solution, TreeNode
+
+
+def test_lowest_common_ancestor():
+    s = Solution()
+
+    root = TreeNode(3)
+    root.left = TreeNode(5)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(6)
+    root.left.right = TreeNode(2)
+    root.right.left = TreeNode(0)
+    root.right.right = TreeNode(8)
+    root.left.right.left = TreeNode(7)
+    root.left.right.right = TreeNode(4)
+    p = root.left
+    q = root.right
+
+    assert s.lowestCommonAncestor(root=root, p=p, q=q).val == 3
