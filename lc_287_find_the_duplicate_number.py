59c043d363cc9403c1f1f1ba2b9dfa2f39fb4a5e
Anh Lai <laingocanh1990@gmail.com>
2023-07-27 21:33:54 +0700

    287. Find the Duplicate Number - Approach 7: Floyd's Tortoise and Hare (Cycle Detection)

--- lc_287_find_the_duplicate_number.py
+++ lc_287_find_the_duplicate_number.py
@@ -9,3 +9,19 @@ class Solution:
             if item in seen:
                 return item
             seen.add(item)
+
+    @staticmethod
+    def find_duplicate_using_floyd_tortoise_and_hare(nums: List[int]) -> int:
+        tortoise = hare = nums[0]
+        while True:
+            tortoise = nums[tortoise]
+            hare = nums[nums[hare]]
+            if tortoise == hare:
+                break
+
+        tortoise = nums[0]
+        while tortoise != hare:
+            tortoise = nums[tortoise]
+            hare = nums[hare]
+
+        return hare
