ecd20e8deb5c39ed7b2d248c60b67820dafcc1ea
Anh Lai <laingocanh1990@gmail.com>
2023-08-02 08:01:02 +0700

    77. Combinations - Approach: Backtracking

--- lc_77_combinations.py
+++ lc_77_combinations.py
@@ -9,16 +9,19 @@ class Solution:
                 ans.append(curr[:])
                 return
 
-            need = k - len(curr)
+            need = k - len(curr)  # the number of elements we still need to add.
             remain = n - first_num + 1
-            available = remain - need
+            available = remain - need  # the amount of numbers we can consider at the current node.
 
             for num in range(first_num, first_num + available + 1):
-                curr.append(num)
+                curr.append(
+                    num)  # Modifying curr and making a recursive call is equivalent to "traversing" to a child node
+                # in the tree.
                 backtrack(curr, num + 1)
-                curr.pop()
+                curr.pop()  # "backtracking" step
 
-            return
+            return  # When we return from a function call, it's equivalent to moving back up the tree (exactly like
+            # in a DFS)
 
         ans = []
         backtrack([], 1)
