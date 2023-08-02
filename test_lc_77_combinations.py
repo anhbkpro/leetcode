dee452526b75f818079ddd7cdb8f445504be41ce
Anh Lai <laingocanh1990@gmail.com>
2023-08-02 08:01:12 +0700

    test 77. Combinations - Approach: Backtracking

--- test_lc_77_combinations.py
+++ test_lc_77_combinations.py
@@ -2,6 +2,5 @@ from lc_77_combinations import Solution
 
 
 def test_combine():
-    assert Solution.combine(n=4, k=2) == [[1, 2], [1, 3], [1, 4], [2, 3], [2, 4],
-                                            [3, 4]]
+    assert Solution.combine(n=4, k=2) == [[1, 2], [1, 3], [1, 4], [2, 3], [2, 4], [3, 4]]
     assert Solution.combine(n=1, k=1) == [[1]]
