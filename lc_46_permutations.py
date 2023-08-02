0f6e8a4cc47839bf557feee5cae3aa559f630593
Anh Lai <laingocanh1990@gmail.com>
2023-08-02 20:29:47 +0700

    46. Permutations - Approach: Backtracking

---
+++ lc_46_permutations.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def permute(nums: List[int]) -> List[List[int]]:
+        def backtrack(curr):
+            if len(curr) == len(nums):
+                ans.append(curr[:])
+                return
+
+            for num in nums:
+                if num not in curr:
+                    curr.append(num)
+                    backtrack(curr)
+                    curr.pop()
+
+        ans = []
+        backtrack([])
+        return ans
