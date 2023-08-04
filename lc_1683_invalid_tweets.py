e35643c98a0a606916789a0c096b3e97831db3e4
Anh Lai <laingocanh1990@gmail.com>
2023-08-04 08:09:25 +0700

    [30 Days of Pandas] 1683. Invalid Tweets

--- lc_1683_invalid_tweets.py
+++ lc_1683_invalid_tweets.py
@@ -8,3 +8,10 @@ def invalid_tweets(tweets: pd.DataFrame) -> pd.DataFrame:
     df = tweets[tweets['content'].str.len() > 15]
     df = df[['tweet_id']]
     return df
+
+
+# LC solution:
+def invalid_tweets_lc(tweets: pd.DataFrame) -> pd.DataFrame:
+    is_valid = tweets['content'].str.len() > 15
+    df = tweets[is_valid]
+    return df[['tweet_id']]
