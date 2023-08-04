845b09e0ba54e1edf76467919d3a2bb8bc745659
Anh Lai <laingocanh1990@gmail.com>
2023-08-04 07:47:52 +0700

    [30 Days of Pandas] 1148. Article Views I - Approach: Selecting rows based on conditions

--- lc_1148_article_views_i.py
+++ lc_1148_article_views_i.py
@@ -4,7 +4,21 @@ import pandas as pd
 # Pandas schema:
 # Views = pd.DataFrame([], columns=['article_id', 'author_id', 'viewer_id', 'view_date']).astype({
 # 'article_id':'Int64', 'author_id':'Int64', 'viewer_id':'Int64', 'view_date':'datetime64[ns]'})
+# My solution:
 def article_views(views: pd.DataFrame) -> pd.DataFrame:
     df = views[views["author_id"] == views["viewer_id"]]
     df = df[["author_id"]].drop_duplicates().rename(columns={"author_id": "id"}).sort_values("id")
+    return df
+
+
+# LC solution:
+def article_views_lc(views: pd.DataFrame) -> pd.DataFrame:
+    df = views[views['author_id'] == views['viewer_id']]
+
+    df.drop_duplicates(subset=['author_id'], inplace=True)
+    df.sort_values(by=['author_id'], inplace=True)
+    df.rename(columns={'author_id': 'id'}, inplace=True)
+
+    df = df[['id']]
+
     return df
