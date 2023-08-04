1abd8a0c6d1111a6828af8a419fc354c370bd6b0
Anh Lai <laingocanh1990@gmail.com>
2023-08-04 12:24:03 +0700

    [30 Days of Pandas] 1873. Calculate Special Bonus

--- lc_1873_calculate_special_bonus.py
+++ lc_1873_calculate_special_bonus.py
@@ -5,9 +5,10 @@ import pandas as pd
 # Employees = pd.DataFrame([], columns=['employee_id', 'name', 'salary'])
 # .astype({'employee_id':'int64', 'name':'object', 'salary':'int64'})
 def calculate_special_bonus(employees: pd.DataFrame) -> pd.DataFrame:
-    employees["bonus"] = employees.apply(
+    employees['bonus'] = employees.apply(
         lambda x: x['salary'] if x['employee_id'] % 2 and not x['name'].startswith('M') else 0,
         axis=1
     )
-    employees.sort_values(by=['employee_id'], inplace=True)
-    return employees[['employee_id', 'bonus']]
+
+    df = employees[['employee_id', 'bonus']].sort_values('employee_id')
+    return df
