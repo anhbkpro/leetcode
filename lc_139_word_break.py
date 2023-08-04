8490de0724713fee390520b1eb3bd86a6f1a0fb2
Anh Lai <laingocanh1990@gmail.com>
2023-08-04 21:14:11 +0700

    139. Word Break - Approach 3: Bottom-Up Dynamic Programming

---
+++ lc_139_word_break.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def wordBreak(s: str, wordDict: List[str]) -> bool:
+        dp = [False] * len(s)
+        for i in range(len(s)):
+            for word in wordDict:
+                # Handle out of bounds case
+                if i < len(word) - 1:
+                    continue
+
+                if i == len(word) - 1 or dp[i - len(word)]:
+                    if s[i - len(word) + 1:i + 1] == word:
+                        dp[i] = True
+                        break
+
+        return dp[-1]
