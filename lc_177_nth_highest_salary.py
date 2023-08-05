4af0a982155db9dfbdf8ed7a250bd9714b6c6986
Anh Lai <laingocanh1990@gmail.com>
2023-08-05 12:03:01 +0700

    [30 Days of Pandas] 177. Nth Highest Salary

--- lc_177_nth_highest_salary.py
+++ lc_177_nth_highest_salary.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 def nth_highest_salary(employee: pd.DataFrame, N: int) -> pd.DataFrame:
-    df = employee[["salary"]].drop_duplicates()
+    df = employee[['salary']].drop_duplicates()
     if len(df) < N:
         return pd.DataFrame({'getNthHighestSalary(2)': [None]})
-    return df.sort_values("salary", ascending=False).head(N).tail(1)
+    return df.sort_values('salary', ascending=False).head(N).tail(1)
