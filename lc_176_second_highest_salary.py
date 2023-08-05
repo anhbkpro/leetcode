4e07d575f049dbb22ead4402f9c3d56f26ce2f36
Anh Lai <laingocanh1990@gmail.com>
2023-08-05 12:02:53 +0700

    [30 Days of Pandas] 176. Second Highest Salary

--- lc_176_second_highest_salary.py
+++ lc_176_second_highest_salary.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 
@@ -8,3 +9,25 @@ def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
         return pd.DataFrame({'SecondHighestSalary': [None]})
     df.rename(columns={'salary': 'SecondHighestSalary'}, inplace=True)
     return df.sort_values('SecondHighestSalary', ascending=False).head(2).tail(1)
+
+
+# LC solution:
+def second_highest_salary_lc(employee: pd.DataFrame) -> pd.DataFrame:
+    # 1. drop any duplicate salaries.
+    employee = employee.drop_duplicates(["salary"])
+
+    # 2. If there are less than two unique salaries, return `np.NaN`.
+    if len(employee["salary"].unique()) < 2:
+        return pd.DataFrame({"SecondHighestSalary": [np.NaN]})
+
+    # 3. Sort the table by `salary` in descending order.
+    employee = employee.sort_values("salary", ascending=False)
+
+    # 4. Drop the `id` column.
+    employee.drop("id", axis=1, inplace=True)
+
+    # 5. Rename the `salary` column.
+    employee.rename({"salary": "SecondHighestSalary"}, axis=1, inplace=True)
+
+    # 6, 7. Return the second-highest salary.
+    return employee.head(2).tail(1)
