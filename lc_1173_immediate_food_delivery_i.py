c56255c2750e52e7762136b765e16c546533c9e0
Anh Lai <laingocanh1990@gmail.com>
2023-08-07 21:44:11 +0700

    [30 Days of Pandas] 1173. Immediate Food Delivery I - LC Solution

--- lc_1173_immediate_food_delivery_i.py
+++ lc_1173_immediate_food_delivery_i.py
@@ -5,3 +5,18 @@ def food_delivery(delivery: pd.DataFrame) -> pd.DataFrame:
     immediate_orders = len(delivery[delivery['order_date'] == delivery['customer_pref_delivery_date']])
     total = len(delivery)
     return pd.DataFrame({'immediate_percentage': [round(immediate_orders / total * 100, 2)]})
+
+
+# LC solution
+def food_delivery_lc(delivery: pd.DataFrame) -> pd.DataFrame:
+    is_valid = delivery['order_date'] == delivery['customer_pref_delivery_date']
+
+    # Count the number of valid (immediate) orders and the number of all orders.
+    valid_count = is_valid.sum()
+    total_count = len(delivery)
+
+    # Round the percentage to 2 decimal places.
+    percentage = round(100 * valid_count / total_count, 2)
+
+    df = pd.DataFrame({'immediate_percentage': [percentage]})
+    return df
