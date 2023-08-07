f860c2f42ebe530614124ab7145dff52426eb5ab
Anh Lai <laingocanh1990@gmail.com>
2023-08-07 08:16:02 +0700

    74. Search a 2D Matrix

--- lc_74_search_a_2d_matrix.py
+++ lc_74_search_a_2d_matrix.py
@@ -1,9 +1,10 @@
 from typing import List
 
 
+# My Solution
 class Solution:
     @staticmethod
-    def searchMatrix(matrix: List[List[int]], target: int) -> bool:
+    def search_matrix(matrix: List[List[int]], target: int) -> bool:
         last_cols = []
         for r in matrix:
             last_cols.append(r[-1])
@@ -23,6 +24,27 @@ class Solution:
             else:
                 hi = mid - 1
 
+        return False
+
+    @staticmethod
+    def search_matrix_lc(matrix: List[List[int]], target: int) -> bool:
+        m = len(matrix)
+        if m == 0:
+            return False
+        n = len(matrix[0])
+
+        # binary search
+        left, right = 0, m * n - 1
+        while left <= right:
+            pivot_idx = (left + right) // 2
+            pivot_element = matrix[pivot_idx // n][pivot_idx % n]
+            if target == pivot_element:
+                return True
+            else:
+                if target < pivot_element:
+                    right = pivot_idx - 1
+                else:
+                    left = pivot_idx + 1
         return False
 
 
