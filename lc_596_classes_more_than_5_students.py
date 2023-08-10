6dfd517fe8925ea4ec554edca261f6512cc8afcc
Anh Lai <laingocanh1990@gmail.com>
2023-08-10 18:05:39 +0700

    [30 Days of Pandas] 596. Classes More Than 5 Students

--- lc_596_classes_more_than_5_students.py
+++ lc_596_classes_more_than_5_students.py
@@ -5,3 +5,12 @@ import pandas as pd
 def find_classes(courses: pd.DataFrame) -> pd.DataFrame:
     df = courses.groupby('class')['student'].count().reset_index()
     return df[df['student'] >= 5].drop(['student'], axis=1)
+
+
+# LC solution
+def find_classes_lc(courses: pd.DataFrame) -> pd.DataFrame:
+    df = courses.groupby('class').size().reset_index(name='count')
+
+    df = df[df['count'] >= 5]
+
+    return df[['class']]
