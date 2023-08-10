590be1b6a0508786f95fb3e2bf2d5838451ac619
Anh Lai <laingocanh1990@gmail.com>
2023-08-10 18:26:44 +0700

    [30 Days of Pandas] 586. Customer Placing the Largest Number of Orders

--- lc_586_customer_placing_the_largest_number_of_orders.py
+++ lc_586_customer_placing_the_largest_number_of_orders.py
@@ -6,3 +6,14 @@ def largest_orders(orders: pd.DataFrame) -> pd.DataFrame:
     df = orders.groupby('customer_number')['order_number'].nunique().reset_index()
     df = df.sort_values(by=['order_number'])
     return df[['customer_number']].tail(1)
+
+
+# LC solution
+def largest_orders_lc(orders: pd.DataFrame) -> pd.DataFrame:
+    # If orders is empty, return an empty DataFrame.
+    if orders.empty:
+        return pd.DataFrame({'customer_number': []})
+
+    df = orders.groupby('customer_number').size().reset_index(name='count')
+    df.sort_values(by='count', ascending=False, inplace=True)
+    return df[['customer_number']][0:1]
