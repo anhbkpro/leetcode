911b294f3b73a0c117b0d18234e9fd5ee209822d
Anh Lai <laingocanh1990@gmail.com>
2023-08-11 08:10:42 +0700

    [30 Days of Pandas] 1693. Daily Leads and Partners

---
+++ lc_1693_daily_leads_and_partners.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+
+def daily_leads_and_partners(daily_sales: pd.DataFrame) -> pd.DataFrame:
+    groups = daily_sales.groupby(['date_id', 'make_name'])
+    stats = groups.agg(
+        unique_leads=('lead_id', 'nunique'),
+        unique_partners=('partner_id', 'nunique'),
+    ).reset_index()
+
+    return stats
