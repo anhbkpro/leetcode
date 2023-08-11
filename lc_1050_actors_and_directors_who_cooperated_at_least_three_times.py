596e90313ed6c9c68ba6c51c0f4e5dc2dba34873
Anh Lai <laingocanh1990@gmail.com>
2023-08-11 16:10:06 +0700

    [30 Days of Pandas] 1050. Actors and Directors Who Cooperated At Least Three Times

---
+++ lc_1050_actors_and_directors_who_cooperated_at_least_three_times.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+
+# My solution
+def actors_and_directors(actor_director: pd.DataFrame) -> pd.DataFrame:
+    df = actor_director.groupby(['actor_id', 'director_id'])['timestamp'].size().reset_index()
+    return df[df['timestamp'] >= 3].drop('timestamp', axis=1)
