005401b0ebe94f53f5e2a0d8694ec8584e6d8a1b
Anh Lai <laingocanh1990@gmail.com>
2023-08-12 10:58:08 +0700

    [30 Days of Pandas] 570. Managers with at Least 5 Direct Reports

--- lc_570_managers_with_at_least_5_direct_reports.py
+++ lc_570_managers_with_at_least_5_direct_reports.py
@@ -8,3 +8,11 @@ def find_managers(employee: pd.DataFrame) -> pd.DataFrame:
     grouped = grouped[grouped['count'] >= 5]
     employee = employee[employee['id'].isin(grouped['managerId'])]
     return employee[['name']]
+
+
+# LC solution
+def find_managers_lc(employee: pd.DataFrame) -> pd.DataFrame:
+    df = employee.groupby('managerId').size().reset_index(name='count')
+    df = df[df['count'] >= 5]
+    managers_info = pd.merge(df, employee, left_on='managerId', right_on='id', how='inner')
+    return managers_info[['name']]
