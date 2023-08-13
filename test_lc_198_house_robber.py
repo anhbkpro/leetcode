0f931f3dc3a0931d3d2bae87b1c82e690623b49c
Anh Lai <laingocanh1990@gmail.com>
2023-08-13 08:58:38 +0700

    198. House Robber - Approach 3: Optimized Dynamic Programming

--- test_lc_198_house_robber.py
+++ test_lc_198_house_robber.py
@@ -1,6 +1,6 @@
-from lc_198_house_robber import rob
+from lc_198_house_robber import Solution
 
 
 def test_rob():
-    assert rob(nums=[1, 2, 3, 1]) == 4
-    assert rob(nums=[2, 7, 9, 3, 1]) == 12
+    assert Solution.rob(nums=[1, 2, 3, 1]) == 4
+    assert Solution.rob(nums=[2, 7, 9, 3, 1]) == 12
