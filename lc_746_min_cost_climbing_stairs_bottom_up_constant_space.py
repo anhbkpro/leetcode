ec6a8fc321b985e5c8b114aab5ca418127871356
Anh Lai <laingocanh1990@gmail.com>
2023-08-13 08:51:44 +0700

    746. Min Cost Climbing Stairs - Approach 3: Bottom-Up, Constant Space

---
+++ lc_746_min_cost_climbing_stairs_bottom_up_constant_space.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def minCostClimbingStairs(cost: List[int]) -> int:
+        down_one = down_two = 0
+        for i in range(2, len(cost) + 1):
+            temp = down_one
+            down_one = min(down_one + cost[i - 1], down_two + cost[i - 2])
+            down_two = temp
+
+        return down_one
