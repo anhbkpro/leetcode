ee7883d9f5fb11e117040b87cfe91f20150a8f47
Anh Lai <laingocanh1990@gmail.com>
2023-08-13 08:39:19 +0700

    1137. N-th Tribonacci Number - DP

--- lc_1137_n_th_tribonacci_number.py
+++ lc_1137_n_th_tribonacci_number.py
@@ -1,11 +1,9 @@
-def tribonacci(n: int) -> int:
-    if n < 3:
-        return 1 if n > 0 else 0
-    a, b, c = 0, 1, 1
-    for i in range(n-2):
-        a, b, c = b, c, a + b + c
-    return c
-
-
 class Solution:
-    pass
+    @staticmethod
+    def tribonacci(n: int) -> int:
+        if n < 3:
+            return 1 if n > 0 else 0
+        a, b, c = 0, 1, 1
+        for i in range(n-2):
+            a, b, c = b, c, a + b + c
+        return c
