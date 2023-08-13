e1948f08b1cbd97c87cdc8a676ef18ee81d8323b
Anh Lai <laingocanh1990@gmail.com>
2023-08-13 08:58:29 +0700

    198. House Robber - Approach 3: Optimized Dynamic Programming

--- lc_198_house_robber.py
+++ lc_198_house_robber.py
@@ -1,28 +1,27 @@
 from typing import List
 
 
-def rob(nums: List[int]) -> int:
-
-    # Special handling for empty case.
-    if not nums:
-        return 0
-
-    max_robbed_amount = [None for _ in range(len(nums) + 1)]
-    n = len(nums)
-    # Base case initialization.
-    max_robbed_amount[n], max_robbed_amount[n - 1] = 0, nums[n - 1]
-
-    # DP table calculations.
-    for i in range(n - 2, -1, -1):
-        print(i)
-        # Same as recursive solution.
-        # option 1: do nothing
-        # option 2: rob house i and i + 2
-        max_robbed_amount[i] = max(max_robbed_amount[i + 1], max_robbed_amount[i + 2] + nums[i])
-        print(f"maxRobbedAmount[{i}]={max_robbed_amount[i]}")
-
-    return max_robbed_amount[0]
-
-
 class Solution:
-    pass
+
+    @staticmethod
+    def rob(nums: List[int]) -> int:
+
+        # Special handling for empty case.
+        if not nums:
+            return 0
+
+        n = len(nums)
+
+        rob_next_plus_one = 0
+        rob_next = nums[n - 1]
+
+        # DP table calculations.
+        for i in range(n - 2, -1, -1):
+            # Same as recursive solution.
+            current = max(rob_next, rob_next_plus_one + nums[i])
+
+            # Update the variables
+            rob_next_plus_one = rob_next
+            rob_next = current
+
+        return rob_next
