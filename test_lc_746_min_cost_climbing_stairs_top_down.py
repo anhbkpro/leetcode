989c3a682029afbc9cb56f9aacdb5e3bd03a76d2
Anh Lai <laingocanh1990@gmail.com>
2023-08-13 08:47:39 +0700

    746. Min Cost Climbing Stairs - Approach 2: Top-Down Dynamic Programming (Recursion + Memoization)

---
+++ test_lc_746_min_cost_climbing_stairs_top_down.py
@@ -0,0 +1,6 @@
+from lc_746_min_cost_climbing_stairs_top_down import Solution
+
+
+def test_min_cost_climbing_stairs():
+    assert Solution.minCostClimbingStairs(cost=[10, 15, 20]) == 15
+    assert Solution.minCostClimbingStairs(cost=[1, 100, 1, 1, 1, 100, 1, 1, 100, 1]) == 6
