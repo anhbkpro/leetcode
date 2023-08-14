f915d866554a20fd7920879750278710347ad34b
Anh Lai <laingocanh1990@gmail.com>
2023-08-14 07:56:16 +0700

    215. Kth Largest Element in an Array - Heap

--- test_lc_215_kth_largest_element_in_an_array.py
+++ test_lc_215_kth_largest_element_in_an_array.py
@@ -1,5 +1,5 @@
-from lc_215_kth_largest_element_in_an_array import findKthLargest
+from lc_215_kth_largest_element_in_an_array import Solution
 
 
 def test_find_kth_largest():
-    assert findKthLargest(nums=[3, 2, 1, 5, 6, 4], k=2) == 5
+    assert Solution.findKthLargest(nums=[3, 2, 1, 5, 6, 4], k=2) == 5
