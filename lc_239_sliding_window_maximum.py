9706a9ddc28f46625c2a2bc7036d28939860bb11
Anh Lai <laingocanh1990@gmail.com>
2023-08-16 21:24:44 +0700

    239. Sliding Window Maximum - Approach: Monotonic Deque

--- lc_239_sliding_window_maximum.py
+++ lc_239_sliding_window_maximum.py
@@ -16,10 +16,13 @@ class Solution:
         res.append(nums[dq[0]])
 
         for i in range(k, len(nums)):
+            print(f"=== Current index: {i}")
             if dq and dq[0] == i - k:
+                print(f"\nPop out the first index {dq[0]} as its not in the range of current window.")
                 dq.popleft()
             while dq and nums[i] >= nums[dq[-1]]:
-                dq.pop()
+                rightmost_element = dq.pop()
+                print(f"Pop index {rightmost_element} as they have smaller corresponding.")
 
             dq.append(i)
             res.append(nums[dq[0]])
