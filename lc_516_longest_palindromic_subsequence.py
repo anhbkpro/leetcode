90bdc8e82fbf609ca7ffaddfc02c7048e69b5a2c
Anh Lai <laingocanh1990@gmail.com>
2023-08-17 05:45:32 +0700

    we can use map instead of 2D array for memo

--- lc_516_longest_palindromic_subsequence.py
+++ lc_516_longest_palindromic_subsequence.py
@@ -2,20 +2,20 @@ class Solution:
     @staticmethod
     def longest_palindrome_subseq(s: str) -> int:
         n = len(s)
-        memo = [[0] * n for _ in range(n)]
+        memo = {}
 
         def lps(i: int, j: int) -> int:
-            if memo[i][j] != 0:
-                return memo[i][j]
+            if (i, j) in memo:
+                return memo[(i, j)]
             if i > j:
                 return 0
             if i == j:
                 return 1
             if s[i] == s[j]:
-                memo[i][j] = 2 + lps(i + 1, j - 1)
+                memo[(i, j)] = 2 + lps(i + 1, j - 1)
             else:
-                memo[i][j] = max(lps(i, j - 1), lps(i + 1, j))
+                memo[(i, j)] = max(lps(i, j - 1), lps(i + 1, j))
 
-            return memo[i][j]
+            return memo[(i, j)]
 
         return lps(0, n - 1)
