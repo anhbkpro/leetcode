caf0ef4ef054497d87eb922c608b15cfa34222cb
Anh Lai <laingocanh1990@gmail.com>
2023-08-17 17:42:24 +0700

    72. Edit Distance - Approach 3: Bottom-Up Dynamic Programming: Tabulation

--- test_lc_72_edit_distance.py
+++ test_lc_72_edit_distance.py
@@ -1,6 +1,7 @@
-from lc_72_edit_distance import min_distance
+from lc_72_edit_distance import Solution
 
 
 def test_my_min_distance():
-    assert min_distance(word1="horse", word2="ros") == 3
-    assert min_distance(word1="intention", word2="execution") == 5
+    assert Solution.min_distance(word1="horse", word2="ros") == 3
+    assert Solution.min_distance(word1="intention", word2="execution") == 5
+    assert Solution.min_distance(word1="a", word2="ab") == 1
