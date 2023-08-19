61b51fba82c83d794f6815a99bea600c1fad1981
Anh Lai <laingocanh1990@gmail.com>
2023-08-19 14:39:48 +0700

    add more comment to explain Disjoin Set

--- lc_1135_connecting_cities_with_minimum_cost.py
+++ lc_1135_connecting_cities_with_minimum_cost.py
@@ -12,9 +12,14 @@ class DisjoinSet:
         :param a: node a
         :return: root of a node
         """
+        # path compression
+        print(f"find root of {a}: ")
+        tmp = a
         while a != self.parents[a]:
+            print(f"continue find root of {a} = {self.parents[a]}")
             a = self.parents[a]
 
+        print(f"===> root of {tmp} is {a}")
         return a
 
     def union(self, a, b):
@@ -27,12 +32,18 @@ class DisjoinSet:
         root_a = self.find(a)
         root_b = self.find(b)
 
+        # if two nodes are already in the same group, skip
         if root_a == root_b:
+            print(f"{a} and {b} are already in the same group")
             return
 
+        # otherwise, union two nodes
+        # union by rank
+        print(f"union {a} and {b}, so {root_a} and {root_b} are in the same group = {root_a}")
         self.parents[root_b] = root_a
 
     def is_in_same_group(self, a, b):
+        # if two nodes are in the same group, they have the same root
         return self.find(a) == self.find(b)
 
 
@@ -47,10 +58,13 @@ class Solution:
         # total edges
         total_edges = 0
         for conn in connections:
+            print(f"1. Consider {conn[0]} and {conn[1]}")
             # if two nodes are already in the same group, skip
             if ds.is_in_same_group(conn[0], conn[1]):
+                print(f"2. {conn[0]} and {conn[1]} are already in the same group")
                 continue
             # otherwise, union two nodes
+            print(f"2. union {conn[0]} and {conn[1]}")
             ds.union(conn[0], conn[1])
             # add cost
             total_cost += conn[2]
