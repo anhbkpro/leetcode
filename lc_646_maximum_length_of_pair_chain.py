12a38f8e8803fb0ce19ba7027ea6daf581239824
Anh Lai <laingocanh1990@gmail.com>
2023-08-26 21:24:53 +0700

    646. Maximum Length of Pair Chain - Approach 2: Iterative Dynamic Programming

---
+++ lc_646_maximum_length_of_pair_chain.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def findLongestChain(pairs: List[List[int]]) -> int:
+        n = len(pairs)
+        pairs.sort()
+        dp = [1] * n
+        ans = 1
+
+        for i in range(n - 1, -1, -1):
+            for j in range(i + 1, n):
+                if pairs[i][1] < pairs[j][0]:
+                    dp[i] = max(dp[i], 1 + dp[j])
+            ans = max(ans, dp[i])
+        return ans
