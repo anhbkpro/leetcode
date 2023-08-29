db4ced8288cbcc2e667586aefbf1f6ce748b9d86
Anh Lai <laingocanh1990@gmail.com>
2023-08-29 22:00:56 +0700

    2483. Minimum Penalty for a Shop - Approach 1: Two Passes

---
+++ lc_2483_minimum_penalty_for_a_shopping_trip.py
@@ -0,0 +1,22 @@
+class Solution:
+    @staticmethod
+    def bestClosingTime(customers: str) -> int:
+        # Start with closing at hour 0, the penalty equals all 'Y' in closed hours.
+        cur_penalty = min_penalty = customers.count('Y')
+        earliest_hour = 0
+
+        for i, ch in enumerate(customers):
+            # If status in hour i is 'Y', moving it to open hours decrement
+            # penalty by 1. Otherwise, moving 'N' to open hours increment
+            # penalty by 1.
+            if ch == 'Y':
+                cur_penalty -= 1
+            else:
+                cur_penalty += 1
+
+            # Update earliest_hour if a smaller penalty is encountered
+            if cur_penalty < min_penalty:
+                earliest_hour = i + 1
+                min_penalty = cur_penalty
+
+        return earliest_hour
