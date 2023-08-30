aecddecfb56e5c8f6edf8e57e4862ce46719908e
Anh Lai <laingocanh1990@gmail.com>
2023-08-30 21:46:59 +0700

    2366. Minimum Replacements to Sort the Array - Approach: Greedy

---
+++ test_lc_2366_minimum_replacements_to_sort_the_array.py
@@ -0,0 +1,6 @@
+from lc_2366_minimum_replacements_to_sort_the_array import Solution
+
+
+def test_minimum_replacement():
+    assert Solution.minimumReplacement([3, 9, 3]) == 2
+    assert Solution.minimumReplacement([1, 2, 3, 4, 5]) == 0
