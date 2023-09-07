5398f7c594f2210019fe51216818afd72c35a4f0
Anh Lai <laingocanh1990@gmail.com>
2023-09-07 21:56:21 +0700

    how to test 2666. Allow One Function Call

--- lc_2666_allow_one_function_call_per_time_unit.js
+++ lc_2666_allow_one_function_call_per_time_unit.js
@@ -17,3 +17,6 @@ let onceFn = once(fn)
 
 console.log(onceFn(1,2,3)); // 6
 console.log(onceFn(2,3,6)); // returns `undefined` without calling fn
+
+// How to test:
+// 1. Run this file with `node lc_2666_allow_one_function_call_per_time_unit.js`
