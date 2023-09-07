23e1c4272b656071cdae9778de0f5bc3105c09aa
Anh Lai <laingocanh1990@gmail.com>
2023-09-07 21:44:30 +0700

    test 2666. Allow One Function Call

--- lc_2666_allow_one_function_call_per_time_unit.js
+++ lc_2666_allow_one_function_call_per_time_unit.js
@@ -12,10 +12,8 @@ var once = function(fn) {
   }
 };
 
-/**
- * let fn = (a,b,c) => (a + b + c)
- * let onceFn = once(fn)
- *
- * onceFn(1,2,3); // 6
- * onceFn(2,3,6); // returns undefined without calling fn
- */
+let fn = (a,b,c) => (a + b + c)
+let onceFn = once(fn)
+
+console.log(onceFn(1,2,3)); // 6
+console.log(onceFn(2,3,6)); // returns `undefined` without calling fn
