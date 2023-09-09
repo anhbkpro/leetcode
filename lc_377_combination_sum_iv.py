e347938692ba3fa4a105aff1f89609210da243f8
Anh Lai <laingocanh1990@gmail.com>
2023-09-09 11:42:12 +0700

    377. Combination Sum IV

---
+++ lc_377_combination_sum_iv.py
@@ -0,0 +1,26 @@
+import functools
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def combinationSum4(nums: List[int], target: int) -> int:
+        # potential optimization
+        # nums.sort()
+
+        @functools.lru_cache(maxsize = None)
+        def combs(remain):
+            if remain == 0:
+                return 1
+
+            result = 0
+            for num in nums:
+                if remain - num >= 0:
+                    result += combs(remain - num)
+                # potential optimization
+                # else:
+                #     break
+
+            return result
+
+        return combs(target)
