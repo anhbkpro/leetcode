acaf688d71a4308baf69ff3f7ae075b8a8c92a93
Anh Lai <laingocanh1990@gmail.com>
2023-09-09 11:42:56 +0700

    377. Combination Sum IV - Approach 1: Top-Down Dynamic Programming

---
+++ test_lc_377_combination_sum_iv.py
@@ -0,0 +1,6 @@
+from lc_377_combination_sum_iv import Solution
+
+
+def test_combination_sum4():
+    assert Solution.combinationSum4([1, 2, 3], 4) == 7
+    assert Solution.combinationSum4([9], 3) == 0
