e61f0ffa3b3344aeec1172b926d107d97d04ca2f
Anh Lai <laingocanh1990@gmail.com>
2023-09-09 12:34:43 +0700

    2627. Debounce - Approach 2: setInterval + clearInterval

--- lc_2627_debounce_function.js
+++ lc_2627_debounce_function.js
@@ -3,7 +3,7 @@
  * @param {number} t milliseconds
  * @return {Function}
  */
-var debounce = function(fn, t) {
+const debounce = function(fn, t) {
     let interval;
     return function(...args) {
         const lastCall = Date.now();
