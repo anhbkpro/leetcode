0f232440eb4a0f226fc8cab3aa06869d298150ec
Anh Lai <laingocanh1990@gmail.com>
2023-09-09 12:32:45 +0700

    2627. Debounce

---
+++ lc_2627_debounce_function.js
@@ -0,0 +1,33 @@
+/**
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+var debounce = function(fn, t) {
+    let interval;
+    return function(...args) {
+        const lastCall = Date.now();
+        clearInterval(interval);
+        interval = setInterval(() => {
+            if (Date.now() - lastCall >= t) {
+                clearInterval(interval);
+                fn(...args);
+            }
+        }, 1);
+    }
+};
+
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ */
+
+const log = debounce(console.log, 100);
+log('Hello'); // cancelled
+log('Hello'); // cancelled
+log('Hello'); // Logged at t=100ms
+
+// How to test:
+// 1. Run this file with `node lc_2627_debounce_function.js`
