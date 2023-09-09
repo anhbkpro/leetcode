cb746d2e2e54f52a5330b3e1cf578a03c3874dd8
Anh Lai <laingocanh1990@gmail.com>
2023-09-09 12:04:44 +0700

    test 2622. Cache With Time Limit

--- lc_2622_cache_with_time_limit.js
+++ lc_2622_cache_with_time_limit.js
@@ -57,3 +57,11 @@ TimeLimitedCache.prototype.count = function() {
  * obj.get(1) // 42
  * obj.count() // 1
  */
+
+const obj = new TimeLimitedCache();
+console.log(obj.set(1, 42, 1000)); // false
+console.log(obj.get(1)); // 42
+console.log(obj.count()); // 1
+
+// How to test:
+// 1. Run this file with `node lc_2622_cache_with_time_limit.js`
