e1619454cd1d836d5aeb7247dd5398b98ebafb88
Anh Lai <laingocanh1990@gmail.com>
2023-09-10 13:15:30 +0700

    344. Reverse String - Divide-and-Conquer Solution

--- test_lc_344_reverse_string.py
+++ test_lc_344_reverse_string.py
@@ -3,8 +3,17 @@ from lc_344_reverse_string import Solution
 
 def test_reverse_string():
     s = ["h", "e", "l", "l", "o"]
-    Solution.reverseString(s)
+    Solution.reverse_string(s)
     assert s == ["o", "l", "l", "e", "h"]
     s = ["H", "a", "n", "n", "a", "h"]
-    Solution.reverseString(s)
+    Solution.reverse_string(s)
+    assert s == ["h", "a", "n", "n", "a", "H"]
+
+
+def test_reverse_string_recursion():
+    s = ["h", "e", "l", "l", "o"]
+    Solution.reverse_string_recursion(s)
+    assert s == ["o", "l", "l", "e", "h"]
+    s = ["H", "a", "n", "n", "a", "h"]
+    Solution.reverse_string_recursion(s)
     assert s == ["h", "a", "n", "n", "a", "H"]
