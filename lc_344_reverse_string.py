4528ad1368d4bb5101a79b1de322ded081af0e2b
Anh Lai <laingocanh1990@gmail.com>
2023-09-10 13:15:23 +0700

    344. Reverse String - Divide-and-Conquer Solution

--- lc_344_reverse_string.py
+++ lc_344_reverse_string.py
@@ -1,7 +1,16 @@
 class Solution:
     @staticmethod
-    def reverseString(s):
+    def reverse_string(s):
         left, right = 0, len(s) - 1
         while left < right:
             s[left], s[right] = s[right], s[left]
             left, right = left + 1, right - 1
+
+    @staticmethod
+    def reverse_string_recursion(s):
+        def helper(left, right):
+            if left < right:
+                s[left], s[right] = s[right], s[left]
+                helper(left + 1, right - 1)
+
+        helper(0, len(s) - 1)
