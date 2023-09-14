42155fc6804d1174bcf9700f8ea7d5967c2f6c86
Anh Lai <laingocanh1990@gmail.com>
2023-09-14 08:52:33 +0700

    test 332. Reconstruct Itinerary - Backtracking + Greedy

---
+++ test_lc_332_reconstruct_itinerary.py
@@ -0,0 +1,13 @@
+from lc_332_reconstruct_itinerary import Solution
+
+solution = Solution()
+
+
+def test_find_itinerary():
+    assert solution.findItinerary(
+        [["MUC", "LHR"], ["JFK", "MUC"], ["SFO", "SJC"], ["LHR", "SFO"]]
+    ) == ["JFK", "MUC", "LHR", "SFO", "SJC"]
+    assert solution.findItinerary(
+        [["JFK", "SFO"], ["JFK", "ATL"], ["SFO", "ATL"], ["ATL", "JFK"],
+            ["ATL", "SFO"]]
+    ) == ["JFK", "ATL", "JFK", "SFO", "ATL", "SFO"]
