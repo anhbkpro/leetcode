20052b302aa84e59f27b161ab2b9e6c99acd294e
Anh Lai <laingocanh1990@gmail.com>
2023-09-14 11:10:28 +0700

    332. Reconstruct Itinerary

--- test_lc_332_reconstruct_itinerary.py
+++ test_lc_332_reconstruct_itinerary.py
@@ -4,10 +4,7 @@ solution = Solution()
 
 
 def test_find_itinerary():
-    assert solution.findItinerary(
-        [["MUC", "LHR"], ["JFK", "MUC"], ["SFO", "SJC"], ["LHR", "SFO"]]
-    ) == ["JFK", "MUC", "LHR", "SFO", "SJC"]
-    assert solution.findItinerary(
-        [["JFK", "SFO"], ["JFK", "ATL"], ["SFO", "ATL"], ["ATL", "JFK"],
-            ["ATL", "SFO"]]
-    ) == ["JFK", "ATL", "JFK", "SFO", "ATL", "SFO"]
+    assert solution.findItinerary([["MUC", "LHR"], ["JFK", "MUC"], ["SFO", "SJC"], ["LHR", "SFO"]]) \
+           == ["JFK", "MUC", "LHR", "SFO", "SJC"]
+    assert solution.findItinerary([["JFK", "SFO"], ["JFK", "ATL"], ["SFO", "ATL"], ["ATL", "JFK"], ["ATL", "SFO"]]) \
+           == ["JFK", "ATL", "JFK", "SFO", "ATL", "SFO"]
