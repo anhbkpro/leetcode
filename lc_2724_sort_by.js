e2e57c2e9539929fba1cffd451c0859a97bced34
Anh Lai <laingocanh1990@gmail.com>
2023-09-15 20:25:21 +0700

    2724. Sort By

--- lc_2724_sort_by.js
+++ lc_2724_sort_by.js
@@ -3,8 +3,8 @@
  * @param {Function} fn
  * @return {Array}
  */
-var sortBy = function(arr, fn) {
-    return arr.sort((a,b) => fn(a) - fn(b))
+let sortBy = function (arr, fn) {
+    return arr.sort((a, b) => fn(a) - fn(b))
 };
 
 // How to use:
