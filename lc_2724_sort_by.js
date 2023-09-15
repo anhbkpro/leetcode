ac2ab5f6c353961c6829446f75fe596ec856f3bb
Anh Lai <laingocanh1990@gmail.com>
2023-09-15 20:24:17 +0700

    2724. Sort By

---
+++ lc_2724_sort_by.js
@@ -0,0 +1,14 @@
+/**
+ * @param {Array} arr
+ * @param {Function} fn
+ * @return {Array}
+ */
+var sortBy = function(arr, fn) {
+    return arr.sort((a,b) => fn(a) - fn(b))
+};
+
+// How to use:
+arr = [{"x": 1}, {"x": 0}, {"x": -1}]
+sortBy(arr, o => o.x)
+console.log(arr) // [{"x": -1}, {"x": 0}, {"x": 1}]
+
