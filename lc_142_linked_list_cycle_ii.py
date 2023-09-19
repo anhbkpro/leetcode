340304beb77f0768af2074b68f5fdbdb78cee8d5
Anh Lai <laingocanh1990@gmail.com>
2023-09-19 20:20:42 +0700

    Approach 2: Floyd's Tortoise and Hare Algorithm

--- lc_142_linked_list_cycle_ii.py
+++ lc_142_linked_list_cycle_ii.py
@@ -22,3 +22,33 @@ class Solution:
 
         # If we reach a null node, there is no cycle
         return None
+
+    @staticmethod
+    def detect_cycle_tortoise_and_hare(head: Optional[ListNode]) -> Optional[ListNode]:
+        # Initialize tortoise and hare pointers
+        tortoise = head
+        hare = head
+
+        # Move tortoise one step and hare two steps
+        while hare and hare.next:
+            tortoise = tortoise.next
+            hare = hare.next.next
+
+            # Check if the hare meets the tortoise
+            if tortoise == hare:
+                break
+
+        # Check if there is no cycle
+        if not hare or not hare.next:
+            return None
+
+        # Reset either tortoise or hare pointer to the head
+        hare = head
+
+        # Move both pointers one step until they meet again
+        while tortoise != hare:
+            tortoise = tortoise.next
+            hare = hare.next
+
+        # Return the node where the cycle begins
+        return tortoise
