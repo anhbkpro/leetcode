7c6a111592746ee12f602b51764f3460cbb4f13e
Anh Lai <laingocanh1990@gmail.com>
2023-09-25 08:41:12 +0700

    389. Find the Difference - Hash

---
+++ lc_389_find_the_difference.py
@@ -0,0 +1,18 @@
+import collections
+
+
+class Solution:
+    @staticmethod
+    def findTheDifference(s: str, t: str) -> str:
+        m, n = len(s), len(t)
+        if m < n:
+            return Solution.findTheDifference(t, s)
+
+        sm = collections.Counter(s)
+        tm = collections.Counter(t)
+
+        for k in sm:
+            if k not in tm or sm[k] != tm[k]:
+                return k
+
+        return ""
