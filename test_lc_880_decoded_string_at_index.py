71b837f6a28e239471c91e7de62906efb0885697
Anh Lai <laingocanh1990@gmail.com>
2023-09-26 20:25:44 +0700

    880. Decoded String at Index

--- test_lc_880_decoded_string_at_index.py
+++ test_lc_880_decoded_string_at_index.py
@@ -2,6 +2,6 @@ from lc_880_decoded_string_at_index import Solution
 
 
 def test_decode_at_index():
-    assert Solution.decodeAtIndex(S="leet2code3", K=10) == "o"
-    assert Solution.decodeAtIndex(S="ha22", K=5) == "h"
-    assert Solution.decodeAtIndex(S="a2345678999999999999999", K=1) == "a"
+    assert Solution.decode_at_index(S="leet2code3", K=10) == "o"
+    assert Solution.decode_at_index(S="ha22", K=5) == "h"
+    assert Solution.decode_at_index(S="a2345678999999999999999", K=1) == "a"
