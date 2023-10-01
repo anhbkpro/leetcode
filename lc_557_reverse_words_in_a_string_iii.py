8d75de3d66c418e178534259eb162090bc66cbab
Anh Lai <laingocanh1990@gmail.com>
2023-10-01 08:44:14 +0700

    557. Reverse Words in a String III - 2 Pointers

--- lc_557_reverse_words_in_a_string_iii.py
+++ lc_557_reverse_words_in_a_string_iii.py
@@ -7,3 +7,21 @@ class Solution:
             rv.append(item[::-1])
 
         return ' '.join(rv)
+
+    @staticmethod
+    def reverseWords2Pointers(s: str) -> str:
+        last_space_index = -1
+        n = len(s)
+        ch_array = list(s)
+        print(ch_array)
+        for i in range(n + 1):
+            if i == n or s[i] == ' ':
+                start_index = last_space_index + 1
+                end_index = i - 1
+                while start_index < end_index:
+                    ch_array[start_index], ch_array[end_index] = ch_array[end_index], ch_array[start_index]
+                    start_index += 1
+                    end_index -= 1
+                last_space_index = i
+
+        return ''.join(ch_array)
