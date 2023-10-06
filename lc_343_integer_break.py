399b48424339a25af531399b47a2e1e9add4c134
Anh Lai <laingocanh1990@gmail.com>
2023-10-06 14:03:29 +0700

    343. Integer Break - Approach 1: Top-Down Dynamic Programming

---
+++ lc_343_integer_break.py
@@ -0,0 +1,21 @@
+from functools import cache
+
+
+class Solution:
+    @staticmethod
+    def integerBreak(n: int) -> int:
+        @cache
+        def dp(num):
+            if num <= 3:
+                return num
+
+            ans = num
+            for i in range(2, num):
+                ans = max(ans, i * dp(num - i))
+
+            return ans
+
+        if n <= 3:
+            return n - 1
+
+        return dp(n)
