d493a030706290c3a2ddd740cd48504fd4664070
Anh Lai <laingocanh1990@gmail.com>
2023-10-08 07:25:20 +0700

    1458. Max Dot Product of Two Subsequences

---
+++ lc_1458_max_dot_product_of_two_subsequences.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def maxDotProduct(nums1: List[int], nums2: List[int]) -> int:
+        if max(nums1) < 0 < min(nums2):
+            return max(nums1) * min(nums2)
+
+        if min(nums1) > 0 > max(nums2):
+            return min(nums1) * max(nums2)
+
+        dp = [[0] * (len(nums2) + 1) for _ in range(len(nums1) + 1)]
+        for i in range(len(nums1) - 1, -1, -1):
+            for j in range(len(nums2) - 1, -1, -1):
+                use = nums1[i] * nums2[j] + dp[i + 1][j + 1]
+                dp[i][j] = max(use, dp[i + 1][j], dp[i][j + 1])
+
+        return dp[0][0]
