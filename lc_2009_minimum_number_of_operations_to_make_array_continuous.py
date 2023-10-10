868aaed11f5728d05389d91a658e2dcdc5e13618
Anh Lai <laingocanh1990@gmail.com>
2023-10-10 21:10:01 +0700

    2009. Minimum Number of Operations to Make Array Continuous - Approach 1: Binary Search

--- lc_2009_minimum_number_of_operations_to_make_array_continuous.py
+++ lc_2009_minimum_number_of_operations_to_make_array_continuous.py
@@ -12,8 +12,9 @@ class Solution:
         for i in range(len(new_nums)):
             left = new_nums[i]
             right = left + n - 1
-            j = bisect_right(new_nums, right)
-            count = j - i
+            j = bisect_right(new_nums, right)  # find the index after the greatest number <= right
+            # for example if new_nums = [2, 3, 4, 7, ->12<-, 15, 44] and right = 9, then j = 4
+            count = j - i  # calculate the number of elements already in our range
             ans = min(ans, n - count)
 
         return ans
