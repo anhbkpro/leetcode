36b57994562fff3eba6e622177ed24e90c106b69
Anh Lai <laingocanh1990@gmail.com>
2023-10-13 20:11:29 +0700

    746. Min Cost Climbing Stairs

--- test_lc_746_min_cost_climbing_stairs.py
+++ test_lc_746_min_cost_climbing_stairs.py
@@ -4,3 +4,8 @@ from lc_746_min_cost_climbing_stairs_bottom_up import Solution
 def test_min_cost_climbing_stairs():
     assert Solution.minCostClimbingStairs(cost=[10, 15, 20]) == 15
     assert Solution.minCostClimbingStairs(cost=[1, 100, 1, 1, 1, 100, 1, 1, 100, 1]) == 6
+
+
+def test_min_cost_climbing_stairs2():
+    assert Solution.minCostClimbingStairs(cost=[10, 15, 20]) == 15
+    assert Solution.minCostClimbingStairs(cost=[1, 100, 1, 1, 1, 100, 1, 1, 100, 1]) == 6
