21ff24c7580b7710c1e4eb56ce09f73134462c69
Anh Lai <laingocanh1990@gmail.com>
2023-10-24 08:07:33 +0700

    add test 515. Find Largest Value in Each Tree Row - Approach 1: Breadth First Search (BFS)

---
+++ test_lc_515_find_largest_value_in_each_tree_row.py
@@ -0,0 +1,19 @@
+from lc_515_find_largest_value_in_each_tree_row import Solution, TreeNode
+
+root = TreeNode(val=1)
+root.left = TreeNode(val=3)
+root.right = TreeNode(val=2)
+root.left.left = TreeNode(val=5)
+root.left.right = TreeNode(val=3)
+root.right.right = TreeNode(val=9)
+s = Solution()
+
+#               1
+#             /   \
+#            3     2
+#           / \     \
+#          5   3     9
+
+
+def test_largest_values():
+    assert s.largestValues(root=root) == [1, 3, 9]
