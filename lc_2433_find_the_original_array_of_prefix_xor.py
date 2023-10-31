ce89b57c1b352488fda15492381f8fdd37ffd029
Anh Lai <laingocanh1990@gmail.com>
2023-10-31 15:24:01 +0700

    2433. Find The Original Array of Prefix Xor - Approach 1: Using XOR Properties

---
+++ lc_2433_find_the_original_array_of_prefix_xor.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def findArray(pref: List[int]) -> List[int]:
+        n = len(pref)
+
+        arr = [0] * n
+        arr[0] = pref[0]
+        for i in range(1, n):
+            arr[i] = pref[i] ^ pref[i - 1]
+
+        return arr
