d3b48c5b9c19a30a9ab6d22257c69e0b7a6ef552
Anh Lai <laingocanh1990@gmail.com>
2023-11-04 07:21:35 +0700

    169. Majority Element - Boyer-Moore Voting Algorithm

---
+++ lc_169_majority_element_boyer_moore_voting_algorithm.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def majorityElement(nums: List[int]) -> int:
+        count = 0
+        candidate = None
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+
+            count += 1 if num == candidate else -1
+
+        return candidate
