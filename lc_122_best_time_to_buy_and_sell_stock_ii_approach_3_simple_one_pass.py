870ab07f992854e271eed2f1b60481af3fd4ab13
Anh Lai <laingocanh1990@gmail.com>
2023-11-05 14:55:11 +0700

    122. Best Time to Buy and Sell Stock II - Approach 3: Simple One Pass

---
+++ lc_122_best_time_to_buy_and_sell_stock_ii_approach_3_simple_one_pass.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def maxProfit(prices: List[int]) -> int:
+        total = 0
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i - 1]:
+                total += prices[i] - prices[i - 1]
+
+        return total
