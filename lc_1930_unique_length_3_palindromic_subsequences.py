41bde21fbe9cd7f673e0dec9cbf81d878046d048
Anh Lai <laingocanh1990@gmail.com>
2023-11-15 07:39:49 +0700

    1930. Unique Length-3 Palindromic Subsequences - Approach 1: Count Letters In-Between

---
+++ lc_1930_unique_length_3_palindromic_subsequences.py
@@ -0,0 +1,16 @@
+class Solution:
+    @staticmethod
+    def countPalindromicSubsequence(s: str) -> int:
+        letters = set(s)
+        ans = 0
+
+        for letter in letters:
+            i, j = s.index(letter), s.rindex(letter)
+            between = set()
+
+            for k in range(i + 1, j):
+                between.add(s[k])
+
+            ans += len(between)
+
+        return ans
