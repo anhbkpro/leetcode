12020bd61d48c8839fb3bd71a2f015f88ba8dac6
Anh Lai <laingocanh1990@gmail.com>
2023-11-19 21:11:39 +0700

    1887. Reduction Operations to Make the Array Elements Equal - Approach: Sort and Count

---
+++ lc_1887_reduction_operations_to_make_the_array_elements_equal.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def reductionOperations(nums: List[int]) -> int:
+        nums.sort()
+        ans = 0
+        up = 0
+
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                up += 1
+
+            ans += up
+
+        return ans
