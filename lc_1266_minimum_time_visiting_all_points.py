1965b29dbc00d2d0f833251f7978fd4a5123bd02
Anh Lai <laingocanh1990@gmail.com>
2023-12-03 16:51:34 +0700

    1266. Minimum Time Visiting All Points

---
+++ lc_1266_minimum_time_visiting_all_points.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def minTimeToVisitAllPoints(points: List[List[int]]) -> int:
+        ans = 0
+        for i in range(len(points) - 1):
+            curr_x, curr_y = points[i]
+            target_x, target_y = points[i + 1]
+            ans += max(abs(target_x - curr_x), abs(target_y - curr_y))
+
+        return ans
---
+++ test_lc_1266_minimum_time_visiting_all_points.py
@@ -0,0 +1,5 @@
+from lc_1266_minimum_time_visiting_all_points import Solution
+
+
+def test_min_time_to_visit_all_points():
+    assert Solution.minTimeToVisitAllPoints(points=[[1, 1], [3, 4], [-1, 0]]) == 7
