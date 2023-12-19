0aff52804d89987ad3fba9dcbb18186337768f7e
Anh Lai <laingocanh1990@gmail.com>
2023-12-19 22:57:17 +0700

    F632 use ==/!= to compare constant literals (str, bytes, int, float, tuple)

--- test_lc_2353_design_a_food_rating_system.py
+++ test_lc_2353_design_a_food_rating_system.py
@@ -1,9 +1,12 @@
 from lc_2353_design_a_food_rating_system import FoodRatings
-food_ratings = FoodRatings(["kimchi", "miso", "sushi", "moussaka", "ramen", "bulgogi"], ["korean", "japanese", "japanese", "greek", "japanese", "korean"], [9, 12, 8, 15, 14, 7])
+
+food_ratings = FoodRatings(["kimchi", "miso", "sushi", "moussaka", "ramen", "bulgogi"],
+                           ["korean", "japanese", "japanese", "greek", "japanese", "korean"], [9, 12, 8, 15, 14, 7])
+
 
 def test_food_ratings():
-    assert food_ratings.highestRated("korean") is "kimchi"
-    assert food_ratings.highestRated("japanese") is "ramen"
-    assert food_ratings.highestRated("greek") is "moussaka"
+    assert food_ratings.highestRated("korean") == "kimchi"
+    assert food_ratings.highestRated("japanese") == "ramen"
+    assert food_ratings.highestRated("greek") == "moussaka"
     food_ratings.changeRating("bulgogi", 10)
-    assert food_ratings.highestRated("korean") is "bulgogi"
+    assert food_ratings.highestRated("korean") == "bulgogi"
