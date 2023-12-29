ba2eb6b0148043896a9ebf91e6a1b2300a9c7a52
Anh Lai <laingocanh1990@gmail.com>
2023-12-29 08:46:40 +0700

    1335. Minimum Difficulty of a Job Schedule - Approach 1: Top-down DP

--- lc_1335_minimum_difficulty_of_a_job_schedule.py
+++ lc_1335_minimum_difficulty_of_a_job_schedule.py
@@ -1,3 +1,4 @@
+from functools import cache
 from typing import List
 
 
@@ -19,3 +20,35 @@ class Solution:
         if min_diff[d][0] == float('inf'):
             return -1
         return min_diff[d][0]
+
+    @staticmethod
+    def minDifficultyTopDown(jobDifficulty: List[int], d: int) -> int:
+        n = len(jobDifficulty)
+        # Edge case: make sure there is at least one job per day
+        if n < d:
+            return -1
+
+        # Precompute the maximum job difficulty for remaining jobs
+        max_job_remaining = jobDifficulty[:]
+        for i in range(n - 2, -1, -1):
+            max_job_remaining[i] = max(max_job_remaining[i], max_job_remaining[i + 1])
+
+        # Use memoization to avoid repeated computation of min_diff states
+        @cache
+        def min_diff(i, days_remaining):
+            # Base case: finish all remaining jobs in the last day
+            if days_remaining == 1:
+                return max_job_remaining[i]
+
+            res = float('inf')
+            daily_max_job_diff = 0  # keep track of the maximum difficulty for today
+
+            # Iterate through possible starting index for the next day
+            # and ensure we have at least one job for each remaining day.
+            for j in range(i, n - days_remaining + 1):
+                daily_max_job_diff = max(daily_max_job_diff, jobDifficulty[j])
+                res = min(res, daily_max_job_diff + min_diff(j + 1, days_remaining - 1))
+
+            return res
+
+        return min_diff(0, d)
--- test_lc_1335_minimum_difficulty_of_a_job_schedule.py
+++ test_lc_1335_minimum_difficulty_of_a_job_schedule.py
@@ -5,3 +5,9 @@ def test_min_difficulty():
     assert Solution().minDifficulty(jobDifficulty=[6, 5, 4, 3, 2, 1], d=2) == 7
     assert Solution().minDifficulty(jobDifficulty=[9, 9, 9], d=4) == -1
     assert Solution().minDifficulty(jobDifficulty=[1, 1, 1], d=3) == 3
+
+
+def test_min_difficulty_top_down():
+    assert Solution().minDifficultyTopDown(jobDifficulty=[6, 5, 4, 3, 2, 1], d=2) == 7
+    assert Solution().minDifficultyTopDown(jobDifficulty=[9, 9, 9], d=4) == -1
+    assert Solution().minDifficultyTopDown(jobDifficulty=[1, 1, 1], d=3) == 3
