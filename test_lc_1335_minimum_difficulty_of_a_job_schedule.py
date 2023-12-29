310d9b1ce85006f76bb5c0503aa92c1a465b86e9
Anh Lai <laingocanh1990@gmail.com>
2023-12-29 08:43:49 +0700

    1335. Minimum Difficulty of a Job Schedule

---
+++ test_lc_1335_minimum_difficulty_of_a_job_schedule.py
@@ -0,0 +1,7 @@
+from lc_1335_minimum_difficulty_of_a_job_schedule import Solution
+
+
+def test_min_difficulty():
+    assert Solution().minDifficulty(jobDifficulty=[6, 5, 4, 3, 2, 1], d=2) == 7
+    assert Solution().minDifficulty(jobDifficulty=[9, 9, 9], d=4) == -1
+    assert Solution().minDifficulty(jobDifficulty=[1, 1, 1], d=3) == 3
