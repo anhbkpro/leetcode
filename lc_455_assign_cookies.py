50d805fd98dae80b8cc844fbe8fd6efdbbc01c0e
Anh Lai <laingocanh1990@gmail.com>
2024-01-01 14:31:59 +0700

    455. Assign Cookies

--- lc_455_assign_cookies.py
+++ lc_455_assign_cookies.py
@@ -4,16 +4,12 @@ from typing import List
 class Solution:
     @staticmethod
     def findContentChildren(g: List[int], s: List[int]) -> int:
-        i = j = 0
         g.sort()
         s.sort()
-        ans = 0
-        while i < len(g) and j < len(s):
-            if s[j] >= g[i]:
-                ans += 1
-                i += 1
-                j += 1
-            else:
-                j += 1
-
-        return ans
+        content_children = 0
+        cookie_index = 0
+        while cookie_index < len(s) and content_children < len(g):
+            if s[cookie_index] >= g[content_children]:
+                content_children += 1
+            cookie_index += 1
+        return content_children
