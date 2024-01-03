5f9f47af4405ad209fad0145a6752f718725b8fb
Anh Lai <laingocanh1990@gmail.com>
2024-01-03 21:11:36 +0700

    2125. Number of Laser Beams in a Bank

---
+++ lc_2125_number_of_laser_beams_in_a_bank.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def numberOfBeams(bank: List[str]) -> int:
+        ans = 0
+        prev_devices = bank[0].count("1")
+        for i in range(1, len(bank)):
+            cur_devices = bank[i].count("1")
+            if cur_devices == 0:
+                continue
+            ans += prev_devices * cur_devices
+            prev_devices = cur_devices
+
+        return ans
