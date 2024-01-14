5a485f137cbab578244f84fbc9ed54161dd1deb2
Anh Lai <laingocanh1990@gmail.com>
2024-01-14 19:14:07 +0700

    make code more concise

--- lc_1657_determine_if_two_strings_are_close.py
+++ lc_1657_determine_if_two_strings_are_close.py
@@ -4,13 +4,7 @@ from collections import Counter
 class Solution:
     @staticmethod
     def closeStrings(word1: str, word2: str) -> bool:
-        if len(word1) != len(word2):
-            return False
-
         count1 = Counter(word1)
         count2 = Counter(word2)
 
-        if count1.keys() != count2.keys():
-            return False
-
-        return sorted(count1.values()) == sorted(count2.values())
+        return sorted(count1.values()) == sorted(count2.values()) and count1.keys() == count2.keys()
