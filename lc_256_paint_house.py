de9523ff70c76bb92963838357ec47d600f954c5
Anh Lai <laingocanh1990@gmail.com>
2024-01-30 07:54:16 +0700

    256. Paint House

--- lc_256_paint_house.py
+++ lc_256_paint_house.py
@@ -2,14 +2,16 @@ from typing import List
 
 
 def min_cost(costs: List[List[int]]) -> int:
-    for n in reversed(range(len(costs) - 1)):
-        # Total cost of painting nth house red.
-        costs[n][0] += min(costs[n + 1][1], costs[n + 1][2])
-        # Total cost of painting nth house green.
-        costs[n][1] += min(costs[n + 1][0], costs[n + 1][2])
-        # Total cost of painting nth house blue.
-        costs[n][2] += min(costs[n + 1][0], costs[n + 1][1])
+    total_houses = len(costs)
+    for i in range(1, total_houses):
+        # Total cost of painting ith house red.
+        costs[i][0] += min(costs[i - 1][1], costs[i - 1][2])
+        # Total cost of painting ith house green.
+        costs[i][1] += min(costs[i - 1][0], costs[i - 1][2])
+        # Total cost of painting ith house blue.
+        costs[i][2] += min(costs[i - 1][0], costs[i - 1][1])
 
     if len(costs) == 0:
         return 0
-    return min(costs[0])  # Return the minimum in the first row.
+
+    return min(costs[-1])  # Return the minimum in the last row.
