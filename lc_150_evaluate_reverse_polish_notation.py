8ad680bfccb12ed0bc085ff48edacd926bf8b4ba
Anh Lai <laingocanh1990@gmail.com>
2024-01-30 08:20:35 +0700

    150. Evaluate Reverse Polish Notation - refactor code using lambda function

--- lc_150_evaluate_reverse_polish_notation.py
+++ lc_150_evaluate_reverse_polish_notation.py
@@ -4,22 +4,21 @@ from typing import List
 class Solution:
     @staticmethod
     def evalRPN(tokens: List[str]) -> int:
-        s = []
+        operations = {
+            "+": lambda a, b: a + b,
+            "-": lambda a, b: a - b,
+            "*": lambda a, b: a * b,
+            "/": lambda a, b: a / b
+        }
+
+        st = []
         for t in tokens:
             if t not in ["+", "-", "*", "/"]:
-                s.append(t)
+                st.append(t)
             else:
-                first = int(s.pop())
-                second = int(s.pop())
-                res = 0
-                if t == "+":
-                    res = first + second
-                elif t == "-":
-                    res = second - first
-                elif t == "*":
-                    res = first * second
-                elif second != 0:
-                    res = second / first
-                s.append(res)
+                first = int(st.pop())
+                second = int(st.pop())
+                op = operations[t]
+                st.append(op(second, first))
 
-        return int(s[0])
+        return int(st[0])
