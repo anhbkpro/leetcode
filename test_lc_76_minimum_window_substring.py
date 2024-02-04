4aa71c9b51f177c4fe5fd7a6dcda6068bd45d9a6
Anh Lai <laingocanh1990@gmail.com>
2024-02-04 17:40:13 +0700

    76. Minimum Window Substring

---
+++ test_lc_76_minimum_window_substring.py
@@ -0,0 +1,7 @@
+from lc_76_minimum_window_substring import Solution
+
+
+def test_min_window():
+    assert Solution.minWindow(s="ADOBECODEBANC", t="ABC") == "BANC"
+    assert Solution.minWindow(s="a", t="a") == "a"
+    assert Solution.minWindow(s="a", t="aa") == ""
