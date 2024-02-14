5e768bf6b90d268c3b27fa1d8cabed1e820acd79
Anh Lai <laingocanh1990@gmail.com>
2024-02-14 08:00:40 +0700

    215. Kth Largest Element in an Array

--- lc_215_kth_largest_element_in_an_array.py
+++ lc_215_kth_largest_element_in_an_array.py
@@ -4,11 +4,25 @@ from typing import List
 
 class Solution:
     @staticmethod
-    def findKthLargest(nums: List[int], k: int) -> int:
+    def find_kth_largest(nums: List[int], k: int) -> int:
+        """
+        Given an integer array nums and an integer k, return the kth largest element in the array.
+        Note that it is the kth largest element in the sorted order, not the kth distinct element.
+        :param nums: 1 <= nums.length <= 10^5, -10^4 <= nums[i] <= 10^4
+        :param k: 1 <= k <= nums.length
+        :return: kth largest element in the array
+        Input 1: nums = [3,2,1,5,6,4], k = 2
+        Output: 5
+        Explanation: The sorted nums are [6,5,4,3,2,1]. The 2nd largest number is 5.
+
+        Input 2: nums = [3,2,3,1,2,4,5,5,6], k = 4
+        Output: 4
+        Explanation: The sorted nums are [6,5,5,4,3,3,2,2,1]. The 4th largest number is 4.
+        """
         heap = []
         for num in nums:
             heapq.heappush(heap, num)
             if len(heap) > k:
                 heapq.heappop(heap)
 
-        return heapq.heappop(heap)  # or return heap[0]
+        return heap[0]
--- test_lc_215_kth_largest_element_in_an_array.py
+++ test_lc_215_kth_largest_element_in_an_array.py
@@ -2,4 +2,5 @@ from lc_215_kth_largest_element_in_an_array import Solution
 
 
 def test_find_kth_largest():
-    assert Solution.findKthLargest(nums=[3, 2, 1, 5, 6, 4], k=2) == 5
+    assert Solution.find_kth_largest(nums=[3, 2, 1, 5, 6, 4], k=2) == 5
+    assert Solution.find_kth_largest(nums=[3, 2, 3, 1, 2, 4, 5, 5, 6], k=4) == 4
