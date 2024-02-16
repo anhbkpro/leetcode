0b131f3dbb1186e712bf5a6ae78649c079dbf5d2
Anh Lai <laingocanh1990@gmail.com>
2024-02-16 16:46:46 +0700

    1481. Least Number of Unique Integers after K Removals

---
+++ lc_1481_least_number_of_unique_integers_after_k_removals.py
@@ -0,0 +1,22 @@
+import heapq
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def find_least_num_of_unique_ints(arr: List[int], k: int) -> int:
+        freq = Counter(arr)
+        data = heapq.nsmallest(k, freq.keys(), freq.get)
+
+        index = 0
+        while k > 0 and data:
+            item = data[index]
+            to_removed_count = min(k, freq[item])
+            k -= to_removed_count
+            freq[item] -= to_removed_count
+            if freq[item] == 0:
+                del freq[item]
+            index += 1
+
+        return len(freq)
