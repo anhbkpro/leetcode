ae7d56b7f044c038e371f8f1bf3bfabcb8604e64
Anh Lai <laingocanh1990@gmail.com>
2024-02-19 08:40:33 +0700

    210. Course Schedule II - Kahn's Algorithm

--- lc_210_course_schedule_ii.py
+++ lc_210_course_schedule_ii.py
@@ -2,32 +2,35 @@ from collections import deque
 from typing import List
 
 
-def find_order(numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-    indegree = [0] * numCourses
-    adj = [[] for _ in range(numCourses)]
-
-    for pair in prerequisites:
-        second, first = pair
-        indegree[second] += 1
-        adj[first].append(second)
-
-    q = deque([c for c in range(numCourses) if indegree[c] == 0])
-
-    res = []
-    while q:
-        node = q.popleft()
-        res.append(node)
-        for neighbor in adj[node]:
-            # Delete the edge "node -> neighbor".
-            indegree[neighbor] -= 1
-            if indegree[neighbor] == 0:
-                q.append(neighbor)
-
-    if any(indegree):
-        return []
-
-    return res
-
-
 class Solution:
-    pass
+    @staticmethod
+    def find_order(numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        graph = [[] for _ in range(numCourses)]
+        indegree = [0] * numCourses
+        for second, first in prerequisites:
+            graph[first].append(second)
+            indegree[second] += 1
+
+        dq = deque([course for course in range(numCourses) if indegree[course] == 0])
+        if not graph:
+            return list(range(numCourses))
+
+        visited = [False] * numCourses
+        ans = []
+        while dq:
+            node = dq.popleft()
+            visited[node] = True
+            ans.append(node)
+
+            for neighbor in graph[node]:
+                if visited[neighbor]:
+                    continue
+
+                indegree[neighbor] -= 1
+                if indegree[neighbor] == 0:
+                    dq.append(neighbor)
+
+        if len(ans) < numCourses:  # this is a cyclic graph
+            return []
+
+        return ans
