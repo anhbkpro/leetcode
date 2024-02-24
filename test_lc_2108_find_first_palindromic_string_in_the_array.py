83a7cf3376cc14390387a662416f403951b7c407
Anh Lai <laingocanh1990@gmail.com>
2024-02-24 09:15:51 +0700

    autoformatter with black

--- test_lc_2108_find_first_palindromic_string_in_the_array.py
+++ test_lc_2108_find_first_palindromic_string_in_the_array.py
@@ -2,5 +2,8 @@ from lc_2108_find_first_palindromic_string_in_the_array import Solution
 
 
 def test_first_palindrome():
-    assert Solution.firstPalindrome(words = ["abc","car","ada","racecar","cool"]) == "ada"
-    assert Solution.firstPalindrome(words = ["def","ghi"]) == ""
+    assert (
+        Solution.firstPalindrome(words=["abc", "car", "ada", "racecar", "cool"])
+        == "ada"
+    )
+    assert Solution.firstPalindrome(words=["def", "ghi"]) == ""
