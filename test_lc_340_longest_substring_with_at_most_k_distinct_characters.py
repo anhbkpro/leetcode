27cc969f4133686a11add29285a501d957471bb9
Anh Lai <laingocanh1990@gmail.com>
2024-03-06 07:49:49 +0700

    340. Longest Substring with At Most K Distinct Characters

--- test_lc_340_longest_substring_with_at_most_k_distinct_characters.py
+++ test_lc_340_longest_substring_with_at_most_k_distinct_characters.py
@@ -3,7 +3,7 @@ from lc_340_longest_substring_with_at_most_k_distinct_characters import (
 )
 
 
-def test_length_of_longest_substring_kdistinct():
+def test_length_of_longest_substring_k_distinct():
     assert Solution.length_of_longest_substring_k_distinct(s="eceba", k=2) == 3
     assert Solution.length_of_longest_substring_k_distinct(s="aa", k=1) == 2
     assert Solution.length_of_longest_substring_k_distinct(s="a", k=1) == 1
