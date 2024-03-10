3a6e7aaa7e9a0c7a5676598726050a23e12de724
Anh Lai <laingocanh1990@gmail.com>
2024-03-10 08:57:00 +0700

    1002. Find Common Characters

---
+++ find_common_characters_test.py
@@ -0,0 +1,10 @@
+from find_common_characters import Solution
+
+solution = Solution()
+
+
+def test_find_common_chars():
+    assert sorted(solution.commonChars(["bella", "label", "roller"])) == sorted(
+        ["e", "l", "l"]
+    )
+    assert solution.commonChars(["cool", "lock", "cook"]) == ["c", "o"]
