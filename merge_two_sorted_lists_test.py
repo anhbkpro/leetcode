e7148d5fd326bb478311c91c11fbcc0fe82a145e
Anh Lai <laingocanh1990@gmail.com>
2024-03-25 14:16:35 +0700

    fix test for wrong import

--- merge_two_sorted_lists_test.py
+++ merge_two_sorted_lists_test.py
@@ -1,55 +1,20 @@
-from merge_two_sorted_lists import Solution, LinkedList
+from merge_two_sorted_lists import Solution, ListNode
 
 
 def test_mergeTwoLists():
     s = Solution()
-    l1 = LinkedList()
-    l1.append(1)
-    l1.append(2)
-    l1.append(4)
-    l2 = LinkedList()
-    l2.append(1)
-    l2.append(3)
-    l2.append(4)
-    result = s.mergeTwoLists(l1.head, l2.head)
+    l1 = ListNode(1)
+    l1.next = ListNode(2)
+    l1.next.next = ListNode(4)
+    l2 = ListNode(1)
+    l2.next = ListNode(3)
+    l2.next.next = ListNode(4)
+    result = s.mergeTwoLists(l1, l2)
     assert result.val == 1
     assert result.next.val == 1
     assert result.next.next.val == 2
     assert result.next.next.next.val == 3
     assert result.next.next.next.next.val == 4
     assert result.next.next.next.next.next.val == 4
-    assert result.next.next.next.next.next.next is None
-    l1 = LinkedList()
-    l1.append(1)
-    l2 = LinkedList()
-    l2.append(1)
-    l2.append(2)
-    l2.append(3)
-    result = s.mergeTwoLists(l1.head, l2.head)
-    assert result.val == 1
-    assert result.next.val == 1
-    assert result.next.next.val == 2
-    assert result.next.next.next.val == 3
-    assert result.next.next.next.next is None
-    l1 = LinkedList()
-    l1.append(1)
-    l2 = LinkedList()
-    l2.append(2)
-    l2.append(3)
-    result = s.mergeTwoLists(l1.head, l2.head)
-    assert result.val == 1
-    assert result.next.val == 2
-    assert result.next.next.val == 3
-    assert result.next.next.next is None
-    l1 = LinkedList()
-    l1.append(1)
-    l2 = LinkedList()
-    l2.append(2)
-    l2.append(3)
-    l2.append(4)
-    result = s.mergeTwoLists(l1.head, l2.head)
-    assert result.val == 1
-    assert result.next.val == 2
-    assert result.next.next.val == 3
-    assert result.next.next.next.val == 4
-    assert result.next.next.next.next is None
+
+
