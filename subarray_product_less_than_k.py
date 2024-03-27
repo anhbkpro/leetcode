41dd6a30c1adac4838dd10a4c63ecfc155957004
Anh Lai <laingocanh1990@gmail.com>
2024-03-27 21:01:08 +0700

    713. Subarray Product Less Than K

--- subarray_product_less_than_k.py
+++ subarray_product_less_than_k.py
@@ -2,8 +2,9 @@ from typing import List
 
 
 class Solution:
-    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
-        # Handle edge cases where k is 0 or 1 (no subarrays possible)
+    @staticmethod
+    def num_subarray_product_less_than_k(nums: List[int], k: int) -> int:
+        # Handle edge cases where k is 0 or 1 (no sub-arrays possible)
         if k <= 1:
             return 0
 
@@ -13,20 +14,15 @@ class Solution:
         # Use two pointers to maintain a sliding window
         left = 0
         for right, num in enumerate(nums):
-            product *= (
-                num  # Expand the window by including the element at the right pointer
-            )
+            # Expand the window by including the element at the right pointer
+            product *= num
 
             # Shrink the window from the left while the product is greater than or equal to k
             while product >= k:
-                product //= nums[
-                    left
-                ]  # Remove the element at the left pointer from the product
+                product //= nums[left]  # Remove the element at the left pointer from the product
                 left += 1
 
-            # Update the total count by adding the number of valid subarrays with the current window size
-            total_count += (
-                right - left + 1
-            )  # right - left + 1 represents the current window size
+            # Update the total count by adding the number of valid sub-arrays with the current window size
+            total_count += right - left + 1 # right - left + 1 represents the current window size
 
         return total_count
--- subarray_product_less_than_k_test.py
+++ subarray_product_less_than_k_test.py
@@ -2,7 +2,6 @@ from subarray_product_less_than_k import Solution
 
 
 def test_numSubarrayProductLessThanK():
-    sol = Solution()
-    assert sol.numSubarrayProductLessThanK([10, 5, 2, 6], 100) == 8
-    assert sol.numSubarrayProductLessThanK([1, 2, 3], 0) == 0
-    assert sol.numSubarrayProductLessThanK([1, 1, 1], 1) == 0
+    assert Solution.num_subarray_product_less_than_k([10, 5, 2, 6], 100) == 8
+    assert Solution.num_subarray_product_less_than_k([1, 2, 3], 0) == 0
+    assert Solution.num_subarray_product_less_than_k([1, 1, 1], 1) == 0
