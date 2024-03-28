bbb0c47d7e1eb2137086af8d46fd1995e32caf8c
Anh Lai <laingocanh1990@gmail.com>
2024-03-28 09:39:47 +0700

    560. Subarray Sum Equals K

--- prefix-sum/subarray_sum_equals_k.py
+++ prefix-sum/subarray_sum_equals_k.py
@@ -4,17 +4,17 @@ from typing import List
 
 class Solution:
     @staticmethod
-    def subarraySum(nums: List[int], k: int) -> int:
-        count = defaultdict(int)
-        count[0] = 1
+    def subarray_sum(nums: List[int], k: int) -> int:
+        sum_occurrences_map = defaultdict(int)
+        sum_occurrences_map[0] = 1
         prefix_sum = 0
         ans = 0
         for num in nums:
             prefix_sum += num
-            ans += count.get(prefix_sum - k, 0)
-            if prefix_sum not in count:
-                count[prefix_sum] = 0
+            ans += sum_occurrences_map.get(prefix_sum - k, 0)
+            if prefix_sum not in sum_occurrences_map:
+                sum_occurrences_map[prefix_sum] = 0
 
-            count[prefix_sum] += 1
+            sum_occurrences_map[prefix_sum] += 1
 
         return ans
