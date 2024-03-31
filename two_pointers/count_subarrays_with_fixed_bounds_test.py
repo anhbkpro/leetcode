f81cb505738e5ea52660c7621c0de448e1f353b3
Anh Lai <laingocanh1990@gmail.com>
2024-03-31 09:42:02 +0700

    fix missing assert

--- two_pointers/count_subarrays_with_fixed_bounds_test.py
+++ two_pointers/count_subarrays_with_fixed_bounds_test.py
@@ -2,5 +2,5 @@ from count_subarrays_with_fixed_bounds import Solution
 
 
 def test_count_sub_arrays():
-    Solution.count_sub_arrays(nums=[1, 3, 5, 2, 7, 5], minK=1, maxK=5) == 2
-    Solution.count_sub_arrays(nums=[1, 1, 1, 1], minK=1, maxK=1) == 10
+    assert Solution.count_sub_arrays(nums=[1, 3, 5, 2, 7, 5], minK=1, maxK=5) == 2
+    assert Solution.count_sub_arrays(nums=[1, 1, 1, 1], minK=1, maxK=1) == 10
