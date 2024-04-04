6fc4e3c7aae1674249f410e71e24b15c7fac1c7b
Anh Lai <laingocanh1990@gmail.com>
2024-04-04 08:21:39 +0700

    79. Word Search

--- backtracking/word_search.py
+++ backtracking/word_search.py
@@ -1,4 +1,9 @@
 class Solution(object):
+    def __init__(self):
+        self.board = None
+        self.COLS = None
+        self.ROWS = None
+
     def exist(self, board, word):
         """
         :type board: List[List[str]]
@@ -14,22 +19,16 @@ class Solution(object):
                 if self.backtrack(row, col, word):
                     return True
 
-        # no match found after all exploration
+        # no match found after all explorations
         return False
 
     def backtrack(self, row, col, suffix):
-        # bottom case: we find match for each letter in the word
+        # bottom case: we find a match for each letter in the word
         if len(suffix) == 0:
             return True
 
         # Check the current status, before jumping into backtracking
-        if (
-            row < 0
-            or row == self.ROWS
-            or col < 0
-            or col == self.COLS
-            or self.board[row][col] != suffix[0]
-        ):
+        if row < 0 or row == self.ROWS or col < 0 or col == self.COLS or self.board[row][col] != suffix[0]:
             return False
 
         ret = False
@@ -38,11 +37,11 @@ class Solution(object):
         # explore the 4 neighbor directions
         for rowOffset, colOffset in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
             ret = self.backtrack(row + rowOffset, col + colOffset, suffix[1:])
-            # break instead of return directly to do some cleanup afterwards
+            # break instead of return directly to do some cleanup afterward
             if ret:
                 break
 
-        # revert the change, a clean slate and no side-effect
+        # revert the change, a clean slate and no side effect
         self.board[row][col] = suffix[0]
 
         # Tried all directions, and did not find any match
