9bbc84f5e4f0e5da69d4157e264787cee96969f6
Anh Lai <laingocanh1990@gmail.com>
2024-04-04 08:18:04 +0700

    79. Word Search

--- backtracking/word_search_test.py
+++ backtracking/word_search_test.py
@@ -9,3 +9,18 @@ def test_exist():
         )
         is True
     )
+    assert (
+        Solution().exist(
+            board=[["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]],
+            word="SEE",
+        )
+        is True
+    )
+
+    assert (
+        Solution().exist(
+            board=[["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]],
+            word="ABCB",
+        )
+        is False
+    )
