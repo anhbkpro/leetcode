dcdbc169dfb797ca01bdb6f9c3f9881d18dbb369
Anh Lai <laingocanh1990@gmail.com>
2024-04-07 10:16:38 +0700

    678. Valid Parenthesis String

---
+++ dp/valid_parenthesis_string_test.py
@@ -0,0 +1,12 @@
+from valid_parenthesis_string import Solution
+
+
+def test_checkValidString():
+    s = Solution()
+    assert s.checkValidString("()") == True
+    assert s.checkValidString("(*)") == True
+    assert s.checkValidString("(*))") == True
+    assert s.checkValidString("((*)") == True
+    assert s.checkValidString("((**))") == True
+    assert s.checkValidString("((**)))") == True
+    assert s.checkValidString("((**))(") == False
