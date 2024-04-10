db79e95e529d5c095d7a509ac03c427fe627b558
Anh Lai <laingocanh1990@gmail.com>
2024-04-10 20:40:25 +0700

    950. Reveal Cards In Increasing Order

---
+++ two_pointers/reveal_cards_in_increasing_order_test.py
@@ -0,0 +1,14 @@
+from reveal_cards_in_increasing_order import Solution
+
+
+def test_deckRevealedIncreasing():
+    s = Solution()
+    assert s.deckRevealedIncreasing(deck=[17, 13, 11, 2, 3, 5, 7]) == [
+        2,
+        13,
+        3,
+        11,
+        5,
+        17,
+        7,
+    ]
