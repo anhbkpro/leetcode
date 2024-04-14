db7b63cdc67d5247045d017a3ec23ec979c6ac3f
Anh Lai <laingocanh1990@gmail.com>
2024-04-14 07:31:30 +0700

    84. Largest Rectangle in Histogram

---
+++ stack/largest_rectangle_in_histogram_test.py
@@ -0,0 +1,6 @@
+from largest_rectangle_in_histogram import Solution
+
+
+def test_largestRectangleArea():
+    assert Solution().largestRectangleArea([2, 1, 5, 6, 2, 3]) == 10
+    assert Solution().largestRectangleArea([2, 4]) == 4
