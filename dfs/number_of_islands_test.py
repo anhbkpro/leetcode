d4145083f6a579542734d3d8745aa66be0b383a4
Anh Lai <laingocanh1990@gmail.com>
2024-04-19 20:43:50 +0700

    200. Number of Islands

---
+++ dfs/number_of_islands_test.py
@@ -0,0 +1,16 @@
+from dfs.number_of_islands import Solution
+
+
+def test_num_islands():
+    assert Solution().numIslands([
+        ['1', '1', '1', '1', '0'],
+        ['1', '1', '0', '1', '0'],
+        ['1', '1', '0', '0', '0'],
+        ['0', '0', '0', '0', '0']
+    ]) == 1
+    assert Solution().numIslands([
+        ['1', '1', '0', '0', '0'],
+        ['1', '1', '0', '0', '0'],
+        ['0', '0', '1', '0', '0'],
+        ['0', '0', '0', '1', '1']
+    ]) == 3
