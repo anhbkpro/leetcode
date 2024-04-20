7a5ca99551aa36c70c9f4756acb00b06dea3fff8
Anh Lai <laingocanh1990@gmail.com>
2024-04-20 19:21:05 +0700

    1992. Find All Groups of Farmland

---
+++ dfs/find_all_groups_of_farmland.py
@@ -0,0 +1,49 @@
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def find_farmland(land: List[List[int]]) -> List[List[int]]:
+        m = len(land)
+        n = len(land[0])
+        ans = []
+        visited_farm = set()
+
+        def dfs(r, c, max_x, max_y):
+            if r < 0 or r >= m or c < 0 or c >= n or land[r][c] != 1:
+                return max_x, max_y
+
+            land[r][c] = 0
+            max_x = max(r, max_x)
+            max_y = max(c, max_y)
+            # print(f"--- found farm land at [{r}, {c}]")
+            data = dfs(r + 1, c, max_x, max_y)
+            max_x = max(max_x, data[0])
+            max_y = max(max_y, data[1])
+            data = dfs(r - 1, c, max_x, max_y)
+            max_x = max(max_x, data[0])
+            max_y = max(max_y, data[1])
+            data = dfs(r, c + 1, max_x, max_y)
+            max_x = max(max_x, data[0])
+            max_y = max(max_y, data[1])
+            data = dfs(r, c - 1, max_x, max_y)
+            max_x = max(max_x, data[0])
+            max_y = max(max_y, data[1])
+            return max_x, max_y
+
+        for r in range(m):
+            for c in range(n):
+                min_x = r
+                min_y = c
+                max_x = r
+                max_y = c
+                if land[r][c] == 1:
+                    # print(f"start farm [{r}, {c}]")
+                    min_x = min(min_x, r)
+                    min_y = min(min_y, c)
+                    max_x, max_y = dfs(r, c, max_x, max_y)
+                    # print(max_x, max_y)
+                    if (min_x, min_y) not in visited_farm:
+                        visited_farm.add((r, c))
+                        ans.append([min_x, min_y, max_x, max_y])
+        return ans
