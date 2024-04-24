86be81b14a3af7c333e12e8f6c0e4f7c3de3dc14
Anh Lai <laingocanh1990@gmail.com>
2024-04-24 19:30:16 +0700

    1137. N-th Tribonacci Number - Top-Down

--- dp/n_th_tribonacci_number.py
+++ dp/n_th_tribonacci_number.py
@@ -6,3 +6,14 @@ class Solution:
         for i in range(n-2):
             a, b, c = b, c, a + b + c
         return c
+
+
+    def tribonacci_top_down(self, n: int) -> int:
+        dp = {0: 0, 1: 1, 2: 1}
+        def dfs(n):
+            if n in dp:
+                return dp[n]
+            dp[n] = dfs(n-1) + dfs(n-2) + dfs(n-3)
+            return dp[n]
+
+        return dfs(n)
--- dp/n_th_tribonacci_number_test.py
+++ dp/n_th_tribonacci_number_test.py
@@ -4,3 +4,5 @@ from dp.n_th_tribonacci_number import Solution
 def test_tribonacci():
     assert Solution().tribonacci(4) == 4
     assert Solution().tribonacci(25) == 1389537
+    assert Solution().tribonacci_top_down(4) == 4
+    assert Solution().tribonacci_top_down(25) == 1389537
