eb37e01c8bf542284bb55d91091446f78ecf3c80
Anh Lai <laingocanh1990@gmail.com>
2024-04-29 19:49:21 +0700

    2997. Minimum Number of Operations to Make Array XOR Equal to K

---
+++ bit_manipulation/minimum_number_of_operations_to_make_array_xor_equal_to_k_test.py
@@ -0,0 +1,5 @@
+from bit_manipulation.minimum_number_of_operations_to_make_array_xor_equal_to_k import Solution
+
+
+def test_min_operations_xor():
+    assert Solution().minOperations(nums = [2,1,3,4], k = 1) == 2
