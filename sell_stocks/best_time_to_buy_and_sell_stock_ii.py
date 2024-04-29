d863a467a568f816b16169286a2a050fe3db629c
Anh Lai <laingocanh1990@gmail.com>
2024-04-29 20:36:38 +0700

    122. Best Time to Buy and Sell Stock II

---
+++ sell_stocks/best_time_to_buy_and_sell_stock_ii.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        i = 0
+        valley = prices[0]
+        peak = prices[0]
+        max_profit = 0
+
+        while i < len(prices) - 1:
+            while i < len(prices) - 1 and prices[i] >= prices[i + 1]:
+                # down trend
+                i += 1
+            # find the local min (vally) for this down trend
+            valley = prices[i]
+            while i < len(prices) - 1 and prices[i] <= prices[i + 1]:
+                # up trend
+                i += 1
+
+            # find the local max (peak) for this up trend
+            peak = prices[i]
+            max_profit += peak - valley
+
+        return max_profit
