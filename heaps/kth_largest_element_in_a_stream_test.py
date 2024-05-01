63cd4a51bf04b2fa561509f67cf46c90b99a8696
Anh Lai <laingocanh1990@gmail.com>
2024-05-01 16:24:37 +0700

    703. Kth Largest Element in a Stream

---
+++ heaps/kth_largest_element_in_a_stream_test.py
@@ -0,0 +1,10 @@
+from heaps.kth_largest_element_in_a_stream import KthLargest
+import pytest
+
+
+def test_kth_largest_element():
+    obj = KthLargest(3, [4, 5, 8, 2])
+    assert obj.add(3) == 4
+    assert obj.add(5) == 5
+    assert obj.add(10) == 5
+    assert obj.add(9) == 8
