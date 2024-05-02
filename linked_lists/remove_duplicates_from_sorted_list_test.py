b3d919c05fe826dfe03a54929127fb7708059ec0
Anh Lai <laingocanh1990@gmail.com>
2024-05-02 20:12:28 +0700

    83. Remove Duplicates from Sorted List

---
+++ linked_lists/remove_duplicates_from_sorted_list_test.py
@@ -0,0 +1,12 @@
+from linked_lists.remove_duplicates_from_sorted_list import ListNode, Solution
+
+
+def test_remove_duplicates_from_sorted_list():
+    # head = [1,1,2]
+    # Output: [1,2]
+    head = ListNode(1)
+    head.next = ListNode(1)
+    head.next.next = ListNode(2)
+    ans = Solution().deleteDuplicates(head)
+    assert ans.val == 1
+    assert ans.next.val == 2
