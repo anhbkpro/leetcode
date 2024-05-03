c9d7f94d9b13bdf8c436d7b6ec8acd5c06951cc5
Anh Lai <laingocanh1990@gmail.com>
2024-05-03 14:02:39 +0700

    344. Reverse String

---
+++ arraystrings/reverse_string_test.py
@@ -0,0 +1,7 @@
+from arraystrings.reverse_string import Solution
+
+
+def test_reverse_string():
+    input = ["h", "e", "l", "l", "o"]
+    Solution().reverseString(input)
+    assert input == ["o", "l", "l", "e", "h"]
