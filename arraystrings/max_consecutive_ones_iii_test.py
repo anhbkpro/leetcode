927a8144248b6386f3a54d4b53e84e77a2c67c29
Anh Lai <laingocanh1990@gmail.com>
2024-05-03 14:41:04 +0700

    1004. Max Consecutive Ones III

---
+++ arraystrings/max_consecutive_ones_iii_test.py
@@ -0,0 +1,6 @@
+from arraystrings.max_consecutive_ones_iii import Solution
+
+
+def test_max_consecutive_ones():
+    assert Solution().longestOnes([1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0], 2) == 6
+    assert Solution().longestOnes(nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3) == 10
