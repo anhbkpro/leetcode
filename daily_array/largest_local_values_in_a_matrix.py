f04def53febbf40990d5ace2a14b4d67631f1a64
Anh Lai <laingocanh1990@gmail.com>
2024-05-12 08:24:26 +0700

    2373. Largest Local Values in a Matrix

---
+++ daily_array/largest_local_values_in_a_matrix.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def find_max(self, grid: List[List[int]], row: int, col: int) -> int:
+        max_element = 0
+        for i in range(row, row + 3):
+            for j in range(col, col + 3):
+                max_element = max(max_element, grid[i][j])
+
+        return max_element
+
+    def largestLocal(self, grid: List[List[int]]) -> List[List[int]]:
+        n = len(grid)
+        max_local = [[0]*(n-2) for _ in range(n-2)]
+        print(max_local)
+        for i in range(n-2):
+            for j in range(n-2):
+                max_local[i][j] = self.find_max(grid, i, j)
+
+        return max_local
---
+++ daily_array/largest_local_values_in_a_matrix_test.py
@@ -0,0 +1,6 @@
+from daily_array.largest_local_values_in_a_matrix import Solution
+
+
+def test_largest_local_values_in_a_matrix():
+    assert Solution().largestLocal([[9, 9, 8, 1], [5, 6, 2, 6], [8, 2, 6, 4], [6, 2, 2, 2]]) == [[9, 9], [8, 6]]
+    assert Solution().largestLocal([[1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 2, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1]]) == [[2, 2, 2], [2, 2, 2], [2, 2, 2]]
