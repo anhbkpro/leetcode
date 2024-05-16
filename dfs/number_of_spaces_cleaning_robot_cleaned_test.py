5fec51d096c4aa5b0b88a2a61c28b26d2ad5632d
Anh Lai <laingocanh1990@gmail.com>
2024-05-16 20:31:56 +0700

    2061. Number of Spaces Cleaning Robot Cleaned

---
+++ dfs/number_of_spaces_cleaning_robot_cleaned_test.py
@@ -0,0 +1,6 @@
+from dfs.number_of_spaces_cleaning_robot_cleaned import Solution
+
+
+def test_number_of_spaces_cleaning_robot_cleaned():
+    assert Solution().numberOfCleanRooms(room = [[0,0,0],[1,1,0],[0,0,0]]) == 7
+    assert Solution().numberOfCleanRooms(room = [[0,1,0],[1,0,0],[0,0,0]]) == 1
