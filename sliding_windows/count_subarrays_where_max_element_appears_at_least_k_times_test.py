e758666738660e204e92fceec6a62ffeb8346c69
Anh Lai <laingocanh1990@gmail.com>
2024-05-28 23:25:59 +0700

    1208. Get Equal Substrings Within Budget

---
+++ sliding_windows/count_subarrays_where_max_element_appears_at_least_k_times_test.py
@@ -0,0 +1,7 @@
+from sliding_windows.count_subarrays_where_max_element_appears_at_least_k_times import Solution
+
+
+def test_count_sub_arrays():
+    sol = Solution()
+    assert sol.count_sub_arrays(nums=[1, 4, 2, 1], k=3) == 0
+    assert sol.count_sub_arrays(nums=[1, 3, 2, 3, 3], k=2) == 6
---
+++ sliding_windows/get_equal_substrings_within_budget_test.py
@@ -0,0 +1,5 @@
+from sliding_windows.get_equal_substrings_within_budget import Solution
+
+
+def test_equal_substrings():
+    assert Solution().equalSubstring("abcd", "bcdf", 3) == 3
