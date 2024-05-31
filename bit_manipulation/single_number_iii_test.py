d662cc916540c3653049417d7a1cc8d21774a572
Anh Lai <laingocanh1990@gmail.com>
2024-05-31 22:01:42 +0700

    260. Single Number III

---
+++ bit_manipulation/single_number_iii_test.py
@@ -0,0 +1,6 @@
+from bit_manipulation.single_number_iii import Solution
+
+
+def test_single_number():
+    assert Solution().singleNumber([1, 2, 1, 3, 2, 5]) == [3, 5]
+    assert Solution().singleNumber([-1,0]) == [-1,0]
