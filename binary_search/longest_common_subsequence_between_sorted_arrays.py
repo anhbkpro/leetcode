de80852463993559d7b0d106e3e38033b50bcb34
Anh Lai <laingocanh1990@gmail.com>
2024-06-01 09:00:55 +0700

    1940. Longest Common Subsequence Between Sorted Arrays

---
+++ binary_search/longest_common_subsequence_between_sorted_arrays.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+class Solution:
+    def longestCommonSubsequence(self, arrays: List[List[int]]) -> List[int]:
+        def binary_search(array: List[int], target: int) -> bool:
+            l, r = 0, len(array) - 1
+            while l <= r:
+                m = (l + r) // 2
+                if array[m] == target:
+                    return True
+                elif array[m] < target:
+                    l = m + 1
+                else:
+                    r = m - 1
+
+            return False
+
+        s = []
+        for num in arrays[0]:
+            valid = True
+            for array in arrays[1:]:
+                if not binary_search(array, num):
+                    valid = False
+            if valid and (len(s) == 0 or num > s[-1]):
+                s.append(num)
+
+        return s
---
+++ binary_search/longest_common_subsequence_between_sorted_arrays_test.py
@@ -0,0 +1,7 @@
+from binary_search.longest_common_subsequence_between_sorted_arrays import Solution
+
+
+def test_longest_common_subsequence():
+    assert Solution().longestCommonSubsequence(
+            arrays=[[1, 3, 4, 5], [1, 2, 3, 4, 5], [1, 2, 3, 4, 4, 5]]
+        ) == [1, 3, 4, 5]
