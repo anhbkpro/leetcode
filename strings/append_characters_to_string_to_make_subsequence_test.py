381cee783715b7df7578e5ea44d9b987104b9673
Anh Lai <laingocanh1990@gmail.com>
2024-06-03 22:22:34 +0700

    2486. Append Characters to String to Make Subsequence

---
+++ strings/append_characters_to_string_to_make_subsequence_test.py
@@ -0,0 +1,7 @@
+from strings.append_characters_to_string_to_make_subsequence import Solution
+
+
+def test_append_characters_to_string_to_make_subsequence():
+    assert Solution().appendCharacters("zzazz", "zz") == 0
+    assert Solution().appendCharacters("mbadm", "bda") == 1
+    assert Solution().appendCharacters("coaching", "coding") == 4
