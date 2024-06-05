61d134cf70dc07fc0827d3d4c078b0963ca64696
Anh Lai <laingocanh1990@gmail.com>
2024-06-05 14:23:27 +0700

    49. Group Anagrams

---
+++ strings/group_anagrams_test.py
@@ -0,0 +1,7 @@
+from strings.group_anagrams import Solution
+
+
+def test_group_anagrams():
+    assert sorted(Solution().groupAnagrams(["eat", "tea", "bat"])) == [["bat"], ["eat", "tea"]]
+    assert sorted(Solution().groupAnagrams([""])) == [[""]]
+    assert sorted(Solution().groupAnagrams(["a"])) == [["a"]]
