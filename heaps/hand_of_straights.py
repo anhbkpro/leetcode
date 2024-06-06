bdc72c6b260805bb4fe03dd668bf38be3b391163
Anh Lai <laingocanh1990@gmail.com>
2024-06-06 10:48:35 +0700

    846. Hand of Straights

---
+++ heaps/hand_of_straights.py
@@ -0,0 +1,34 @@
+from collections import Counter
+import heapq
+from typing import List
+
+
+
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        c = Counter(hand)
+        n = len(hand)
+        if n % groupSize != 0:
+            return False
+
+        heap = []
+        for num in hand:
+            heapq.heappush(heap, num)
+
+        while len(heap) > 0:
+            num = heapq.heappop(heap)
+            freq = c[num]
+            if freq == 0:
+                continue
+
+            c[num] -= freq
+            for i in range(1, groupSize):
+                if c[num + i] < freq:
+                    return False
+
+                c[num + i] -= freq
+                if c[num + i] == 0:
+                    del c[num + i]
+
+
+        return True
---
+++ heaps/hand_of_straights_test.py
@@ -0,0 +1,6 @@
+from heaps.hand_of_straights import Solution
+
+
+def test_hand_of_straights():
+    assert Solution().isNStraightHand([1, 2, 3, 6, 2, 3, 4, 7, 8], 3) == True
+    assert Solution().isNStraightHand([1, 2, 3, 4, 5], 4) == False
