522dc4c0eac0d9a787e650cf1943d2149190543d
Anh Lai <laingocanh1990@gmail.com>
2024-06-09 15:41:30 +0700

    add more tests for 2083. Substrings That Begin and End With the Same Letter

--- prefix_sum/substrings_that_begin_and_end_with_the_same_letter_test.py
+++ prefix_sum/substrings_that_begin_and_end_with_the_same_letter_test.py
@@ -3,3 +3,5 @@ from .substrings_that_begin_and_end_with_the_same_letter import Solution
 
 def test_substrings_that_begin_and_end_with_the_same_letter():
   assert Solution().numberOfSubstrings("abcba") == 7
+  assert Solution().numberOfSubstrings("abacad") == 9
+  assert Solution().numberOfSubstrings("a") == 1
