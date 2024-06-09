5a73e42e653bddf762dea3a632c4768a877ddabe
Anh Lai <laingocanh1990@gmail.com>
2024-06-09 14:36:50 +0700

    974. Subarray Sums Divisible by K

---
+++ hashing/subarray_sums_divisible_by_k_test.py
@@ -0,0 +1,6 @@
+from hashing.subarray_sums_divisible_by_k import Solution
+
+
+def test_subarray_sums_divisible_by_k():
+    assert Solution().subarraysDivByK([4, 5, 0, -2, -3, 1], 5) == 7
+    assert Solution().subarraysDivByK([5], 9) == 0
