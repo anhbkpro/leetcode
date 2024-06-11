a3059a4922bec7e44a86e4e052dabbae2913941e
Anh Lai <laingocanh1990@gmail.com>
2024-06-11 13:44:05 +0700

    1122. Relative Sort Array

---
+++ sorting/lc1122_relative_sort_array/relative_sort_array.py
@@ -0,0 +1,23 @@
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        ans = []
+        f1 = Counter(arr1)
+        f2 = Counter(arr2)
+        remaining = []
+        for num in arr2:
+            ans.extend([num]*f1[num])
+
+        visited = set()
+        for num in arr1:
+            if num in f2 or num in visited:
+                continue
+            remaining.extend([num]*f1[num])
+            visited.add(num)
+
+        remaining.sort()
+        ans.extend(remaining)
+        return ans
---
+++ sorting/lc1122_relative_sort_array/relative_sort_array_test.py
@@ -0,0 +1,5 @@
+from .relative_sort_array import Solution
+
+
+def test_relativeSortArray():
+    assert Solution().relativeSortArray(arr1 = [2,3,1,3,2,4,6,7,9,2,19], arr2 = [2,1,4,3,9,6]) == [2,2,2,1,4,3,3,9,6,7,19]
