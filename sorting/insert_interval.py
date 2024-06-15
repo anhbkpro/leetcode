e58a834c28babcb51b4f083f2e4bea3a087f1ceb
Anh Lai <laingocanh1990@gmail.com>
2024-06-15 22:13:16 +0700

    57. Insert Interval

---
+++ sorting/insert_interval.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        intervals.append(newInterval)
+        intervals.sort(key = lambda x: x[0])
+
+        merged = []
+        for start, end in intervals:
+            # not intersect
+            if not merged or merged[-1][1] < start:
+                merged.append([start, end])
+            # intersect
+            else:
+                merged[-1][1] = max(merged[-1][1], end)
+        return merged
