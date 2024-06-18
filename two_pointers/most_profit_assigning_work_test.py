ee0802a72c168a5356033b3172b574717afeb7c6
Anh Lai <laingocanh1990@gmail.com>
2024-06-18 21:05:59 +0700

    826. Most Profit Assigning Work

---
+++ two_pointers/most_profit_assigning_work_test.py
@@ -0,0 +1,7 @@
+from .most_profit_assigning_work import Solution
+
+
+def test_most_profit_assigning_work():
+    assert Solution().maxProfitAssignment(
+        difficulty=[2, 4, 6, 8, 10], profit=[10, 20, 30, 40, 50], worker=[4, 5, 6, 7]
+    ) == 100
