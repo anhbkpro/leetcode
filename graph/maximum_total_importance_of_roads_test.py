86ad1c26d2206cb0f38ec49d3b0ca9c3a8c23716
Anh Lai <laingocanh1990@gmail.com>
2024-06-29 23:32:12 +0700

    2192. All Ancestors of a Node in a Directed Acyclic Graph

--- graph/maximum_total_importance_of_roads_test.py
+++ graph/maximum_total_importance_of_roads_test.py
@@ -1,7 +1,7 @@
 from .maximum_total_importance_of_roads import Solution
 
 
-def test_maximumImportance():
+def test_maximum_importance():
     assert Solution().maximumImportance(n=5, roads=[[0, 3], [2, 4], [1, 3]]) == 20
     assert (
         Solution().maximumImportance(
