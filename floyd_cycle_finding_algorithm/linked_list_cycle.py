d8c950f3012d16ff49f9300f005ece9c27c59544
Anh Lai <laingocanh1990@gmail.com>
2024-07-04 08:57:35 +0700

    141. Linked List Cycle

---
+++ floyd_cycle_finding_algorithm/linked_list_cycle.py
@@ -0,0 +1,18 @@
+from linked_lists.linked_list_node import LinkedListNode
+from typing import Optional
+
+
+class Solution:
+    def hasCycle(self, head: Optional[LinkedListNode]) -> bool:
+        if head is None:
+            return False
+
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
+
+        return False
---
+++ floyd_cycle_finding_algorithm/linked_list_cycle_test.py
@@ -0,0 +1,16 @@
+from .linked_list_cycle import Solution
+from linked_lists.linked_list_node import LinkedListNode
+
+
+def test_linked_list_cycle():
+    node = LinkedListNode(3)
+    node.next = LinkedListNode(2)
+    node.next.next = LinkedListNode(0)
+    node.next.next.next = LinkedListNode(-4)
+    node.next.next.next.next = node.next
+    assert Solution().hasCycle(node) == True
+
+    node = LinkedListNode(1)
+    node.next = LinkedListNode(2)
+    node.next.next = node
+    assert Solution().hasCycle(node) == True
