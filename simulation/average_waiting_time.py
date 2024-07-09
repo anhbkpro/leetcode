05ee662c1ff2387f10e7d8acfacb4187f8ff3336
Anh Lai <laingocanh1990@gmail.com>
2024-07-09 22:06:23 +0700

    1701. Average Waiting Time

---
+++ simulation/average_waiting_time.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def averageWaitingTime(self, customers: List[List[int]]) -> float:
+        last_serve_time = 0
+        total_wait_time = 0
+        for arrival, time in customers:
+            start_serve = max(arrival, last_serve_time)
+            wait_time = start_serve + time - arrival
+            total_wait_time += wait_time
+            last_serve_time = start_serve + time
+        return total_wait_time/len(customers)
