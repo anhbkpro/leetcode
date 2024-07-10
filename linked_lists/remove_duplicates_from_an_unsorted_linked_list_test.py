c9bfbffb139e6de4de8170f3dc45a7cd93ee173d
Anh Lai <laingocanh1990@gmail.com>
2024-07-10 22:32:11 +0700

    1836. Remove Duplicates From an Unsorted Linked List

--- linked_lists/remove_duplicates_from_an_unsorted_linked_list_test.py
+++ linked_lists/remove_duplicates_from_an_unsorted_linked_list_test.py
@@ -1,45 +1,37 @@
 from .remove_duplicates_from_an_unsorted_linked_list import Solution
 from .linked_list_node import LinkedListNode
+from .linked_list import LinkedList
 
 
 def test_remove_duplicates_from_an_unsorted_linked_list():
     # head = [1,2,3,2]
     # Output: [1,3]
-    head = LinkedListNode(1)
-    head.next = LinkedListNode(2)
-    head.next.next = LinkedListNode(3)
-    head.next.next.next = LinkedListNode(2)
-    expected = LinkedListNode(1)
-    expected.next = LinkedListNode(3)
-    solution = Solution()
-    actual = solution.deleteDuplicatesUnsorted(head)
-    assert are_equal(actual, expected)
+    actual = LinkedList()
+    actual.create_linked_list([1, 2, 3, 2])
+    head = actual.head
+    expected = LinkedList()
+    expected.create_linked_list([1, 3])
+    actual = Solution().deleteDuplicatesUnsorted(head)
+    assert are_equal(actual, expected.head)
 
     # head = [2,1,1,2]
     # Output: []
-    head = LinkedListNode(2)
-    head.next = LinkedListNode(1)
-    head.next.next = LinkedListNode(1)
-    head.next.next.next = LinkedListNode(2)
+    actual = LinkedList()
+    actual.create_linked_list([2, 1, 1, 2])
+    head = actual.head
     expected = None
-    solution = Solution()
-    actual = solution.deleteDuplicatesUnsorted(head)
-    assert are_equal(actual, expected)
+    actual = Solution().deleteDuplicatesUnsorted(head)
+    assert actual == expected
 
     # head = [3,2,2,1,3,2,4]
     # Output: [1,4]
-    head = LinkedListNode(3)
-    head.next = LinkedListNode(2)
-    head.next.next = LinkedListNode(2)
-    head.next.next.next = LinkedListNode(1)
-    head.next.next.next.next = LinkedListNode(3)
-    head.next.next.next.next.next = LinkedListNode(2)
-    head.next.next.next.next.next.next = LinkedListNode(4)
-    expected = LinkedListNode(1)
-    expected.next = LinkedListNode(4)
-    solution = Solution()
-    actual = solution.deleteDuplicatesUnsorted(head)
-    assert are_equal(actual, expected)
+    actual = LinkedList()
+    actual.create_linked_list([3, 2, 2, 1, 3, 2, 4])
+    head = actual.head
+    expected = LinkedList()
+    expected.create_linked_list([1, 4])
+    actual = Solution().deleteDuplicatesUnsorted(head)
+    assert are_equal(actual, expected.head)
 
 def are_equal(head1: LinkedListNode, head2: LinkedListNode) -> bool:
     while head1 and head2:
