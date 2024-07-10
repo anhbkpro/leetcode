3719725e84c317691632f3cacb48ff4965d440ee
Anh Lai <laingocanh1990@gmail.com>
2024-07-10 08:21:15 +0700

    1598. Crawler Log Folder - Counter - O(1) Space

--- stacks_and_queues/crawler_log_folder.py
+++ stacks_and_queues/crawler_log_folder.py
@@ -12,3 +12,14 @@ class Solution:
                 folder_stack.append(current_operation)
 
         return len(folder_stack)
+
+    def minOperationsO1Space(self, logs: List[str]) -> int:
+        operations = 0
+        for current_operation in logs:
+            if current_operation == "../":
+                if operations:
+                    operations -= 1
+            elif current_operation != "./":
+                operations += 1
+
+        return operations
--- stacks_and_queues/crawler_log_folder_test.py
+++ stacks_and_queues/crawler_log_folder_test.py
@@ -6,9 +6,12 @@ def test_crawler_log_folder():
 
     logs = ["d1/", "d2/", "../", "d21/", "./"]
     assert solution.minOperations(logs) == 2
+    assert solution.minOperationsO1Space(logs) == 2
 
     logs = ["d1/", "d2/", "./", "d3/", "../", "d31/"]
     assert solution.minOperations(logs) == 3
+    assert solution.minOperationsO1Space(logs) == 3
 
     logs = ["d1/", "../", "../", "../"]
     assert solution.minOperations(logs) == 0
+    assert solution.minOperationsO1Space(logs) == 0
