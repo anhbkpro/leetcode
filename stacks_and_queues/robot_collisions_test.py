da617fe75dacc7124d2380feebdd169b085376d4
Anh Lai <laingocanh1990@gmail.com>
2024-07-13 23:04:59 +0700

    2751. Robot Collisions

---
+++ stacks_and_queues/robot_collisions_test.py
@@ -0,0 +1,5 @@
+from .robot_collisions import Solution
+
+
+def test_survived_robots_healths():
+    assert Solution().survivedRobotsHealths(positions = [5,4,3,2,1], healths = [2,17,9,15,10], directions = "RRRRR") == [2,17,9,15,10]
