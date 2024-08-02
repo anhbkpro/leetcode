039e15f75ebda56f78245bcca47c8cd6086db3fe
Anh Lai <laingocanh1990@gmail.com>
2024-08-02 17:00:12 +0700

    2134. Minimum Swaps to Group All 1's Together II

---
+++ sliding_windows/minimum_swaps_to_group_all_1s_together_ii_test.py
@@ -0,0 +1,7 @@
+from .minimum_swaps_to_group_all_1s_together_ii import Solution
+
+
+def test_min_swaps():
+    assert Solution().minSwaps(nums = [0,1,0,1,1,0,0]) == 1
+    assert Solution().minSwaps(nums = [0,1,1,1,0,0,1,1,0]) == 2
+    assert Solution().minSwaps(nums = [1,1,0,0,1]) == 0
--- sliding_windows/subarrays_with_k_different_integers_test.py
+++ sliding_windows/subarrays_with_k_different_integers_test.py
@@ -1,7 +1,7 @@
 from .subarrays_with_k_different_integers import Solution
 
 
-def test_subarraysWithKDistinct():
+def test_subarrays_with_k_distinct():
     sol = Solution()
     assert sol.subarraysWithKDistinct([1, 2, 1, 2, 3], 2) == 7
     assert sol.subarraysWithKDistinct([1, 2, 1, 3, 4], 3) == 3
