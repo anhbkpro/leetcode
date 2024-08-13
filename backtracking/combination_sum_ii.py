5e4334b0db15819d6ecde77d80915c4157e669a5
Anh Lai <laingocanh1990@gmail.com>
2024-08-13 22:11:18 +0700

    40. Combination Sum II

--- backtracking/combination_sum_ii.py
+++ backtracking/combination_sum_ii.py
@@ -1,44 +1,23 @@
-from typing import Counter, List
-
-
 class Solution:
-    def combinationSum2(
-        self, candidates: List[int], target: int
-    ) -> List[List[int]]:
+    def combinationSum2(self, candidates, target):
+        answer = []
+        candidates.sort()
+        self.backtrack(candidates, target, 0, [], answer)
+        return answer
 
-        def backtrack(comb, remain, curr, counter, results):
-            if remain == 0:
-                # make a deep copy of the current combination
-                #   rather than keeping the reference.
-                results.append(list(comb))
-                return
-            elif remain < 0:
-                return
-
-            for next_curr in range(curr, len(counter)):
-                candidate, freq = counter[next_curr]
-
-                if freq <= 0:
-                    continue
-
-                # add a new element to the current combination
-                comb.append(candidate)
-                counter[next_curr] = (candidate, freq - 1)
-
-                # continue the exploration with the updated combination
-                backtrack(comb, remain - candidate, next_curr, counter, results)
-
-                # backtrack the changes, so that we can try another candidate
-                counter[next_curr] = (candidate, freq)
-                comb.pop()
-
-        results = []  # container to hold the final combinations
-        counter = Counter(candidates)
-        # convert the counter table to a list of (num, count) tuples
-        counter = [(c, counter[c]) for c in counter]
-
-        backtrack(
-            comb=[], remain=target, curr=0, counter=counter, results=results
-        )
-
-        return results
+    def backtrack(self, candidates, target, totalIdx, path, answer):
+        if target < 0:
+            return  # backtracking
+        if target == 0:
+            answer.append(path)
+            return  # end
+        for i in range(totalIdx, len(candidates)):
+            if i > totalIdx and candidates[i] == candidates[i - 1]:
+                continue
+            self.backtrack(
+                candidates,
+                target - candidates[i],
+                i + 1,
+                path + [candidates[i]],
+                answer,
+            )
