9e2dcf7075d217208ee6091f63726ba6fee4404c
Anh Lai <laingocanh1990@gmail.com>
2024-08-16 13:26:34 +0700

    263. Ugly Number

--- arraystrings/ugly_number_test.py
+++ arraystrings/ugly_number_test.py
@@ -3,5 +3,5 @@ from .ugly_number import Solution
 
 def test_is_ugly():
     assert Solution().isUgly(6) == True
-    assert Solution().isUgly(1) == True
+    assert Solution().isUgly(8) == True
     assert Solution().isUgly(14) == False
