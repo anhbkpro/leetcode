84d259a8ae80f0df80a15261900dbfbfbed85119
Anh Lai <laingocanh1990@gmail.com>
2024-08-31 16:25:24 +0700

    1514. Path with Maximum Probability

---
+++ arraystrings/path_with_maximum_probability_test.py
@@ -0,0 +1,10 @@
+from .path_with_maximum_probability import Solution
+
+
+def test_max_probability():
+    assert (
+        Solution().maxProbability(
+            3, [[0, 1], [1, 2], [0, 2]], [0.5, 0.5, 0.2], 0, 2
+        )
+        == 0.25
+    )
