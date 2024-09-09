ed0825f7750523f81fa924f2343c66977a409326
Anh Lai <laingocanh1990@gmail.com>
2024-09-09 19:48:05 +0700

    2326. Spiral Matrix IV

---
+++ linked_lists/spiral_matrix_iv_test.py
@@ -0,0 +1,9 @@
+from .spiral_matrix_iv import Solution
+from .linked_list_node import LinkedListNode
+
+
+def test_spiral_matrix():
+    head = LinkedListNode(0)
+    head.next = LinkedListNode(1)
+    head.next.next = LinkedListNode(2)
+    assert Solution().spiralMatrix(m = 1, n = 4, head = head) == [[0,1,2,-1]]
