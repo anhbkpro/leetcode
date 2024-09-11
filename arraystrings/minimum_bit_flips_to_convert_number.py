6ed151bdd12a31cbaa4d575d5e0b55b60529a7ef
Anh Lai <laingocanh1990@gmail.com>
2024-09-11 15:56:21 +0700

    2220. Minimum Bit Flips to Convert Number - Shift Bits

--- arraystrings/minimum_bit_flips_to_convert_number.py
+++ arraystrings/minimum_bit_flips_to_convert_number.py
@@ -15,3 +15,14 @@ class Solution:
                 ans += 1
 
         return ans + start_bin[:(start_len - min_len)].count("1") + goal_bin[:(goal_len - min_len)].count("1")
+
+    def minBitFlipsByShiftBit(self, start: int, goal: int) -> int:
+        count = 0
+        while start > 0 or goal > 0:
+            # Increment count if the current bits differ
+            if (start & 1) != (goal & 1):
+                count += 1
+            # Shift both numbers to the right to check the next bits
+            start >>= 1
+            goal >>= 1
+        return count
