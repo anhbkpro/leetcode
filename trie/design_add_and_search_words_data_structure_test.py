366d8325def1741001b14b8256c451c5d9c2ede1
Anh Lai <laingocanh1990@gmail.com>
2024-09-26 08:36:26 +0700

    211. Design Add and Search Words Data Structure

--- trie/design_add_and_search_words_data_structure_test.py
+++ trie/design_add_and_search_words_data_structure_test.py
@@ -7,10 +7,12 @@ def test_search_existing_word():
     word_dict.addWord("example")
     assert word_dict.search("example") == True
 
+
 # Search for an empty string
 def test_search_empty_string():
     word_dict = WordDictionary()
     assert word_dict.search("") == False
+
 
 # Search for a word that does not exist in the dictionary
 def test_search_non_existing_word():
@@ -18,8 +20,11 @@ def test_search_non_existing_word():
     word_dict.addWord("example")
     assert word_dict.search("examples") == False
 
-# Search for a word that has a dot in it
-def test_search_word_with_dot():
+
+# Search for a word using '.' as a wildcard character
+def test_search_wildcard_character():
     word_dict = WordDictionary()
-    word_dict.addWord("example")
-    assert word_dict.search("exa.ple") == True
+    word_dict.addWord("apple")
+    word_dict.addWord("banana")
+    word_dict.addWord("cherry")
+    assert word_dict.search("a..le") == True
