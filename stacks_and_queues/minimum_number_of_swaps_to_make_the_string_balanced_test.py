ed6ba8a97330120ddb65e919851818c483697c11
Anh Lai <laingocanh1990@gmail.com>
2024-10-08 08:14:36 +0700

    Approach 2: Space-Optimized Stack

--- stacks_and_queues/minimum_number_of_swaps_to_make_the_string_balanced_test.py
+++ stacks_and_queues/minimum_number_of_swaps_to_make_the_string_balanced_test.py
@@ -2,6 +2,9 @@ from .minimum_number_of_swaps_to_make_the_string_balanced import Solution
 
 
 def test_minimum_number_of_swaps_to_make_the_string_balanced():
-    assert Solution().minSwaps("][][") == 1
-    assert Solution().minSwaps("]]][[[") == 2
-    assert Solution().minSwaps("[]") == 0
+    assert Solution().min_swaps_stack("][][") == 1
+    assert Solution().min_swaps_stack("]]][[[") == 2
+    assert Solution().min_swaps_stack("[]") == 0
+    assert Solution().min_swaps_space_optimized_stack("][][") == 1
+    assert Solution().min_swaps_space_optimized_stack("]]][[[") == 2
+    assert Solution().min_swaps_space_optimized_stack("[]") == 0
