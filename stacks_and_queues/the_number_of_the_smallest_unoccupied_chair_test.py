972e5659778a67ccffb3b8a2abd3e800a34b115a
Anh Lai <laingocanh1990@gmail.com>
2024-10-11 21:39:12 +0700

    1942. The Number of the Smallest Unoccupied Chair

---
+++ stacks_and_queues/the_number_of_the_smallest_unoccupied_chair_test.py
@@ -0,0 +1,6 @@
+from .the_number_of_the_smallest_unoccupied_chair import Solution
+
+
+def test_the_number_of_the_smallest_unoccupied_chair():
+    assert Solution().smallestChair(times = [[1,4],[2,3],[4,6]], targetFriend = 1) == 1
+    assert Solution().smallestChair(times = [[3,10],[1,5],[2,6]], targetFriend = 0) == 2
