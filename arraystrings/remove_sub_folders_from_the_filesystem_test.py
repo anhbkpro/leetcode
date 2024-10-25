064e7b95b45c5d8ead6037c8872bd60922084cf6
Anh Lai <laingocanh1990@gmail.com>
2024-10-25 16:15:17 +0700

    1233. Remove Sub-Folders from the Filesystem

--- arraystrings/remove_sub_folders_from_the_filesystem_test.py
+++ arraystrings/remove_sub_folders_from_the_filesystem_test.py
@@ -5,3 +5,6 @@ def test_remove_sub_folders_from_the_filesystem():
     assert sorted(
         Solution().removeSubfolders(folder=["/a", "/a/b", "/c/d", "/c/d/e", "/c/f"])
     ) == ["/a", "/c/d", "/c/f"]
+    assert sorted(Solution().removeSubfolders(folder=["/a", "/a/b/c", "/a/b/d"])) == [
+        "/a"
+    ]
