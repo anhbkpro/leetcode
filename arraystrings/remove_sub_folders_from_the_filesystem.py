87917bca6ff7fc1e4a86dc217de628de5fd76195
Anh Lai <laingocanh1990@gmail.com>
2024-10-26 06:17:25 +0700

    1233. Remove Sub-Folders from the Filesystem - Using Set

--- arraystrings/remove_sub_folders_from_the_filesystem.py
+++ arraystrings/remove_sub_folders_from_the_filesystem.py
@@ -1,20 +1,29 @@
-from typing import List
-
-
 class Solution:
-    def removeSubfolders(self, folder: List[str]) -> List[str]:
-        folder = sorted(folder)
+    def removeSubfolders(self, folder) -> list[str]:
+        # Create a set to store all folder paths for fast lookup
+        folder_set = set(folder)
+        result = []
 
-        parent_set = set()
-        sub_folders = set()
+        # Iterate through each folder to check if it's a sub-folder
         for f in folder:
-            splited_folders = f.split("/")
-            for i in range(1, len(f)):
-                prefix = "/".join(splited_folders[:i])
-                if prefix in parent_set:
-                    sub_folders.add(f)
-                    break
-            if f not in sub_folders:
-                parent_set.add(f)
+            is_sub_folder = False
+            prefix = f
 
-        return list(parent_set)
+            # Check all prefixes of the current folder path
+            while not prefix == "":
+                pos = prefix.rfind("/")
+                if pos == -1:
+                    break
+
+                # Reduce the prefix to its parent folder
+                prefix = prefix[0:pos]
+
+                # If the parent folder exists in the set, mark as sub-folder
+                if prefix in folder_set:
+                    is_sub_folder = True
+                    break
+
+            # If not a sub-folder, add it to the result
+            if not is_sub_folder:
+                result.append(f)
+        return result
