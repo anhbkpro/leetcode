66325bf3c00c6f2c07c0187ae2711b0decb60087
Anh Lai <laingocanh1990@gmail.com>
2024-10-26 07:08:48 +0700

    1233. Remove Sub-Folders from the Filesystem

--- arraystrings/remove_sub_folders_from_the_filesystem_test.py
+++ arraystrings/remove_sub_folders_from_the_filesystem_test.py
@@ -1,16 +1,29 @@
-from .remove_sub_folders_from_the_filesystem import SolutionSet, SolutionSort
+from .remove_sub_folders_from_the_filesystem import (
+    SolutionSet,
+    SolutionSort,
+    SolutionTrie,
+)
 
 
 def test_remove_sub_folders_from_the_filesystem():
+    # Test cases for SolutionSet
     assert sorted(
         SolutionSet().removeSubfolders(folder=["/a", "/a/b", "/c/d", "/c/d/e", "/c/f"])
     ) == ["/a", "/c/d", "/c/f"]
     assert sorted(
         SolutionSet().removeSubfolders(folder=["/a", "/a/b/c", "/a/b/d"])
     ) == ["/a"]
+    # Test cases for SolutionSort
     assert sorted(
         SolutionSort().removeSubfolders(folder=["/a", "/a/b", "/c/d", "/c/d/e", "/c/f"])
     ) == ["/a", "/c/d", "/c/f"]
     assert sorted(
         SolutionSort().removeSubfolders(folder=["/a", "/a/b/c", "/a/b/d"])
+    ) == ["/a"]
+    # Test cases for SolutionTrie
+    assert sorted(
+        SolutionTrie().removeSubfolders(folder=["/a", "/a/b", "/c/d", "/c/d/e", "/c/f"])
+    ) == ["/a", "/c/d", "/c/f"]
+    assert sorted(
+        SolutionTrie().removeSubfolders(folder=["/a", "/a/b/c", "/a/b/d"])
     ) == ["/a"]
