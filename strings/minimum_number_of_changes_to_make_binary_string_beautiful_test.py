3bb518bc8b589c53ebd0c3decf3a52c21d678ece
Anh Lai <laingocanh1990@gmail.com>
2024-11-05 20:47:09 +0700

    2914. Minimum Number of Changes to Make Binary String Beautiful - Greedy

--- strings/minimum_number_of_changes_to_make_binary_string_beautiful_test.py
+++ strings/minimum_number_of_changes_to_make_binary_string_beautiful_test.py
@@ -1,6 +1,11 @@
-from .minimum_number_of_changes_to_make_binary_string_beautiful import Solution
+from .minimum_number_of_changes_to_make_binary_string_beautiful import (
+    Solution,
+    GreedySolution,
+)
 
 
 def test_minimum_number_of_changes_to_make_binary_string_beautiful():
     assert Solution().minChanges(s="1001") == 2
     assert Solution().minChanges(s="10") == 1
+    assert GreedySolution().minChanges(s="1001") == 2
+    assert GreedySolution().minChanges(s="10") == 1
