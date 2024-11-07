2eeb3e1e32d350e636d639de0c2f77f9e634e10a
Anh Lai <laingocanh1990@gmail.com>
2024-11-07 12:38:11 +0700

    Approach 1: Using a Bit Count Array

--- arraystrings/largest_combination_with_bitwise_and_greater_than_zero.py
+++ arraystrings/largest_combination_with_bitwise_and_greater_than_zero.py
@@ -12,3 +12,12 @@ class Solution:
                 if c != "0":
                     m[n - i] = m.get(n - i, 0) + 1
         return max(m.values())
+
+class SolutionBitCount:
+    def largestCombination(self, candidates: List[int]) -> int:
+        bit_count = [0] * 24
+        for i in range(24):
+            for num in candidates:
+                if (num & (1 << i)) != 0:
+                    bit_count[i] += 1
+        return max(bit_count)
--- arraystrings/largest_combination_with_bitwise_and_greater_than_zero_test.py
+++ arraystrings/largest_combination_with_bitwise_and_greater_than_zero_test.py
@@ -1,5 +1,6 @@
-from .largest_combination_with_bitwise_and_greater_than_zero import Solution
+from .largest_combination_with_bitwise_and_greater_than_zero import Solution, SolutionBitCount
 
 
 def test_largest_combination_with_bitwise_and_greater_than_zero():
     assert Solution().largestCombination(candidates=[16, 17, 71, 62, 12, 24, 14]) == 4
+    assert SolutionBitCount().largestCombination(candidates=[16, 17, 71, 62, 12, 24, 14]) == 4
