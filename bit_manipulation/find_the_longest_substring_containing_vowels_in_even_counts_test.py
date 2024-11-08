2a7715ba8414cbefb26f8c418598700cd598daf1
Anh Lai <laingocanh1990@gmail.com>
2024-11-08 21:59:54 +0700

    1829. Maximum XOR for Each Query

---
+++ bit_manipulation/find_the_longest_substring_containing_vowels_in_even_counts_test.py
@@ -0,0 +1,5 @@
+from .find_the_longest_substring_containing_vowels_in_even_counts import Solution
+
+
+def test_find_the_longest_substring():
+    assert Solution().findTheLongestSubstring("eleetminicoworoep") == 13
