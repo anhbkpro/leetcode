d8fc990ed014d05ad4cb37453eb4d84abbc4e056
Anh Lai <laingocanh1990@gmail.com>
2024-12-09 21:27:25 +0700

    3152. Special Array II

--- binary_search/special_array_ii.py
+++ binary_search/special_array_ii.py
@@ -1,7 +1,7 @@
 from typing import List, Tuple
 
 
-class Solution:
+class SolutionBinarySearch:
     def isArraySpecial(
         self, nums: List[int], queries: List[Tuple[int, int]]
     ) -> List[bool]:
@@ -45,3 +45,26 @@ class Solution:
                 return True
 
         return False
+
+
+class SolutionPrefixSum:
+    def isArraySpecial(self, nums: List[int], queries: List[List[int]]) -> List[bool]:
+        ans = [False] * len(queries)
+        prefix = [0] * len(nums)
+        prefix[0] = 0
+
+        for i in range(1, len(nums)):
+            if nums[i] % 2 == nums[i - 1] % 2:
+                # new violative index found
+                prefix[i] = prefix[i - 1] + 1
+            else:
+                prefix[i] = prefix[i - 1]
+
+        for i in range(len(queries)):
+            query = queries[i]
+            start = query[0]
+            end = query[1]
+
+            ans[i] = prefix[end] - prefix[start] == 0
+
+        return ans
--- binary_search/special_array_ii_test.py
+++ binary_search/special_array_ii_test.py
@@ -1,9 +1,22 @@
-from .special_array_ii import Solution
+from .special_array_ii import SolutionBinarySearch, SolutionPrefixSum
 
 
 def test_is_array_special():
-    assert Solution().isArraySpecial(nums=[3, 4, 1, 2, 6], queries=[[0, 4]]) == [False]
-    assert Solution().isArraySpecial(nums=[4, 3, 1, 6], queries=[[0, 2], [2, 3]]) == [
+    assert SolutionBinarySearch().isArraySpecial(
+        nums=[3, 4, 1, 2, 6], queries=[[0, 4]]
+    ) == [False]
+    assert SolutionBinarySearch().isArraySpecial(
+        nums=[4, 3, 1, 6], queries=[[0, 2], [2, 3]]
+    ) == [
+        False,
+        True,
+    ]
+    assert SolutionPrefixSum().isArraySpecial(
+        nums=[3, 4, 1, 2, 6], queries=[[0, 4]]
+    ) == [False]
+    assert SolutionPrefixSum().isArraySpecial(
+        nums=[4, 3, 1, 6], queries=[[0, 2], [2, 3]]
+    ) == [
         False,
         True,
     ]
