5c487b3f8055f5ea80b313edf13a4e34e81ced1a
Anh Lai <laingocanh1990@gmail.com>
2024-12-10 20:48:22 +0700

    2981. Find Longest Special Substring That Occurs Thrice I

---
+++ hashing/find_longest_special_substring_that_occurs_thrice_i_test.py
@@ -0,0 +1,5 @@
+from .find_longest_special_substring_that_occurs_thrice_i import Solution
+
+
+def test_find_longest_special_substring_that_occurs_thrice_i():
+    assert Solution().maximumLength(s="aaaa") == 2
