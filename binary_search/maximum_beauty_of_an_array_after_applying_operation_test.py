d8022398e50ba945f5e8984914647cf4e1dfeeec
Anh Lai <laingocanh1990@gmail.com>
2024-12-11 23:20:03 +0700

    2779. Maximum Beauty of an Array After Applying Operation

---
+++ binary_search/maximum_beauty_of_an_array_after_applying_operation_test.py
@@ -0,0 +1,5 @@
+from .maximum_beauty_of_an_array_after_applying_operation import Solution
+
+
+def test_maximum_beauty_of_an_array_after_applying_operation():
+    assert Solution().maximumBeauty(nums=[4, 6, 1, 2], k=2) == 3
