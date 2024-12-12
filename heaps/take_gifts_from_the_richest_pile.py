36b795a254216b3b642a75f7275fe6c90e1779ee
Anh Lai <laingocanh1990@gmail.com>
2024-12-12 10:39:49 +0700

    remove unused var

--- heaps/take_gifts_from_the_richest_pile.py
+++ heaps/take_gifts_from_the_richest_pile.py
@@ -9,9 +9,9 @@ class Solution:
         for gift in gifts:
             heapq.heappush(gifts_heap, -gift)
 
-        for i in range(k):
-            item = heapq.heappop(gifts_heap)
-            left_gift = floor(sqrt(-item))
+        for _ in range(k):
+            item = -heapq.heappop(gifts_heap)
+            left_gift = floor(sqrt(item))
             heapq.heappush(gifts_heap, -left_gift)
 
         return -sum(gifts_heap)
