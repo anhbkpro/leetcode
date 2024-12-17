47e3ae38eee65c3a478dc7631f6377ad60efeb59
Anh Lai <laingocanh1990@gmail.com>
2024-12-17 20:35:30 +0700

    fix test

--- heaps/construct_string_with_repeat_limit_test.py
+++ heaps/construct_string_with_repeat_limit_test.py
@@ -2,5 +2,5 @@ from .construct_string_with_repeat_limit import Solution
 
 
 def test_repeat_limited_string():
-    assert Solution().constructString(s="cczazcc", repeatLimit=3) == "zzcccac"
-    assert Solution().constructString(s="aababab", repeatLimit=2) == "bbabaa"
+    assert Solution().repeatLimitedString(s="cczazcc", repeatLimit=3) == "zzcccac"
+    assert Solution().repeatLimitedString(s="aababab", repeatLimit=2) == "bbabaa"
