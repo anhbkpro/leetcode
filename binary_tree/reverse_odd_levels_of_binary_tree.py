ce6fdca42e5b74f74d117a02cc657bb550e39a9f
Anh Lai <laingocanh1990@gmail.com>
2024-12-20 13:19:54 +0700

    2415. Reverse Odd Levels of Binary Tree

--- binary_tree/reverse_odd_levels_of_binary_tree.py
+++ binary_tree/reverse_odd_levels_of_binary_tree.py
@@ -9,7 +9,7 @@ class TreeNode:
         self.right = right
 
 
-class Solution:
+class SolutionDFS:
     def __traverse_DFS(self, left_child, right_child, level):
         if not left_child or not right_child:
             return
@@ -22,3 +22,42 @@ class Solution:
     def reverseOddLevels(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         self.__traverse_DFS(root.left, root.right, 0)
         return root
+
+
+class SolutionBFS:
+    def reverseOddLevels(self, root):
+        if not root:
+            return None  # Return None if the tree is empty.
+
+        queue = [root]  # Start BFS with the root node.
+        level = 0
+
+        while queue:
+            size = len(queue)
+            current_level_nodes = []
+
+            # Process all nodes at the current level.
+            for _ in range(size):
+                node = queue.pop(0)
+                current_level_nodes.append(node)
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+            # Reverse node values if the current level is odd.
+            if level % 2 == 1:
+                left, right = 0, len(current_level_nodes) - 1
+                while left < right:
+                    tmp = current_level_nodes[left].val
+                    current_level_nodes[left].val = current_level_nodes[
+                        right
+                    ].val
+                    current_level_nodes[right].val = tmp
+                    left += 1
+                    right -= 1
+
+            level += 1
+
+        return root  # Return the modified tree root.
--- binary_tree/reverse_odd_levels_of_binary_tree_test.py
+++ binary_tree/reverse_odd_levels_of_binary_tree_test.py
@@ -1,4 +1,4 @@
-from .reverse_odd_levels_of_binary_tree import TreeNode, Solution
+from .reverse_odd_levels_of_binary_tree import TreeNode, SolutionDFS, SolutionBFS
 
 
 def test_reverse_odd_levels():
@@ -8,10 +8,20 @@ def test_reverse_odd_levels():
     root.left = TreeNode(3)
     root.right = TreeNode(5)
 
-    result = Solution().reverseOddLevels(root=root)
-    assert result.val == 2
-    assert result.left.val == 5
-    assert result.right.val == 3
+    dfs_result = SolutionDFS().reverseOddLevels(root=root)
+    assert dfs_result.val == 2
+    assert dfs_result.left.val == 5
+    assert dfs_result.right.val == 3
+
+    root = TreeNode(2)
+    root.left = TreeNode(3)
+    root.right = TreeNode(5)
+    bfs_result = SolutionBFS().reverseOddLevels(root=root)
+    assert bfs_result.val == 2
+    assert bfs_result.left.val == 5
+    assert bfs_result.right.val == 3
+
+
 
 
 
