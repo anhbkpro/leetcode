4ff350019e27dced6f028d52f85af1f4142d3f42
Anh Lai <laingocanh1990@gmail.com>
2025-01-20 09:42:46 +0700

    explicit type for variable

--- hashing/first_completely_painted_row_or_column.py
+++ hashing/first_completely_painted_row_or_column.py
@@ -1,16 +1,21 @@
-from typing import List
+from typing import List, Dict, Tuple
 
 
 class Solution:
     def firstCompleteIndex(self, arr: List[int], mat: List[List[int]]) -> int:
-        m = len(mat)
-        n = len(mat[0])
-        rows = [0] * m
-        cols = [0] * n
-        num_to_pos = {}
-        for r in range(m):
-            for c in range(n):
-                num_to_pos[mat[r][c]] = (r, c)
+        """
+        Finds the first index in the array where either a complete row or column
+        in the matrix has been marked.
+
+        :param arr: List of integers representing the order of marking.
+        :param mat: 2D list representing the matrix.
+        :return: The first index where a row or column is completely filled, or -1 if none.
+        """
+        m, n = len(mat), len(mat[0])
+        rows, cols = [0] * m, [0] * n
+        num_to_pos: Dict[int, Tuple[int, int]] = {
+            mat[r][c]: (r, c) for r in range(m) for c in range(n)
+        }
 
         for i, num in enumerate(arr):
             r, c = num_to_pos[num]
