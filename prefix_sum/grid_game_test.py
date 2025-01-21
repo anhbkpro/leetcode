1a25991c6591c390dfdc916207a2fc96f760f8b5
Anh Lai <laingocanh1990@gmail.com>
2025-01-21 21:43:32 +0700

    2017. Grid Game

---
+++ prefix_sum/grid_game_test.py
@@ -0,0 +1,66 @@
+import unittest
+from typing import List
+from .grid_game import Solution
+
+
+class Solution:
+    def gridGame(self, grid: List[List[int]]) -> int:
+        first_row_sum = sum(grid[0])
+        second_row_sum = 0
+        minimum_sum = float("inf")
+        for turn_index in range(len(grid[0])):
+            first_row_sum -= grid[0][turn_index]
+            minimum_sum = min(minimum_sum, max(first_row_sum, second_row_sum))
+            second_row_sum += grid[1][turn_index]
+        return minimum_sum
+
+
+class TestGridGame(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+
+    def test_basic_case(self):
+        grid = [[2, 5, 4], [1, 5, 1]]
+        expected = 4
+        result = self.solution.gridGame(grid)
+        self.assertEqual(result, expected)
+
+    def test_single_column(self):
+        grid = [[5], [2]]
+        expected = 0
+        result = self.solution.gridGame(grid)
+        self.assertEqual(result, expected)
+
+    def test_all_zeros(self):
+        grid = [[0, 0, 0], [0, 0, 0]]
+        expected = 0
+        result = self.solution.gridGame(grid)
+        self.assertEqual(result, expected)
+
+    def test_large_numbers(self):
+        grid = [[1000000, 1000000], [1000000, 1000000]]
+        expected = 1000000
+        result = self.solution.gridGame(grid)
+        self.assertEqual(result, expected)
+
+    def test_asymmetric_grid(self):
+        grid = [[1, 3, 1, 15], [1, 3, 3, 1]]
+        expected = 7
+        result = self.solution.gridGame(grid)
+        self.assertEqual(result, expected)
+
+    def test_equal_paths(self):
+        grid = [[3, 3, 3], [3, 3, 3]]
+        expected = 3
+        result = self.solution.gridGame(grid)
+        self.assertEqual(result, expected)
+
+    def test_large_grid(self):
+        grid = [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]]
+        expected = 40
+        result = self.solution.gridGame(grid)
+        self.assertEqual(result, expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
