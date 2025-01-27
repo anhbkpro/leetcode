64fc82205a2215a13ac09db73a48f7ee81fb85a5
Anh Lai <laingocanh1990@gmail.com>
2025-01-27 08:49:06 +0700

    1462. Course Schedule IV

--- graph/course_schedule_iv.py
+++ graph/course_schedule_iv.py
@@ -1,7 +1,7 @@
 from typing import List
 
 
-class Solution:
+class SolutionTreeTraversalOnDemand:
     # Performs DFS and returns true if there's a path between src and target and false otherwise.
     def isPrerequisite(
         self, adjList: dict, visited: List[bool], src: int, target: int
@@ -38,3 +38,119 @@ class Solution:
             )
 
         return result
+
+class SolutionTreeTraversalPreprocessed:
+    # Iterate over each node and perform BFS starting from it.
+    # Mark the starting node as a prerequisite to all the nodes in the BFS
+    # traversal.
+    def preprocess(
+        self,
+        numCourses: int,
+        prerequisites: List[List[int]],
+        adjList: dict[int, List[int]],
+        isPrerequisite: List[List[bool]],
+    ) -> None:
+        for i in range(numCourses):
+            q = deque([i])
+
+            while q:
+                node = q.popleft()
+
+                for adj in adjList.get(node, []):
+                    # If we have marked i as a prerequisite of adj it implies we
+                    # have visited it. This is equivalent to using a visited
+                    # array.
+                    if not isPrerequisite[i][adj]:
+                        isPrerequisite[i][adj] = True
+                        q.append(adj)
+
+    def checkIfPrerequisite(
+        self,
+        numCourses: int,
+        prerequisites: List[List[int]],
+        queries: List[List[int]],
+    ) -> List[bool]:
+        adjList = {}
+        for edge in prerequisites:
+            if edge[0] not in adjList:
+                adjList[edge[0]] = []
+            adjList[edge[0]].append(edge[1])
+
+        isPrerequisite = [[False] * numCourses for _ in range(numCourses)]
+        self.preprocess(numCourses, prerequisites, adjList, isPrerequisite)
+
+        answer = []
+        for query in queries:
+            answer.append(isPrerequisite[query[0]][query[1]])
+
+        return answer
+
+
+class SolutionTopologicalSortKahnAlgorithm:
+    def checkIfPrerequisite(
+        self,
+        numCourses: int,
+        prerequisites: List[List[int]],
+        queries: List[List[int]],
+    ) -> List[bool]:
+        adjList = defaultdict(list)
+        indegree = [0] * numCourses
+
+        for edge in prerequisites:
+            adjList[edge[0]].append(edge[1])
+            indegree[edge[1]] += 1
+
+        q = deque()
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                q.append(i)
+
+        nodePrerequisites = defaultdict(set)
+
+        while q:
+            node = q.popleft()
+
+            for adj in adjList[node]:
+                # Add node and prerequisite of the node to the prerequisites of adj
+                nodePrerequisites[adj].add(node)
+                for prereq in nodePrerequisites[node]:
+                    nodePrerequisites[adj].add(prereq)
+
+                indegree[adj] -= 1
+                if indegree[adj] == 0:
+                    q.append(adj)
+
+        answer = []
+        for q in queries:
+            answer.append(q[0] in nodePrerequisites[q[1]])
+
+        return answer
+
+class SolutionFloydWarshallAlgorithm:
+    def checkIfPrerequisite(
+        self,
+        numCourses: int,
+        prerequisites: List[List[int]],
+        queries: List[List[int]],
+    ) -> List[bool]:
+        isPrerequisite = [[False] * numCourses for _ in range(numCourses)]
+
+        for edge in prerequisites:
+            isPrerequisite[edge[0]][edge[1]] = True
+
+        for intermediate in range(numCourses):
+            for src in range(numCourses):
+                for target in range(numCourses):
+                    # If there is a path src -> intermediate and intermediate -> target, then src -> target exists as well
+                    isPrerequisite[src][target] = isPrerequisite[src][
+                        target
+                    ] or (
+                        isPrerequisite[src][intermediate]
+                        and isPrerequisite[intermediate][target]
+                    )
+
+        answer = []
+        for query in queries:
+            answer.append(isPrerequisite[query[0]][query[1]])
+
+        return answer
