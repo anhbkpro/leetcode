d71eccf49514345d9918091b83307bb3497f575a
Anh Lai <laingocanh1990@gmail.com>
2025-02-03 08:20:47 +0700

    combine var init

--- arraystrings/longest_strictly_increasing_or_strictly_decreasing_subarray.py
+++ arraystrings/longest_strictly_increasing_or_strictly_decreasing_subarray.py
@@ -3,9 +3,7 @@ from typing import List
 
 class Solution:
     def longestMonotonicSubarray(self, nums: List[int]) -> int:
-        max_length = 1
-        dec_length = 1
-        inc_length = 1
+        max_length, dec_length, inc_length = 1, 1, 1
         for i in range(1, len(nums)):
             if nums[i] < nums[i - 1]:
                 dec_length += 1
