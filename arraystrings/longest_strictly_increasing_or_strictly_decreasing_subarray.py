dce5b6d2ae2310f85bae347a3378a33ae9a96a8f
Anh Lai <laingocanh1990@gmail.com>
2025-02-03 08:19:39 +0700

    rename var

--- arraystrings/longest_strictly_increasing_or_strictly_decreasing_subarray.py
+++ arraystrings/longest_strictly_increasing_or_strictly_decreasing_subarray.py
@@ -4,18 +4,18 @@ from typing import List
 class Solution:
     def longestMonotonicSubarray(self, nums: List[int]) -> int:
         max_length = 1
-        local_decrease = 1
-        local_increase = 1
+        dec_length = 1
+        inc_length = 1
         for i in range(1, len(nums)):
             if nums[i] < nums[i - 1]:
-                local_decrease += 1
-                local_increase = 1
+                dec_length += 1
+                inc_length = 1
             elif nums[i] > nums[i - 1]:
-                local_decrease = 1
-                local_increase += 1
+                dec_length = 1
+                inc_length += 1
             else:
-                local_increase = 1
-                local_decrease = 1
-            max_length = max(max_length, local_decrease, local_increase)
+                inc_length = 1
+                dec_length = 1
+            max_length = max(max_length, dec_length, inc_length)
 
         return max_length
