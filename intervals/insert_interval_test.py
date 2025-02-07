b99e0c5f1f88141f7a29a002ff4fa19adf730ac9
Anh Lai <laingocanh1990@gmail.com>
2025-02-07 10:56:45 +0700

    fix test 57. Insert Interval

--- intervals/insert_interval_test.py
+++ intervals/insert_interval_test.py
@@ -1,12 +1,19 @@
 import unittest
+from abc import ABC, abstractmethod
 from .insert_interval import SolutionLinearSearch, SolutionBinarySearch
 
 
-class TestInsertIntervalBase:
-    """Base test class for insert interval implementations"""
+class TestInsertIntervalBase(ABC):
+    """Abstract base test class for insert interval implementations"""
+
+    @property
+    @abstractmethod
+    def SolutionClass(self):
+        """Return the solution class to test"""
+        pass
 
     def setUp(self):
-        self.solution = self.get_solution()
+        self.solution = self.SolutionClass()
 
     def test_example_1(self):
         """Test example case with insertion in the middle"""
@@ -25,8 +32,7 @@ class TestInsertIntervalBase:
         new_interval = [4, 8]
         expected = [[1, 2], [3, 10], [12, 16]]
         self.assertEqual(
-            self.solution.insert(intervals.copy(), new_interval.copy()),
-            expected
+            self.solution.insert(intervals.copy(), new_interval.copy()), expected
         )
 
     def test_empty_intervals(self):
@@ -35,8 +41,7 @@ class TestInsertIntervalBase:
         new_interval = [5, 7]
         expected = [[5, 7]]
         self.assertEqual(
-            self.solution.insert(intervals.copy(), new_interval.copy()),
-            expected
+            self.solution.insert(intervals.copy(), new_interval.copy()), expected
         )
 
     def test_insert_at_start(self):
@@ -45,8 +50,7 @@ class TestInsertIntervalBase:
         new_interval = [1, 2]
         expected = [[1, 2], [3, 5], [6, 8]]
         self.assertEqual(
-            self.solution.insert(intervals.copy(), new_interval.copy()),
-            expected
+            self.solution.insert(intervals.copy(), new_interval.copy()), expected
         )
 
     def test_insert_at_end(self):
@@ -55,8 +59,7 @@ class TestInsertIntervalBase:
         new_interval = [6, 8]
         expected = [[1, 2], [3, 5], [6, 8]]
         self.assertEqual(
-            self.solution.insert(intervals.copy(), new_interval.copy()),
-            expected
+            self.solution.insert(intervals.copy(), new_interval.copy()), expected
         )
 
     def test_complete_overlap(self):
@@ -65,8 +68,7 @@ class TestInsertIntervalBase:
         new_interval = [2, 5]
         expected = [[1, 6]]
         self.assertEqual(
-            self.solution.insert(intervals.copy(), new_interval.copy()),
-            expected
+            self.solution.insert(intervals.copy(), new_interval.copy()), expected
         )
 
     def test_no_overlap(self):
@@ -75,8 +77,7 @@ class TestInsertIntervalBase:
         new_interval = [3, 3]
         expected = [[1, 2], [3, 3], [4, 5], [7, 8]]
         self.assertEqual(
-            self.solution.insert(intervals.copy(), new_interval.copy()),
-            expected
+            self.solution.insert(intervals.copy(), new_interval.copy()), expected
         )
 
     def test_single_point_overlap(self):
@@ -85,8 +86,7 @@ class TestInsertIntervalBase:
         new_interval = [3, 6]
         expected = [[1, 9]]
         self.assertEqual(
-            self.solution.insert(intervals.copy(), new_interval.copy()),
-            expected
+            self.solution.insert(intervals.copy(), new_interval.copy()), expected
         )
 
     def test_negative_numbers(self):
@@ -95,8 +95,7 @@ class TestInsertIntervalBase:
         new_interval = [-2, 1]
         expected = [[-5, -3], [-2, 2], [4, 6]]
         self.assertEqual(
-            self.solution.insert(intervals.copy(), new_interval.copy()),
-            expected
+            self.solution.insert(intervals.copy(), new_interval.copy()), expected
         )
 
     def test_large_numbers(self):
@@ -105,21 +104,24 @@ class TestInsertIntervalBase:
         new_interval = [900, 2100]
         expected = [[1, 3000]]
         self.assertEqual(
-            self.solution.insert(intervals.copy(), new_interval.copy()),
-            expected
+            self.solution.insert(intervals.copy(), new_interval.copy()), expected
         )
 
 
 class TestInsertIntervalLinearSearch(TestInsertIntervalBase, unittest.TestCase):
     """Test cases for linear search implementation"""
-    def get_solution(self):
-        return SolutionLinearSearch()
+
+    @property
+    def SolutionClass(self):
+        return SolutionLinearSearch
 
 
 class TestInsertIntervalBinarySearch(TestInsertIntervalBase, unittest.TestCase):
     """Test cases for binary search implementation"""
-    def get_solution(self):
-        return SolutionBinarySearch()
+
+    @property
+    def SolutionClass(self):
+        return SolutionBinarySearch
 
     def test_input_modification(self):
         """Test that binary search implementation modifies input as expected"""
