7ba7b8c9dde91a72b33c8b145274c596a97e8e3e
Anh Lai <laingocanh1990@gmail.com>
2025-02-08 20:57:07 +0700

    215. Kth Largest Element in an Array

--- heaps/kth_largest_element_in_an_array.py
+++ heaps/kth_largest_element_in_an_array.py
@@ -4,9 +4,10 @@ from typing import List
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        heap = [num for num in nums]
-        heapq.heapify(heap)
-        for _ in range(len(nums) - k):
-            heapq.heappop(heap)
+        heap = []
+        for num in nums:
+            heapq.heappush(heap, num)
+            if len(heap) > k:
+                heapq.heappop(heap)
 
         return heap[0]
--- heaps/kth_largest_element_in_an_array_test.py
+++ heaps/kth_largest_element_in_an_array_test.py
@@ -1,6 +1,23 @@
+import unittest
 from heaps.kth_largest_element_in_an_array import Solution
 
 
-def test_findKthLargest():
-    assert Solution().findKthLargest([3, 2, 1, 5, 6, 4], 2) == 5
-    assert Solution().findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4) == 4
+class TestFindKthLargest(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+
+    def test_example_1(self):
+        nums = [3, 2, 1, 5, 6, 4]
+        k = 2
+        expected = 5
+        self.assertEqual(self.solution.findKthLargest(nums, k), expected)
+
+    def test_example_2(self):
+        nums = [3, 2, 3, 1, 2, 4, 5, 5, 6]
+        k = 4
+        expected = 4
+        self.assertEqual(self.solution.findKthLargest(nums, k), expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
