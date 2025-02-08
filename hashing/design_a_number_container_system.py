f0390a9bf7e344240abf33bd55ac2dbb1e78768b
Anh Lai <laingocanh1990@gmail.com>
2025-02-08 16:35:50 +0700

    2349. Design a Number Container System

---
+++ hashing/design_a_number_container_system.py
@@ -0,0 +1,29 @@
+from collections import defaultdict
+from sortedcontainers import SortedSet
+
+
+class NumberContainers:
+    def __init__(self):
+        self.number_to_indices = defaultdict(SortedSet)
+        self.index_to_number = {}
+
+    def change(self, index: int, number: int) -> None:
+        if index in self.index_to_number:
+            prev_number = self.index_to_number[index]
+            self.number_to_indices[prev_number].remove(index)
+            if not self.number_to_indices[prev_number]:
+                del self.number_to_indices[prev_number]
+
+        self.index_to_number[index] = number
+        self.number_to_indices[number].add(index)
+
+    def find(self, number: int) -> int:
+        if number not in self.number_to_indices:
+            return -1
+        return self.number_to_indices[number][0]
+
+
+# Your NumberContainers object will be instantiated and called as such:
+# obj = NumberContainers()
+# obj.change(index,number)
+# param_2 = obj.find(number)
---
+++ hashing/design_a_number_container_system_test.py
@@ -0,0 +1,30 @@
+import unittest
+from .design_a_number_container_system import NumberContainers
+
+
+class TestNumberContainers(unittest.TestCase):
+    def setUp(self):
+        self.obj = NumberContainers()
+
+    def test_example_1(self):
+        self.obj.change(1, 1)
+        self.obj.change(2, 2)
+        self.assertEqual(self.obj.find(1), 1)
+        self.assertEqual(self.obj.find(2), 2)
+        self.obj.change(1, 2)
+        self.assertEqual(self.obj.find(1), -1)
+        self.assertEqual(self.obj.find(2), 1)
+
+    def test_example_2(self):
+        self.assertEqual(self.obj.find(10), -1)
+        self.obj.change(2, 10)
+        self.obj.change(1, 10)
+        self.obj.change(3, 10)
+        self.obj.change(5, 10)
+        self.assertEqual(self.obj.find(10), 1)
+        self.obj.change(1, 20)
+        self.assertEqual(self.obj.find(10), 2)
+
+
+if __name__ == "__main__":
+    unittest.main()
