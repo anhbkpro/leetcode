8426b330ca87d40288bc078d7b0f6b5e31496e0c
Anh Lai <laingocanh1990@gmail.com>
2025-02-18 22:35:08 +0700

    2375. Construct Smallest Number From DI String

--- tests/test_construct_smallest_number_from_di_string.py
+++ tests/test_construct_smallest_number_from_di_string.py
@@ -6,29 +6,37 @@ class TestConstructSmallestNumberFromDIString(unittest.TestCase):
     def setUp(self):
         self.solution = Solution()
 
+    def test_example_1(self):
+        """Test with example 1"""
+        self.assertEqual(self.solution.smallestNumber(pattern="IIIDIDDD"), "123549876")
+
+    def test_example_2(self):
+        """Test with example 2"""
+        self.assertEqual(self.solution.smallestNumber(pattern="DDD"), "4321")
+
     def test_single_increasing(self):
         """Test with single 'I' pattern"""
-        self.assertEqual(self.solution.smallestNumber("I"), "12")
+        self.assertEqual(self.solution.smallestNumber(pattern="I"), "12")
 
     def test_single_decreasing(self):
         """Test with single 'D' pattern"""
-        self.assertEqual(self.solution.smallestNumber("D"), "21")
+        self.assertEqual(self.solution.smallestNumber(pattern="D"), "21")
 
     def test_all_increasing(self):
         """Test with all increasing pattern 'III'"""
-        self.assertEqual(self.solution.smallestNumber("III"), "1234")
+        self.assertEqual(self.solution.smallestNumber(pattern="III"), "1234")
 
     def test_all_decreasing(self):
         """Test with all decreasing pattern 'DDD'"""
-        self.assertEqual(self.solution.smallestNumber("DDD"), "4321")
+        self.assertEqual(self.solution.smallestNumber(pattern="DDD"), "4321")
 
     def test_mixed_pattern(self):
         """Test with mixed pattern 'IDIDID'"""
-        self.assertEqual(self.solution.smallestNumber("IDIDID"), "1325476")
+        self.assertEqual(self.solution.smallestNumber(pattern="IDIDID"), "1325476")
 
     def test_alternating_pattern(self):
         """Test with alternating pattern 'DIDI'"""
-        self.assertEqual(self.solution.smallestNumber("DIDI"), "21435")
+        self.assertEqual(self.solution.smallestNumber(pattern="DIDI"), "21435")
 
 
 if __name__ == "__main__":
