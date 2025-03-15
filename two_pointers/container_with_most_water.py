9be3c9b80c34387af8261841cd6f5626a220a273
AnhL <laingocanh1990@gmail.com>
2025-03-15 18:27:22 +0700

    Add images for 11. Container With Most Water

--- two_pointers/container_with_most_water.py
+++ two_pointers/container_with_most_water.py
@@ -3,17 +3,16 @@ from typing import List
 
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        p_left, p_right = 0, len(height) - 1
-        max_area = 0
-        while p_left < p_right:
-            width = p_right - p_left
-            min_height = min(height[p_left], height[p_right])
-            max_area = max(max_area, width * min_height)
+        maxarea = 0
+        left = 0
+        right = len(height) - 1
 
-            # Move the pointer with smaller height
-            if height[p_left] <= height[p_right]:
-                p_left += 1
+        while left < right:
+            width = right - left
+            maxarea = max(maxarea, min(height[left], height[right]) * width)
+            if height[left] <= height[right]:
+                left += 1
             else:
-                p_right -= 1
+                right -= 1
 
-        return max_area
+        return maxarea
