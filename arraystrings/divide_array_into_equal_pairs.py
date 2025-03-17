6b6a07418d5891fddbcc522d1a0db48f280a14bf
AnhL <laingocanh1990@gmail.com>
2025-03-17 14:33:54 +0700

    2206. Divide Array Into Equal Pairs - use values()

--- arraystrings/divide_array_into_equal_pairs.py
+++ arraystrings/divide_array_into_equal_pairs.py
@@ -5,7 +5,7 @@ from typing import List
 class Solution:
     def divideArray(self, nums: List[int]) -> bool:
         freq = Counter(nums)
-        for _, v in freq.items():
+        for v in freq.values():
             if v % 2:
                 return False
         return True
