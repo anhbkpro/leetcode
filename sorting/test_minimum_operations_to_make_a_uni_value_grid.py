8f9dad580eb4bfd33b28f15cbb702e3424090eb4
AnhL <laingocanh1990@gmail.com>
2025-03-26 22:43:08 +0700

    2033. Minimum Operations to Make a Uni-Value Grid

---
+++ sorting/test_minimum_operations_to_make_a_uni_value_grid.py
@@ -0,0 +1,29 @@
+import unittest
+from .minimum_operations_to_make_a_uni_value_grid import Solution
+
+
+class TestMinOperations(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+
+    def test_sample_case(self):
+        grid = [[2, 4], [6, 8]]
+        x = 2
+        result = self.solution.minOperations(grid, x)
+        self.assertEqual(result, 4)
+
+    def test_sample_case_2(self):
+        grid = [[1, 5], [2, 3]]
+        x = 1
+        result = self.solution.minOperations(grid, x)
+        self.assertEqual(result, 5)
+
+    def test_sample_case_3(self):
+        grid = [[1, 2], [3, 4]]
+        x = 2
+        result = self.solution.minOperations(grid, x)
+        self.assertEqual(result, -1)
+
+
+if __name__ == "__main__":
+    unittest.main()
