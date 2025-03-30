48d35fc8f7951920f878d26a23c7f000a781f8c8
AnhL <laingocanh1990@gmail.com>
2025-03-30 20:21:59 +0700

    763. Partition Labels - TwoPointers

--- hashing/partition_labels.py
+++ hashing/partition_labels.py
@@ -1,7 +1,7 @@
 from typing import List
 
 
-class Solution:
+class MergeIntervals:
     def partitionLabels(self, s: str) -> List[int]:
         if not s:
             return []
@@ -22,3 +22,24 @@ class Solution:
             else:
                 merged.append([start, end])
         return [end - start + 1 for start, end in merged]
+
+
+class TwoPointers:
+    def partitionLabels(self, s: str) -> List[int]:
+        # Stores the last index of each character in 's'
+        last_occurrence = [0] * 26
+        for i, char in enumerate(s):
+            last_occurrence[ord(char) - ord("a")] = i
+
+        partition_end = 0
+        partition_start = 0
+        partition_sizes = []
+
+        for i, char in enumerate(s):
+            partition_end = max(partition_end, last_occurrence[ord(char) - ord("a")])
+            # End of the current partition
+            if i == partition_end:
+                partition_sizes.append(i - partition_start + 1)
+                partition_start = i + 1
+
+        return partition_sizes
---
+++ hashing/test_partition_labels_implementations.py
@@ -0,0 +1,90 @@
+import pytest
+from partition_labels import MergeIntervals, TwoPointers
+
+
+class TestMergeIntervals:
+    @pytest.fixture
+    def solution(self):
+        return MergeIntervals()
+
+    def test_example1(self, solution):
+        s = "ababcbacadefegdehijhklij"
+        assert solution.partitionLabels(s) == [9, 7, 8]
+
+    def test_example2(self, solution):
+        s = "eccbbbbdec"
+        assert solution.partitionLabels(s) == [10]
+
+    def test_single_char(self, solution):
+        s = "a"
+        assert solution.partitionLabels(s) == [1]
+
+    def test_all_same(self, solution):
+        s = "aaa"
+        assert solution.partitionLabels(s) == [3]
+
+    def test_no_overlap(self, solution):
+        s = "abc"
+        assert solution.partitionLabels(s) == [1, 1, 1]
+
+    def test_empty(self, solution):
+        s = ""
+        assert solution.partitionLabels(s) == []
+
+    def test_complex(self, solution):
+        s = "abacbdefghijklmnopqrstuvwxyz"
+        assert solution.partitionLabels(s) == [5] + [1] * 23
+
+    def test_overlapping(self, solution):
+        s = "ababab"
+        assert solution.partitionLabels(s) == [6]
+
+
+class TestTwoPointers:
+    @pytest.fixture
+    def solution(self):
+        return TwoPointers()
+
+    def test_example1(self, solution):
+        s = "ababcbacadefegdehijhklij"
+        assert solution.partitionLabels(s) == [9, 7, 8]
+
+    def test_example2(self, solution):
+        s = "eccbbbbdec"
+        assert solution.partitionLabels(s) == [10]
+
+    def test_single_char(self, solution):
+        s = "a"
+        assert solution.partitionLabels(s) == [1]
+
+    def test_all_same(self, solution):
+        s = "aaa"
+        assert solution.partitionLabels(s) == [3]
+
+    def test_no_overlap(self, solution):
+        s = "abc"
+        assert solution.partitionLabels(s) == [1, 1, 1]
+
+    def test_empty(self, solution):
+        s = ""
+        assert solution.partitionLabels(s) == []
+
+    def test_complex(self, solution):
+        s = "abacbdefghijklmnopqrstuvwxyz"
+        assert solution.partitionLabels(s) == [5] + [1] * 23
+
+    def test_overlapping(self, solution):
+        s = "ababab"
+        assert solution.partitionLabels(s) == [6]
+
+    def test_all_alphabet(self, solution):
+        s = "abcdefghijklmnopqrstuvwxyz"
+        assert solution.partitionLabels(s) == [1] * 26
+
+    def test_reverse_alphabet(self, solution):
+        s = "zyxwvutsrqponmlkjihgfedcba"
+        assert solution.partitionLabels(s) == [1] * 26
+
+    def test_alternating_pairs(self, solution):
+        s = "aabbccddee"
+        assert solution.partitionLabels(s) == [2] * 5
