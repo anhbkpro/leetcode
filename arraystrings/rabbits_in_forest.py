411608f145dd5143c9a2abd41ee2c400b71bf9e9
AnhL <laingocanh1990@gmail.com>
2025-04-20 22:27:46 +0700

    781. Rabbits in Forest

---
+++ arraystrings/rabbits_in_forest.py
@@ -0,0 +1,7 @@
+import collections
+
+
+class Solution(object):
+    def numRabbits(self, answers):
+        count = collections.Counter(answers)
+        return sum(-v % (k + 1) + v for k, v in count.items())
