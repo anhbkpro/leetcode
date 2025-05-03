98ebcf5659315c99c22a6472a92cde5bc51754fe
AnhL <laingocanh1990@gmail.com>
2025-05-03 11:26:38 +0700

    test: 838. Push Dominoes

---
+++ arraystrings/test_push_dominoes.py
@@ -0,0 +1,25 @@
+import pytest
+from .push_dominoes import Solution
+
+
+@pytest.mark.parametrize(
+    "input_dominoes, expected_output",
+    [
+        (".L.R...LR..L..", "LL.RR.LLRRLL.."),
+        ("RR.L", "RR.L"),
+        (".", "."),
+        ("L", "L"),
+        ("R", "R"),
+        ("", ""),
+        ("R.L", "R.L"),
+        ("...L...R...", "LLLL...RRRR"),
+        ("R...L", "RR.LL"),
+        ("R.R.L", "RRR.L"),
+        ("L.....R", "L.....R"),
+        ("R.....L", "RRR.LLL"),
+    ],
+)
+def test_push_dominoes(input_dominoes: str, expected_output: str):
+    solution = Solution()
+    actual_output = solution.push_dominoes(input_dominoes)
+    assert actual_output == expected_output
