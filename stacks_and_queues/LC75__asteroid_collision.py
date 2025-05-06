0bed19c77368fc782eacf99c5e85395c4f13da9b
AnhL <laingocanh1990@gmail.com>
2025-05-06 08:27:18 +0700

    735. Asteroid Collision

---
+++ stacks_and_queues/LC75__asteroid_collision.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stk = []
+        for asteroid in asteroids:
+            right_hand_asteroid_win = True
+            while stk and stk[-1] > 0 and asteroid < 0:
+                top = stk[-1]
+                if abs(top) < abs(asteroid):
+                    stk.pop()
+                    continue
+                elif abs(top) == abs(asteroid):
+                    stk.pop()
+
+                right_hand_asteroid_win = False
+                break
+
+            if right_hand_asteroid_win:
+                stk.append(asteroid)
+
+        return stk
---
+++ stacks_and_queues/test_LC75__asteroid_collision.py
@@ -0,0 +1,26 @@
+import pytest
+from stacks_and_queues.LC75__asteroid_collision import Solution
+
+
+@pytest.mark.parametrize(
+    "asteroids, expected_output",
+    [
+        ([5, 10, -5], [5, 10]),
+        ([8, -8], []),
+        ([10, 2, -5], [10]),
+        ([-2, -1, 1, 2], [-2, -1, 1, 2]),
+        ([1, -1, -2, 2], [-2, 2]),
+        ([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]),
+        ([-1, -2, -3, -4, -5], [-1, -2, -3, -4, -5]),
+        ([1], [1]),
+        ([], []),
+        ([2, -2, 1, -1], []),
+        ([10, -10, 10, -10], []),
+        ([1, 2, -2, -1], []),
+        ([3, 2, -2, -3], []),
+    ],
+)
+def test_asteroid_collision(asteroids, expected_output):
+    solution = Solution()
+    actual_output = solution.asteroidCollision(asteroids)
+    assert actual_output == expected_output
