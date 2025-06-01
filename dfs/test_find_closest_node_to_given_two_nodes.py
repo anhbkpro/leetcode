4a5d8ac436e51664f37ed39a5360f8ab12f9d842
AnhL <laingocanh1990@gmail.com>
2025-06-01 20:26:15 +0700

    fix import

--- dfs/test_find_closest_node_to_given_two_nodes.py
+++ dfs/test_find_closest_node_to_given_two_nodes.py
@@ -1,5 +1,5 @@
 import unittest
-from find_closest_node_to_given_two_nodes import Solution
+from .find_closest_node_to_given_two_nodes import Solution
 
 
 class TestFindClosestNodeToGivenTwoNodes(unittest.TestCase):
