c5780901ef8e964d4d1ce50729907d8c0802b939
AnhL <laingocanh1990@gmail.com>
2025-06-01 20:24:57 +0700

    2929. Distribute Candies Among Children II

---
+++ arraystrings/distribute_candies_among_children_ii.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def distributeCandies(self, n: int, limit: int) -> List[int]:
+        ans = 0
+        for i in range(min(limit, n) + 1):
+            if n - i > 2 * limit:
+                continue
+            ans += min(n - i, limit) - max(0, n - i - limit) + 1
+        return ans
