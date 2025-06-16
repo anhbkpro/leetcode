a8b923cc1b6edb76c8e3642c5fd305e2cefc854f
AnhL <laingocanh1990@gmail.com>
2025-06-16 21:44:10 +0700

    reduce compare operation and reduce loop size

--- greedy/maximum_difference_between_increasing_elements.py
+++ greedy/maximum_difference_between_increasing_elements.py
@@ -5,8 +5,10 @@ class Solution:
     def maximumDifference(self, nums: List[int]) -> int:
         min_num = nums[0]
         ans = -1
-        for num in nums:
-            min_num = min(min_num, num)
+        for i in range(1, len(nums)):
+            num = nums[i]
             if min_num < num:
                 ans = max(ans, num - min_num)
+            else:
+                min_num = num
         return ans
