7b37a1f547bef422442466975f773e2d51f983ea
AnhL <laingocanh1990@gmail.com>
2025-06-21 21:35:36 +0700

    3085. Minimum Deletions to Make String K-Special

--- arraystrings/maximum_manhattan_distance_after_k_changes.py
+++ arraystrings/maximum_manhattan_distance_after_k_changes.py
@@ -12,17 +12,12 @@ class Solution:
             elif it == "W":
                 west += 1
             times1 = min(north, south, k)  # modification times for N and S
-            times2 = min(
-                east, west, k - times1
-            )  # modification times for E and W
+            times2 = min(east, west, k - times1)  # modification times for E and W
             ans = max(
                 ans,
-                self.count(north, south, times1)
-                + self.count(east, west, times2),
+                self.count(north, south, times1) + self.count(east, west, times2),
             )
         return ans
 
     def count(self, drt1: int, drt2: int, times: int) -> int:
-        return (
-            abs(drt1 - drt2) + times * 2
-        )  # Calculate modified Manhattan distance
+        return abs(drt1 - drt2) + times * 2  # Calculate modified Manhattan distance
