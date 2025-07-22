d1c369303a73dc75f99ed53c4c2519de3f91e69f
AnhL <laingocanh1990@gmail.com>
2025-07-22 21:13:05 +0700

    1695. Maximum Erasure Value - remember last index

--- sliding_windows/maximum_erasure_value.py
+++ sliding_windows/maximum_erasure_value.py
@@ -21,3 +21,20 @@ class Solution:
             right += 1
 
         return ans
+
+
+class SolutionRememberLastIndex:
+    def maximumUniqueSubarray(self, nums: List[int]) -> int:
+        n = len(nums)
+        last_index_map = defaultdict(int)
+        prefix_sum = [0] * (n + 1)
+        ans = start = 0
+        for end in range(n):
+            curr_element = nums[end]
+            prefix_sum[end + 1] = prefix_sum[end] + curr_element
+            if curr_element in last_index_map:
+                start = max(start, last_index_map[curr_element] + 1)
+            ans = max(ans, prefix_sum[end + 1] - prefix_sum[start])
+            last_index_map[curr_element] = end
+
+        return ans
