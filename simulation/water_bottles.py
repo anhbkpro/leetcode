510608db7a9ab9e3bb0c2cb95dbbe873f19b3a44
AnhL <laingocanh1990@gmail.com>
2025-10-01 11:19:28 +0700

    1518. Water Bottles
    Signed-off-by: AnhL <laingocanh1990@gmail.com>

--- simulation/water_bottles.py
+++ simulation/water_bottles.py
@@ -1,10 +1,10 @@
 class Solution:
     def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
-        ans = numBottles
+        consumed_bottles = numBottles
         empty = numBottles
         while empty >= numExchange:
             numBottles = empty // numExchange
-            ans += numBottles
+            consumed_bottles += numBottles
             empty = numBottles + empty % numExchange
 
-        return ans
+        return consumed_bottles
--- simulation/water_bottles_test.py
+++ simulation/water_bottles_test.py
@@ -1,6 +1,24 @@
-from .water_bottles import Solution
+import unittest
+from simulation.water_bottles import Solution
 
+class TestWaterBottles(unittest.TestCase):
+    def setUp(self):
+        self.sol = Solution()
 
-def test_water_bottles():
-    assert Solution().numWaterBottles(numBottles=9, numExchange=3) == 13
-    assert Solution().numWaterBottles(numBottles=15, numExchange=4) == 19
+    def test_example1(self):
+        self.assertEqual(self.sol.numWaterBottles(9, 3), 13)
+
+    def test_example2(self):
+        self.assertEqual(self.sol.numWaterBottles(15, 4), 19)
+
+    def test_no_exchange(self):
+        self.assertEqual(self.sol.numWaterBottles(5, 10), 5)
+
+    def test_one_exchange(self):
+        self.assertEqual(self.sol.numWaterBottles(3, 3), 4)
+
+    def test_large_numbers(self):
+        self.assertEqual(self.sol.numWaterBottles(100, 5), 124)
+
+if __name__ == "__main__":
+    unittest.main()
