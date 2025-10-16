625b28eb20a124e96798574dbf6336424336e7db
AnhL <laingocanh1990@gmail.com>
2025-10-16 19:35:55 +0700

    smallest_missing_non_negative_integer_after_operations
    Signed-off-by: AnhL <laingocanh1990@gmail.com>

---
+++ greedy/smallest_missing_non_negative_integer_after_operations.py
@@ -0,0 +1,11 @@
+from typing import Counter, List
+
+
+class Solution:
+    def findSmallestInteger(self, nums: List[int], value: int) -> int:
+        mp = Counter(x % value for x in nums)
+        mex = 0
+        while mp[mex % value] > 0:
+            mp[mex % value] -= 1
+            mex += 1
+        return mex
