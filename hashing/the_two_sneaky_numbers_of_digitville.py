59a693d680d6977758482d9e07c03d55a9ff1f19
anhlai <laingocanh1990@gmail.com>
2025-10-31 22:26:43 +0700

    3289. The Two Sneaky Numbers of Digitville
    Signed-off-by: anhlai <laingocanh1990@gmail.com>

---
+++ hashing/the_two_sneaky_numbers_of_digitville.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def getSneakyNumbers(self, nums: List[int]) -> List[int]:
+        res = []
+        count = {}
+        for x in nums:
+            count[x] = count.get(x, 0) + 1
+            if count[x] == 2:
+                res.append(x)
+        return res
