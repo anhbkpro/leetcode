4e1209d1d5ac43365701075584985df96de6265e
anhlai <laingocanh1990@gmail.com>
2025-11-09 16:00:48 +0700

    2169. Count Operations to Obtain Zero
    Signed-off-by: anhlai <laingocanh1990@gmail.com>

---
+++ arraystrings/count_operations_to_obtain_zero.py
@@ -0,0 +1,7 @@
+class Solution:
+    def countOperations(self, num1: int, num2: int) -> int:
+        op = 0
+        while num1 > 0 and num2 > 0:
+            num1, num2 = (num1 - num2, num2) if num1 >= num2 else (num1, num2 - num1)
+            op += 1
+        return op
