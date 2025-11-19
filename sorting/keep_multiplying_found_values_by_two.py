ac0551d30f22e0c9bf6d3c151f5eba4d315461fc
anhlai <laingocanh1990@gmail.com>
2025-11-19 20:15:41 +0700

    2154. Keep Multiplying Found Values by Two
    Signed-off-by: anhlai <laingocanh1990@gmail.com>

---
+++ sorting/keep_multiplying_found_values_by_two.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def findFinalValue(self, nums: List[int], original: int) -> int:
+        nums.sort()
+        for num in nums:
+            if num == original:
+                original *= 2
+        return original
