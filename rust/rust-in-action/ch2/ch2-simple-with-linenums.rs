6cfe0e3a4dcbce854d8e586e6f9a163fa4a6d3e6
anhlai <laingocanh1990@gmail.com>
2025-11-26 20:24:43 +0700

    manually incrementing an index variable
    Signed-off-by: anhlai <laingocanh1990@gmail.com>

---
+++ rust/rust-in-action/ch2/ch2-simple-with-linenums.rs
@@ -0,0 +1,16 @@
+fn main() {
+  let search_term = "picture";
+  let quote = "\n
+Every face, every shop, bedroom window, public-house, and
+dark square is a picture feverishly turned--in search of what?
+It is the same with books. What do we seek through millions of pages?";
+
+  let mut line_number: usize = 1;
+
+  for line in quote.lines() {
+    if line.contains(search_term) {
+      println!("{}: {}", line_number, line);
+    }
+    line_number += 1;
+  }
+}
