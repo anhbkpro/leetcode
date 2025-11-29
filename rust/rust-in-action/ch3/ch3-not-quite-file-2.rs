334623ffe29ee5007ac5ebc71b513427f9c07653
anhlai <laingocanh1990@gmail.com>
2025-11-29 11:06:16 +0700

    use Struct to mimic a file and simulate reading its contents
    Signed-off-by: anhlai <laingocanh1990@gmail.com>

--- rust/rust-in-action/ch3/ch3-not-quite-file-2.rs
+++ rust/rust-in-action/ch3/ch3-not-quite-file-2.rs
@@ -6,5 +6,34 @@ struct File {
     data: Vec<u8>,
 }
 
-fn open(f: &mut File) {true}
+fn open(f: &mut File) -> bool {true}
 
+fn close(f: &mut File) -> bool {true}
+
+fn read(f: &mut File, save_to: &mut Vec<u8>) -> usize { // Returns the number of bytes read
+    let mut tmp = f.data.clone(); // Makes a copy of the data here because save_to.append() shrinks the input Vec<T>
+    let read_length = tmp.len();
+
+    save_to.reserve(read_length); // Ensures that there is sufficient space to fit the incoming data
+    save_to.append(&mut tmp); // Allocates sufficient data in the save_to buffer to hold the contents of f
+    read_length
+}
+
+fn main() {
+    let mut f2 = File {
+        name: String::from("2.txt"),
+        data: vec![114, 117, 115, 116, 33],
+    };
+
+    let mut buffer: Vec<u8> = vec![];
+
+    open(&mut f2);
+    let f2_length = read(&mut f2, &mut buffer);
+    close(&mut f2);
+
+    let text = String::from_utf8_lossy(&buffer);
+
+    println!("{:?}", f2);
+    println!("{} is {} bytes long!", f2.name, f2_length);
+    println!("{}", text);
+}
