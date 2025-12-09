6ac853c5dddcb160c71c46188863df9e1c0c6a18
anhlai <laingocanh1990@gmail.com>
2025-12-09 07:30:56 +0700

    3583. Count Special Triplets
    Signed-off-by: anhlai <laingocanh1990@gmail.com>

---
+++ rust/problems/count_special_triplets/src/main.rs
@@ -0,0 +1,40 @@
+use std::collections::HashMap;
+
+struct Solution;
+
+impl Solution {
+    pub fn special_triplets(nums: Vec<i32>) -> i32 {
+        const MOD: i64 = 1_000_000_007;
+        let mut num_cnt: HashMap<i32, i32> = HashMap::new();
+        let mut num_partial_cnt: HashMap<i32, i32> = HashMap::new();
+
+        for &v in &nums {
+            *num_cnt.entry(v).or_insert(0) += 1;
+        }
+
+        let mut ans: i64 = 0;
+        for v in nums {
+            let target = v * 2;
+            let l_cnt = *num_partial_cnt.get(&target).unwrap_or(&0);
+            *num_partial_cnt.entry(v).or_insert(0) += 1;
+            let total_cnt = *num_cnt.get(&target).unwrap_or(&0);
+            let partial_cnt = *num_partial_cnt.get(&target).unwrap_or(&0);
+            let r_cnt = total_cnt - partial_cnt;
+            ans = (ans + l_cnt as i64 * r_cnt as i64) % MOD;
+        }
+
+        ans as i32
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_special_triplets() {
+        assert_eq!(Solution::special_triplets(vec![6, 3, 6]), 1);
+        assert_eq!(Solution::special_triplets(vec![8, 4, 2, 8, 4]), 2);
+
+    }
+}
