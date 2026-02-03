dc328372580fc9d6ff95efe2f8a7a90e3e51e825
anhlai <laingocanh1990@gmail.com>
2026-02-03 21:52:06 +0700

    better way to check a turn
    Signed-off-by: anhlai <laingocanh1990@gmail.com>

--- python/sorting/trionic_array_i.py
+++ python/sorting/trionic_array_i.py
@@ -2,27 +2,15 @@ from typing import List
 
 class Solution:
     def isTrionic(self, nums: List[int]) -> bool:
-        if len(nums) < 3:
+        n = len(nums)
+        if nums[0] >= nums[1]:
             return False
 
-        # 1 = increasing, -1 = decreasing
-        direction = 1
-        turns = 1
+        count = 1
+        for i in range(2, n):
+            if nums[i - 1] == nums[i]:
+                return False
+            if (nums[i - 2] - nums[i - 1]) * (nums[i - 1] - nums[i]) < 0:
+                count += 1
 
-        if nums[1] <= nums[0]:
-            return False  # must start increasing
-
-        for i in range(1, len(nums)):
-            diff = nums[i] - nums[i - 1]
-
-            if diff == 0:
-                return False  # must be strictly monotonic
-
-            if diff > 0 and direction == -1:
-                direction = 1
-                turns += 1
-            elif diff < 0 and direction == 1:
-                direction = -1
-                turns += 1
-
-        return turns == 3
+        return count == 3
