8170fba7c82410c0c0701b7dbbfc85979d52fb27
anhlai <laingocanh1990@gmail.com>
2026-02-05 22:56:57 +0700

    3379. Transformed Array
    Signed-off-by: anhlai <laingocanh1990@gmail.com>

---
+++ rust/problems/transformed_array/src/main.rs
@@ -0,0 +1,18 @@
+struct Solution;
+
+
+impl Solution {
+    pub fn construct_transformed_array(nums: Vec<i32>) -> Vec<i32> {
+        let n = nums.len();
+        nums.iter()
+            .enumerate()
+            .map(|(i, &num)| nums[((i as i32 + num) % n as i32 + n as i32) as usize % n])
+            .collect()
+    }
+}
+
+fn main() {
+    let nums = vec![1, 2, 3, 4, 5];
+    let result = Solution::construct_transformed_array(nums);
+    println!("{:?}", result);
+}
