4b96113541b8f1a0f3f0a8b5a58f3c72d67b7921
anhlai <laingocanh1990@gmail.com>
2026-02-07 21:25:49 +0700

    1653. Minimum Deletions to Make String Balanced
    Signed-off-by: anhlai <laingocanh1990@gmail.com>

--- python/arraystrings/minimum_deletions_to_make_string_balanced.py
+++ python/arraystrings/minimum_deletions_to_make_string_balanced.py
@@ -25,3 +25,25 @@ class Solution:
         for i in range(n):
             min_deletions = min(min_deletions, count_a[i] + count_b[i])
         return min_deletions
+
+    def minimum_deletions_combined_pass(self, s: str) -> int:
+        n = len(s)
+        count_a = [0] * n
+        a_count = 0
+
+        # First pass: compute count_a which stores the number of
+        # 'a' characters to the right of the current position
+        for i in range(n - 1, -1, -1):
+            count_a[i] = a_count
+            if s[i] == "a":
+                a_count += 1
+
+        min_deletions = n
+        b_count = 0
+        # Second pass: compute minimum deletions on the fly
+        for i in range(n):
+            min_deletions = min(count_a[i] + b_count, min_deletions)
+            if s[i] == "b":
+                b_count += 1
+
+        return min_deletions
