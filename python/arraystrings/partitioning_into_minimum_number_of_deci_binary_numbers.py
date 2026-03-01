4681218874c9c18107b8f22f8e636131b552119a
anhlai <laingocanh1990@gmail.com>
2026-03-01 21:06:46 +0700

    1689. Partitioning Into Minimum Number Of Deci-Binary Numbers
    Signed-off-by: anhlai <laingocanh1990@gmail.com>

--- python/arraystrings/partitioning_into_minimum_number_of_deci_binary_numbers.py
+++ python/arraystrings/partitioning_into_minimum_number_of_deci_binary_numbers.py
@@ -1,7 +1,3 @@
 class Solution:
     def minPartitions(self, n: str) -> int:
-        max_ch = 0
-        for ch in n:
-            max_ch = max(max_ch, int(ch))
-
-        return max_ch
+        return int(max(n))
