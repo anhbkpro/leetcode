208665fa7b6204a1585865ba24797d24c9eacd6e
anhlai <laingocanh1990@gmail.com>
2026-03-04 22:07:45 +0700

    1582. Special Positions in a Binary Matrix
    Signed-off-by: anhlai <laingocanh1990@gmail.com>

---
+++ python/matrix/special_positions_in_a_binary_matrix.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def numSpecial(self, mat: List[List[int]]) -> int:
+        m = len(mat)
+        n = len(mat[0])
+        r = [0] * m
+        c = [0] * n
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] == 1:
+                    r[i] += 1
+                    c[j] += 1
+        ans = 0
+        for i in range(m):
+            for j in range(n):
+                if r[i] == 1 and c[j] == 1 and mat[i][j] == 1:
+                    ans += 1
+
+        return ans
