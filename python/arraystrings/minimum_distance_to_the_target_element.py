9fdc368d5e0f2bb1fb5577e7a2b69d5deb7e5f24
anhlai <laingocanh1990@gmail.com>
2026-04-14 11:34:40 +0700

    1848. Minimum Distance to the Target Element
    Signed-off-by: anhlai <laingocanh1990@gmail.com>

---
+++ python/arraystrings/minimum_distance_to_the_target_element.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def getMinDistance(self, nums: List[int], target: int, start: int) -> int:
+        ans = len(nums)
+        for i, num in enumerate(nums):
+            if num == target:
+                ans = min(ans, abs(i - start))
+
+        return ans
