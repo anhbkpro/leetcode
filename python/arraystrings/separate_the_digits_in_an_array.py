8e9fc016e7bb2f6b3f44784ad9d4d7af7afb546c
anhlai <laingocanh1990@gmail.com>
2026-05-11 19:58:46 +0700

    2553. Separate the Digits in an Array
    Signed-off-by: anhlai <laingocanh1990@gmail.com>

---
+++ python/arraystrings/separate_the_digits_in_an_array.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def separateDigits(self, nums: List[int]) -> List[int]:
+        res = []
+        for x in nums:
+            tmp = []
+            while x > 0:
+                tmp.append(x % 10)
+                x //= 10
+            res.extend(tmp[::-1])
+        return res
