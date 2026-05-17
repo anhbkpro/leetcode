ddee4267bf1cdd5a5cb645ba4ba6c4ca05313068
anhlai <laingocanh1990@gmail.com>
2026-05-17 19:58:36 +0700

    154. Find Minimum in Rotated Sorted Array II
    Signed-off-by: anhlai <laingocanh1990@gmail.com>

---
+++ python/arraystrings/find_minimum_in_rotated_sorted_array_ii.py
@@ -0,0 +1,10 @@
+from bisect import bisect_left
+from typing import List
+
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        while len(nums) > 1 and nums[-1] == nums[0]:
+            nums.pop()
+
+        return nums[bisect_left(nums, True, key=lambda n: n <= nums[-1])]
