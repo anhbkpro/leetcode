7eab035a0bd8ce31320c20c317c61428e04ccada
anhlai <laingocanh1990@gmail.com>
2026-06-08 19:28:28 +0700

    Implement pivotArray2 method for partitioning array based on pivot and enhance type hinting in partition_array_according_to_given_pivot.py
    Signed-off-by: anhlai <laingocanh1990@gmail.com>

--- python/arraystrings/partition_array_according_to_given_pivot.py
+++ python/arraystrings/partition_array_according_to_given_pivot.py
@@ -6,7 +6,7 @@ class Solution:
         ans = [0] * len(nums)
         less = 0
         greater = len(nums) - 1
-        for i, j in zip(range(len(nums)), range(len(nums) - 1, -1, -1)):
+        for i, j in zip[tuple[int, int]](range(len(nums)), range(len(nums) - 1, -1, -1)):
             if nums[i] < pivot:
                 ans[less] = nums[i]
                 less += 1
@@ -17,4 +17,22 @@ class Solution:
             ans[less] = pivot
             less += 1
 
+        return ans
+
+
+    def pivotArray2(self, nums: List[int], pivot: int) -> List[int]:
+        less_than_pivot = list[int]()
+        equal_pivot = list[int]()
+        greater_than_pivot = list[int]()
+        ans = list[int]()
+        for num in nums:
+            if num < pivot:
+                less_than_pivot.append(num)
+            elif num == pivot:
+                equal_pivot.append(num)
+            else:
+                greater_than_pivot.append(num)
+        ans.extend(less_than_pivot)
+        ans.extend(equal_pivot)
+        ans.extend(greater_than_pivot)
         return ans
