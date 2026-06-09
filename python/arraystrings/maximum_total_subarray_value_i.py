6a1d8f3a700562019901a0c78f74aaa5908df28c
anhlai <laingocanh1990@gmail.com>
2026-06-09 21:01:46 +0700

    3689. Maximum Total Subarray Value I
    Signed-off-by: anhlai <laingocanh1990@gmail.com>

---
+++ python/arraystrings/maximum_total_subarray_value_i.py
@@ -0,0 +1,8 @@
+from typing import List
+
+
+class Solution:
+    def maxTotalValue(self, nums: List[int], k: int) -> int:
+        m1 = min(nums)
+        m2 = max(nums)
+        return (m2 - m1) * k
