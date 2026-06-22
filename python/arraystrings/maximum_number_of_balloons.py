6f330fc2e40b30f4ab933c65e469397cd51731b6
anhlai <laingocanh1990@gmail.com>
2026-06-22 08:17:06 +0700

    1189. Maximum Number of Balloons
    Signed-off-by: anhlai <laingocanh1990@gmail.com>

---
+++ python/arraystrings/maximum_number_of_balloons.py
@@ -0,0 +1,7 @@
+from collections import Counter
+
+
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        f = Counter(text)
+        return min(f["b"], f["a"], f["l"] >> 1, f["o"] >> 1, f["n"])
