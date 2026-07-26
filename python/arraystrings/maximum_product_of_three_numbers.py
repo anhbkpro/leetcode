cf30b4dcf6266ff4164ae1c020dc6bef4f983ce9
anhlai <laingocanh1990@gmail.com>
2026-07-26 16:51:25 +0700

    628. Maximum Product of Three Numbers

---
+++ python/arraystrings/maximum_product_of_three_numbers.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def maximumProduct(self, A: List[int]) -> int:
+        A.sort()        
+        return max(
+            A[-1] * A[-2] * A[-3],
+            A[-1] * A[0] * A[1]
+        )
